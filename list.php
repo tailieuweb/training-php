924690b93018be759642d6d8bb31dc4330b6a59c
Thach Tran <thachtran990@gmail.com>
2020-11-04 00:19:42 +0700

    update List

--- list.php
+++ list.php
@@ -8,114 +8,109 @@ if (!isAdmin()) {
 }
 $results = [];
 
-if (isset($_GET['page'])) $page = intval($_GET['page']);
-else $page = 1;
-
-$page = ($page > 0) ? $page : 1;
-$limit = 2;
-$offset = ($page - 1) * $limit;
-$url = 'list.php?';
-
-$option_id_desc = array(
-    'order_by' => 'id DESC',
-    'limit' => $limit,
-    'offset' => $offset,
-);
-$option_id_asc = array(
-    'order_by' => 'id ASC',
-    'limit' => $limit,
-    'offset' => $offset,
-);
-
 
 // Gán hàm addslashes để chống sql injection
 isset($_GET['search']) ? $search = addslashes($_GET['search']) : $search = "";
-$options_search = array(
-    'where' => "username LIKE '%" . ($search) . "%' or fullname like '%" . ($search) . "%'",
-    'limit' => $limit,
-    'offset' => $offset,
-    'order_by' => 'id ASC'
-);
 $query = "SELECT * FROM users WHERE username LIKE '%$search%' OR fullname LIKE '%$search%' ";
 global $conn;
 $sql = mysqli_query($conn, $query);
 $num = mysqli_num_rows($sql);
 
-//sortby 
-$path = explode('=', $_SERVER['REQUEST_URI']);
-$id_list = $path[count($path) - 1];
-if (isset($_POST['list'])) {
-    $id_list = $_POST['list'];
-}
-if (isAdmin()) {
-    $id_list == 1 ? $_SESSION['results_user'] = get_by_options('users', $option_id_desc)
-        : $_SESSION['results_user'] = get_by_options('users', $option_id_asc);
-}
 
-//pagination
-if ($search != "") {
-    $total_rows = get_total('users', $options_search);
-} else {
-    $id_list == 1 ? $total_rows = get_total('users', $option_id_asc)
-        : $total_rows = get_total('users', $option_id_desc);
-}
-
-$total = ceil($total_rows / $limit);
-$pagination = pagination_admin($url, $page, $total);
 ?>
 
 
 
 
 <html>
-    <head>
-        <title>Danh sách Account</title>
-        
-        <link rel="stylesheet" href="public/css/bootstrap.min.css">
-        <link rel="stylesheet" href="public/css/font-awesome.min.css">
-		<link rel="stylesheet" href="public/css/styles.css">
-        <link rel="stylesheet/less" type="text/css" href="public/css/style.less" />
-    <script  type="text/javascript" src="public/js/less.min.js"></script>
-    </head>
-    <body>
-		<div class="container">
+
+<head>
+    <title>List User</title>
+
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public/css/font-awesome.min.css">
+    <link rel="stylesheet" href="public/css/styles.css">
+    <link rel="stylesheet/less" type="text/css" href="public/css/style.less" />
+    <script type="text/javascript" src="public/js/less.min.js"></script>
+    <?php
+    $rowperpage = 4;
+    $row = 0;
+
+    // Previous Button
+    if (isset($_POST['but_prev'])) {
+        $row = $_POST['row'];
+        $row -= $rowperpage;
+        if ($row < 0) {
+            $row = 0;
+        }
+    }
+
+    // Next Button
+    if (isset($_POST['but_next'])) {
+        $row = $_POST['row'];
+        $allcount = $_POST['allcount'];
+
+        $val = $row + $rowperpage;
+        if ($val < $allcount) {
+            $row = $val;
+        }
+    }
+
+    // generating orderby and sort url for table header
+    function sortorder($fieldname)
+    {
+        $sorturl = "?order_by=" . $fieldname . "&sort=";
+        $sorttype = "asc";
+        if (isset($_GET['order_by']) && $_GET['order_by'] == $fieldname) {
+            if (isset($_GET['sort']) && $_GET['sort'] == "asc") {
+                $sorttype = "desc";
+            }
+        }
+        $sorturl .= $sorttype;
+        return $sorturl;
+    }
+    ?>
+</head>
+
+<body>
+    <div class="container">
         <div class="header">
             <h2>List User</h2>
         </div>
-        <form action="search.php" method="GET"  placeholder="Nhập thông tin cần tìm"> 
-    <input type="text" name="username" /> 
-    <input type="submit" value="Search" /> 
-</form>
-    <div class="row">
-        <div class="col-md-12">
-                
-                <form action="list.php?list=" method="post">
-                    <div class="input-group">
-                        <label>Sort By:</label>
-                        <select class="form-control" name="list" id="list">
-                            <option name="list" value="2">ascending</option>
-                            <option name="list" value="1">decrease</option>
-                        </select>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Go</button>
-                </form>
-        </div>
-    </div>
-        
-        <form >
-            <?php echo display_error(); ?>	
+        <form action="search.php" method="GET" placeholder="Nhập thông tin cần tìm">
+            <input type="text" name="username" />
+            <input type="submit" value="Search" />
+        </form>
 
+
+        <?php echo display_error(); ?>
+        <div>
             <table class="table">
                 <thead>
-					<tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Images</th>
-						<th scope="col">Username</th>
-						<th scope="col">Full name</th>
-						<th scope="col">Email</th>
-						<th scope="col">Action</th>
-					</tr>
+                    <tr>
+                        <th scope="col"><a href="<?php echo sortorder('id'); ?>">ID</a></th>
+                        <th>Images</th>
+                        <th scope="col"><a href="<?php echo sortorder('username'); ?>">Username</a></th>
+                        <th scope="col"><a href="<?php echo sortorder('fullname'); ?>">FullName</a></th>
+                        <th scope="col"><a href="<?php echo sortorder('email'); ?>">Email</a></th>
+                        <th scope="col">Action</th>
+                    </tr>
                 </thead>
+
+                <?php
+                // count total number of rows
+                $sql = "SELECT COUNT(*) AS cntrows FROM users";
+                $result = mysqli_query($conn, $sql);
+                $fetchresult = mysqli_fetch_array($result);
+                $allcount = $fetchresult['cntrows'];
+
+                // selecting rows
+                $orderby = " ORDER BY id desc ";
+                if (isset($_GET['order_by']) && isset($_GET['sort'])) {
+                    $orderby = ' order by ' . $_GET['order_by'] . ' ' . $_GET['sort'];
+                }
+                ?>
+
                 <tbody>
                     <?php
                     if (isset($_REQUEST['ok'])) {
@@ -132,7 +127,7 @@ $pagination = pagination_admin($url, $page, $total);
                                     <td><?php echo $result['email']; ?></td>
                                     <td>
                                         <a href="userinfo.php?user_id=<?= $result['id'] ?>"><i class="fa fa-eye" aria-hidden="true"></i></a>
-                                        <a href="edituserid.php?id=<?php echo $result['id']?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                                        <a href="edituserid.php?id=<?php echo $result['id'] ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
                                         <a onclick="return confirm('Are you sure to delete?')" href="delete.php?user_id=<?= $result['id'] ?>"><i class="fa fa-times" aria-hidden="true"></i></a>
                                     </td>
                                 </tr>
@@ -143,55 +138,69 @@ $pagination = pagination_admin($url, $page, $total);
                         ?>
 
                         <?php } else {
-                        foreach ($_SESSION['results_user'] as $result) : ?>
-                            <tr scope="row">
-                                <td><?php echo $result['id']; ?></td>
-                                <td class="fix_overtext" ><img src="./public/images/<?php echo $result['image_profile']?>"  height="30" width="30"  alt=""></td>
-                                <td class="fix_overtext"><?php echo $result['username']; ?></td>
-                                <td  class="fix_overtext"><?php echo $result['fullname']; ?></td>
-                                <td  class="fix_overtext"><?php echo $result['email']; ?></td>
-                                <td>
-                                    
-                                <a href="chitiet.php?id=<?php echo $result['id']?>"><i class="fa fa-eye" aria-hidden="true"></i></a>
-						
-                        <a href="edituserid.php?id=<?php echo $result['id']?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                        // fetch rows
+                        $sql = "SELECT * FROM users " . $orderby . " limit $row," . $rowperpage;
+                        $result = mysqli_query($conn, $sql);
+                        //$sno = $row + 1;
+                        while ($fetch = mysqli_fetch_array($result)) {
+                            $id = $fetch['id'];
+                            $username = $fetch['username'];
+                            $fullname = $fetch['fullname'];
+                            $email = $fetch['email'];
+                            $image = $fetch['image_profile'];
+                        ?>
 
-                        <a href="delete.php?id=<?php echo $result['id']?>" class="delete"><i class="fa fa-times" aria-hidden="true"></i></a>
+
+                            <tr scope="row">
+                                <td><?php echo $id; ?></td>
+                                <td><img src="./public/images/<?php echo $image ?> " height="30" width="30" alt=""></td>
+                                <td><?php echo $username; ?></td>
+                                <td><?php echo $fullname; ?></td>
+                                <td><?php echo $email; ?></td>
+
+                                <td>
+
+                                    <a href="chitiet.php?id=<?php echo $id ?>"><i class="fa fa-eye" aria-hidden="true"></i></a>
+
+                                    <a href="edituserid.php?id=<?php echo $id ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+
+                                    <a href="delete.php?id=<?php echo $id ?>" class="delete"><i class="fa fa-times" aria-hidden="true"></i></a>
                                 </td>
                             </tr>
                     <?php
-                        endforeach;
+
+                        }
                     }
                     ?>
 
                 </tbody>
             </table>
-            <?php if ($search == "") : ?>
-                <div class="row">
-                    <div class="col-md-12">
-                        <?php echo $pagination; ?>
-                    </div>
+            <form method="post" action="">
+                <div id="div_pagination">
+                    <input type="hidden" name="row" value="<?php echo $row; ?>">
+                    <input type="hidden" name="allcount" value="<?php echo $allcount; ?>">
+                    <input type="submit" class="btn btn-info" name="but_prev" value="Previous">
+                    <input type="submit" class="btn btn-info" name="but_next" value="Next">
                 </div>
-            <?php endif;
-             ?>
-
-        </form>
+            </form>
+        </div>
         <div class="back" style="text-align: center">
-		<button type="button" class="btn btn-info" onClick="javascript:history.go(-1)">Back</button>
-          
+            <button type="button" class="btn btn-info" onClick="javascript:history.go(-1)">Back</button>
+
         </div>
-        </div>
-        <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-        <script language="JavaScript" type="text/javascript">
-        $(document).ready(function(){
-            $("a.delete").click(function(e){
-                if(!confirm('Are you delete?')){
+    </div>
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script language="JavaScript" type="text/javascript">
+        $(document).ready(function() {
+            $("a.delete").click(function(e) {
+                if (!confirm('Are you delete?')) {
                     e.preventDefault();
                     return false;
-                 }
+                }
                 return true;
             });
         });
-</script>
-    </body>
+    </script>
+</body>
+
 </html>
