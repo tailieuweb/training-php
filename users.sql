d6cebde5567380e27379a2e0c2c222b66b582edb
thinh <thinhnguyen.npt0101@gmail.com>
2020-11-04 11:09:02 +0700

    commit database

--- users.sql
+++ users.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1:3306
--- Thời gian đã tạo: Th10 04, 2019 lúc 03:50 PM
--- Phiên bản máy phục vụ: 5.7.21
--- Phiên bản PHP: 5.6.35
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 04, 2020 at 04:05 AM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `userlogin`
+-- Database: `users`
 --
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `users`
+-- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
@@ -36,16 +36,20 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(100) NOT NULL,
   `user_type` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
+  `image_profile` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
 
 --
--- Đang đổ dữ liệu cho bảng `users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `fullname`, `email`, `user_type`, `password`) VALUES
-(1, 'admin', 'Admin', 'admin1@gmail.com', 'admin', 'c4ca4238a0b923820dcc509a6f75849b'),
-(2, 'user', 'User', 'user1@gmail.com', 'user', '202cb962ac59075b964b07152d234b70');
+INSERT INTO `users` (`id`, `username`, `fullname`, `email`, `user_type`, `password`, `image_profile`) VALUES
+(1, 'admin', 'admin', 'admin@gmail.com', 'admin', 'c4ca4238a0b923820dcc509a6f75849b', 'admin_profile.png'),
+(4, 'thinh', 'thinh', 'thinhnguyen.npt0101@gmail.com', 'user', 'c81e728d9d4c2f636f067f89cc14862c', 'user_profile.png'),
+(5, 'vuong', 'vuongnguyen', 'vuong@gmail.com', 'user', 'c81e728d9d4c2f636f067f89cc14862c', 'user_profile.png'),
+(41, 'abc', 'van a', 'a11@gmail.com', 'user', '8a1148a74ba479fcaca5e34f5de73d45', 'user_profile.png'),
+(42, 'abvd', 'van a', 'avna@gmail.com', 'user', '6c25acaccd1845b9d3bc2d1f54b81c73', 'user_profile.png');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
