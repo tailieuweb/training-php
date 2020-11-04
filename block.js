2c114b404c75f225a0245a9a607e8149cf8f7670
Pham Van Hieu <vanhieup1998@gmail.com>
2020-11-04 14:39:08 +0700

    delete source

--- block.js
+++
@@ -1,7 +0,0 @@
-function mo_modal()
-{
-    document.getElementById("block_modal").style.display = "block";
-}function close_modal()
-{
-    document.getElementById("block_modal").style.display = "none";
-}
