82fb3b58ee2c102d62f2ffa825ea9f3be126e265
MinhTuyen <levantuyen25092000@gmail.com>
2020-11-04 15:17:21 +0700

    Nop_file_yc

---
+++ footer.php
@@ -0,0 +1,7 @@
+    <script src="./public/js/jquery.js"></script>
+    <script src="./public/js/bootstrap.min.js"></script>
+    <!-- <script src="./public/js/ajax.js"></script> -->
+    
+    <!-- <script>alert(document.cookie)</script> -->
+</body>
+</html>
