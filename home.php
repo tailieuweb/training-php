186d7515d21e3574da631c970744dbb33e9e8d7b
phucapplevn <phucapplevn@github.com>
2020-11-06 16:56:40 +0700

    UPDATE LOGIN - HOME PAGE

--- home.php
+++ home.php
@@ -23,42 +23,47 @@ if(isAdmin() == true && isloggedIn() == true)
 	<title>Home</title>
 	<link rel="stylesheet" type="text/css" href="public/css/styles.css">
 	<link rel="stylesheet/less" type="text/css" href="public/css/style.less" />
+	<link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public/css/font-awesome.min.css">
     <script  type="text/javascript" src="public/js/less.min.js"></script>
 
 </head>
 <body>
+
+	<div class="container">
 	<div class="header">
 		<h2>Admin - Trang chủ</h2>
 	</div>
-	<div class="content">
-		<!-- notification message -->
-		<?php if (isset($_SESSION['success'])) : ?>
-			<div class="error success" >
-				<h3>
-					<?php 
-						echo $_SESSION['success']; 
-						unset($_SESSION['success']);
-					?>
-				</h3>
-			</div>
-		<?php endif ?>
+		<div class="content">
+			<!-- notification message -->
+			<?php if (isset($_SESSION['success'])) : ?>
+				<div class="error success" >
+					<h3>
+						<?php 
+							echo $_SESSION['success']; 
+							unset($_SESSION['success']);
+						?>
+					</h3>
+				</div>
+			<?php endif ?>
 
-		<!-- logged in user information -->
-		<div class="profile_info">
-			<img src="public/images/admin_profile.png"  >
+			<!-- logged in user information -->
+			<div class="profile_info">
+				<img src="public/images/admin_profile.png"  >
 
-			<div>
-                <?php  if (isset($_SESSION['user'])) : ?>
-                    <strong><?php echo $_SESSION['user']['username']; ?></strong>
-					<small>
-						<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
-						<br>
-						<?php echo $_SESSION['user']['fullname']; ?><br>
-                        <?php echo $_SESSION['user']['email']; ?><br>
-                        <a href="admin.php">Add User</a> &nbsp; <a href="list.php?list='1'">List User</a> &nbsp; <a href="edit.php?edit=<?= $_SESSION['user']['id'] ?>">Edit Information</a><br>
-                        <a href="home.php?logout='1'" style="color: red;">Logout</a>
-					</small>
-				<?php endif ?>
+				<div>
+					<?php  if (isset($_SESSION['user'])) : ?>
+						<strong><?php echo $_SESSION['user']['username']; ?></strong>
+						<small>
+							<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
+							<br>
+							<?php echo $_SESSION['user']['fullname']; ?><br>
+							<?php echo $_SESSION['user']['email']; ?><br>
+							<a class="btn" href="admin.php">Add User</a> &nbsp; <a  class="btn" href="list.php?list='1'">List User</a> &nbsp; <a  class="btn" href="edit.php?edit=<?= $_SESSION['user']['id'] ?>">Edit Information</a><br>
+							<a class="btn login" href="home.php?logout='1'" style="background-color: black; color: white; margin-top: 12px;">Logout</a>
+						</small>
+					<?php endif ?>
+				</div>
 			</div>
 		</div>
 	</div>
--- login.php
+++ login.php
@@ -22,32 +22,52 @@ session_start();
 	<link rel="stylesheet" type="text/css" href="public/css/styles.css">
 	<link rel="stylesheet/less" type="text/css" href="public/css/style.less" />
     <script  type="text/javascript" src="public/js/less.min.js"></script>
+	<link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public/css/font-awesome.min.css">
+
 </head>
+<style>
+
+	.btn, login{
+		color: white;
+		background-color:black;
+		padding: 10px 30px;
+	    text-align: center;
+	}
+
+</style>
 <body>
-	<div class="header">
+<div class="container">
+
+<div class="header">
 		<h2>Login</h2>
 	</div>
 	<form method="post" action="login.php">
+	<div class="lg-np">
+	<div class="container">
 
-		<?php echo display_error(); ?>
+<?php echo display_error(); ?>
 
-		<div class="input-group">
-			<label>Username</label>
-			<input type="text" value="<?php if (isset($_COOKIE["user"])){echo $_COOKIE["user"];}?>" name="username" >
-		</div>
-		<div class="input-group">
-			<label>Password</label>
-			<input type="password" value="<?php if (isset($_COOKIE["pass"])){echo $_COOKIE["pass"];}?>" name="password">
-		</div>
-		<div class="row align-items-center">
-			<input type="checkbox" name="remember">Remember Me
-		</div>
-		<div class="input-group">
-			<button type="submit" class="btn" name="login_btn">Login</button>
-		</div>
-		<p>
-			Not yet a member? <a class="btn_register" href="register.php">Sign up</a>
-		</p>
+<div class="input-group">
+	<label>Username</label>
+	<input type="text" value="<?php if (isset($_COOKIE["user"])){echo $_COOKIE["user"];}?>" name="username" >
+</div>
+<div class="input-group">
+	<label>Password</label>
+	<input type="password" value="<?php if (isset($_COOKIE["pass"])){echo $_COOKIE["pass"];}?>" name="password">
+</div>
+<div class="row align-items-center">
+	<input type="checkbox" name="remember">Remember Me
+</div>
+<div class="input-group">
+	<button type="submit" class="btn login" name="login_btn">Login</button>
+</div>
+</div>
+<p>
+	Not yet a member? <a class="btn_register" href="register.php">Sign up</a>
+</p>
+	</div>
 	</form>
+</div>
 </body>
 </html>
