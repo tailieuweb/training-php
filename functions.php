493c7513396aaea17aa5af9fb7c0dc3a1d1aed1a
letrjeudong <letrjeudong@gmail.com>
2020-11-06 14:52:47 +0700

    commit fix adduserid

--- functions.php
+++ functions.php
@@ -110,18 +110,19 @@ function register(){
 		{
 			if (isset($_POST['user_type'])) {
 				$user_type = escape($_POST['user_type']);
-				$query = "INSERT INTO users (username,fullname, email, user_type, password, image_profile) 
-						  VALUES('$username', '$fullname', '$email', '$user_type', '$password', '$path')";
+				$query = "INSERT INTO users (id_encode, username, fullname, email, user_type, password, image_profile, version) 
+						  VALUES('0','$username', '$fullname', '$email', '$user_type', '$password', '$path', '0')";
 				mysqli_query($conn, $query);
 				$_SESSION['success']  = "New user successfully created!!";
+				mysqli_query($conn, "UPDATE users SET id_encode = md5(id) ORDER BY id DESC LIMIT 1");
 				header('location: home.php');
 			}
 			else
 			{
-				$query = "INSERT INTO users (username, fullname, email, user_type, password,image_profile) 
-						  VALUES('$username', '$fullname', '$email', 'user', '$password', '$path')";
+				$query = "INSERT INTO users (username,id_encode, fullname, email, user_type, password, image_profile, version) 
+				VALUES('$username','', '$fullname', '$email', '$user_type', '$password', '$path', '')";
 				mysqli_query($conn, $query);
-	
+				mysqli_query($conn, " UPDATE `users` SET `id_encode` = '100' WHERE `id` = 'SELECT MAX(id)'");
 				$logged_in_user_id = mysqli_insert_id($conn);
 	
 				$_SESSION['user'] = getUserById($logged_in_user_id); // put logged in user in session
@@ -309,15 +310,6 @@ function display_error() {
 	}
 }
 
-//encode id
-function getLink($id)
-{
-	//mã hóa md5 chuổi id 
-	$random = md5(uniqid($id));
-	$_SESSION['links_edit'][$random] = $_SESSION['info_user_id'][$random] = $id;
-	//dùng mảng 2 chiều $_SESSION
-	return "$random";
-}
 
 function getUserById($id){
 	global $conn;
@@ -447,6 +439,24 @@ function passwordID() {
 	if ($password_2 != $password_3) {
 		array_push($errors, "The two passwords do not match");
 	}
+
+	if(!empty($password_1) && !empty($password_2) && !empty($password_3))
+	{
+		if (!preg_match("/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,20}$/", $password_1)) 
+		{
+			array_push($errors, "Password old: Minimum six characters, at least one letter and one number");
+		}
+		if (!preg_match("/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,20}$/",$password_2)) 
+		{
+			array_push($errors, "Password new: Minimum six characters, at least one letter and one number");
+		}
+		if (!preg_match("/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,20}$/",$password_3)) 
+		{
+			array_push($errors, "Re-enter new password: Minimum six characters, at least one letter and one number");
+		}
+	}
+
+
 	// attempt login if no errors on form
 	if (count($errors) == 0) {
 		$password_1  = md5($password_1 );
@@ -471,7 +481,7 @@ function passwordID() {
                     setcookie("user", $row['username'], time() + (86400 * 30)); 
                     setcookie("pass", $row['password'], time() + (86400 * 30)); 
                 }
-				mysqli_query($conn, "UPDATE `users` SET `password` = '$password_2' WHERE `id` = '$id'");	  
+				mysqli_query($conn, "UPDATE `users` SET `password` = '$password_2' WHERE `id` = '$id' AND `version` = '$version1'");	  
 				header('location: index.php');
 			}
 		}else {
@@ -483,6 +493,3 @@ function passwordID() {
 if (isset($_POST['password_btn'])) {
 	passwordID();
 }
-
-
-
