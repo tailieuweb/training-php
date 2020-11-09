a09255db2791490a00722296c767c48cb3e85f5a
Thuận Trần <thuankg1752@gmail.com>
2020-11-09 14:27:34 +0700

    add regex

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@ if (isset($_POST['register_btn'])) {
 		array_push($errors, "Email đã tồn tại. Vui lòng nhập Email khác");
 	}
 	//kiểm tra định dạng chuổi password 
-	elseif (!preg_match("/([a-zA-Z0-9]([\w.!@#$%^&*()]+){8,})/", $password_1)) {
+	elseif (!preg_match("/(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}/", $password_1)) {
 		array_push($errors, "Pass phải từ 8 ký tự trở lên và phải có chứ số, chữ in hoa, chữ thường và có ký tự đặc biệt!!");
 	} else register();
 }
@@ -150,18 +150,18 @@ function register()
 function edit($user_id)
 {
 	global $conn, $errors, $username, $fullname, $email;
-	
+
 	$username = escape($_POST['username']);
 	$email = escape($_POST['email']);
 	$fullname = escape($_POST['fullnme']);
 	//make sure form is filled property
-	if(empty($username)){
+	if (empty($username)) {
 		array_push($errors, "Username is required");
 	}
-	if(empty($email)){
+	if (empty($email)) {
 		array_push($errors, "Email is required");
 	}
-	if(empty($fullname)){
+	if (empty($fullname)) {
 		array_push($errors, "Fullname is required");
 	}
 
