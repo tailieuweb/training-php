8ddff1f98f818a78372e26ea99dca7531467a6dd
duc622 <duc.phan622@gmail.com>
2021-09-24 15:24:03 +0700

    fix bug delete user fix bug update user

--- form_user.php
+++ form_user.php
@@ -3,18 +3,19 @@ require_once 'models/UserModel.php';
 $userModel = new UserModel();
 
 $user = NULL; //Add new user
-$id = NULL;
+$name = NULL;
 
-if (!empty($_GET['id'])) {
-    $id = $_GET['id'];
-    $user = $userModel->findUserById($id);//Update existing user
+if (!empty($_GET['name'])) {
+    $name = $_GET['name'];
+    $user = $userModel->findUserByName($name); //Update existing user
 }
 
 
 if (!empty($_POST['submit'])) {
 
-    if (!empty($id)) {
-        $userModel->updateUser($_POST);
+    if (!empty($name)) {
+        $oldName = $name;
+        $userModel->updateUser($_POST, $oldName);
     } else {
         $userModel->insertUser($_POST);
     }
@@ -24,36 +25,39 @@ if (!empty($_POST['submit'])) {
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>User form</title>
     <?php include 'views/meta.php' ?>
 </head>
+
 <body>
-    <?php include 'views/header.php'?>
+    <?php include 'views/header.php' ?>
     <div class="container">
 
-            <?php if ($user || empty($id)) { ?>
-                <div class="alert alert-warning" role="alert">
-                    User form
+        <?php if ($user || empty($name)) { ?>
+            <div class="alert alert-warning" role="alert">
+                User form
+            </div>
+            <form method="POST">
+                <input type="hidden" name="name" value="<?php echo $name ?>">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input class="form-control" name="name" placeholder="Name" value="<?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?>">
                 </div>
-                <form method="POST">
-                    <input type="hidden" name="id" value="<?php echo $id ?>">
-                    <div class="form-group">
-                        <label for="name">Name</label>
-                        <input class="form-control" name="name" placeholder="Name" value="<?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?>">
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" class="form-control" placeholder="Password">
-                    </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="form-control" placeholder="Password">
+                </div>
 
-                    <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
-                </form>
-            <?php } else { ?>
-                <div class="alert alert-success" role="alert">
-                    User not found!
-                </div>
-            <?php } ?>
+                <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
+            </form>
+        <?php } else { ?>
+            <div class="alert alert-success" role="alert">
+                User not found!
+            </div>
+        <?php } ?>
     </div>
 </body>
+
 </html>
--- models/UserModel.php
+++ models/UserModel.php
@@ -2,25 +2,36 @@
 
 require_once 'BaseModel.php';
 
-class UserModel extends BaseModel {
+class UserModel extends BaseModel
+{
 
-    public function findUserById($id) {
-        $sql = 'SELECT * FROM users WHERE id = '.$id;
+    public function findUserByName($name)
+    {
+        $sql = "SELECT * FROM users WHERE name = '$name' ";
+        $user = $this->select($sql);
+        return $user;
+    }
+
+    public function findUserById($id)
+    {
+        $sql = 'SELECT * FROM users WHERE id = ' . $id;
         $user = $this->select($sql);
 
         return $user;
     }
 
-    public function findUser($keyword) {
-        $sql = 'SELECT * FROM users WHERE user_name LIKE %'.$keyword.'%'. ' OR user_email LIKE %'.$keyword.'%';
+    public function findUser($keyword)
+    {
+        $sql = 'SELECT * FROM users WHERE user_name LIKE %' . $keyword . '%' . ' OR user_email LIKE %' . $keyword . '%';
         $user = $this->select($sql);
 
         return $user;
     }
 
-    public function auth($userName, $password) {
+    public function auth($userName, $password)
+    {
         $md5Password = $password;
-        $sql = 'SELECT * FROM users WHERE name = "' . $userName . '" AND password = "'.$md5Password.'"';
+        $sql = 'SELECT * FROM users WHERE name = "' . $userName . '" AND password = "' . $md5Password . '"';
 
         $user = $this->select($sql);
         return $user;
@@ -31,10 +42,16 @@ class UserModel extends BaseModel {
      * @param $id
      * @return mixed
      */
-    public function deleteUserById($id) {
-        $sql = 'DELETE FROM users WHERE id = '.$id;
-        return $this->delete($sql);
-
+    public function deleteUserById($id)
+    {
+        $maxId = $this->select("SELECT max(id) FROM table");
+        for ($i = 1; $i <= $maxId; $i++) {
+            if (md5($i) == $id) {
+                $sql = 'DELETE FROM users WHERE id = ' . $i;
+                return $this->delete($sql);
+            }
+        }
+        return;
     }
 
     /**
@@ -42,11 +59,11 @@ class UserModel extends BaseModel {
      * @param $input
      * @return mixed
      */
-    public function updateUser($input) {
-        $sql = 'UPDATE users SET 
-                 name = "' . $input['name'] .'", 
-                 password="'. md5($input['password']) .'"
-                WHERE id = ' . $input['id'];
+    public function updateUser($input, $oldName)
+    {
+        $name = $input['name'];
+        $password = $input['password'];
+        $sql = "UPDATE users SET name = '$name' , password = '$password' WHERE name = '$oldName' ";
         $user = $this->update($sql);
 
         return $user;
@@ -57,16 +74,20 @@ class UserModel extends BaseModel {
      * @param $input
      * @return mixed
      */
-    public function insertUser($input) {
+    public function insertUser($input)
+    {
+        echo $input;
+        return;
         $sql = "INSERT INTO `app_web1`.`users` (`name`, `password`) VALUES (" .
-                "'" . $input['name'] . "', '".$input['password']."')";
+            "'" . $input['name'] . "', '" . $input['password'] . "')";
 
         $user = $this->insert($sql);
 
         return $user;
     }
 
-    public function getUsers($params = []) {
+    public function getUsers($params = [])
+    {
         $sql = 'SELECT * FROM users';
         $users = $this->select($sql);
 
