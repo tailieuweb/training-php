cc0c87f6059c6ae1370de722106e512f83479928
VoDangVinh <01686228222vinh@gmail.com>
2021-09-25 16:56:41 +0700

    update

--- models/UserModel.php
+++ models/UserModel.php
@@ -21,7 +21,6 @@ class UserModel extends BaseModel {
     public function auth($userName, $password) {
         $md5Password = $password;
         $sql = 'SELECT * FROM users WHERE name = "' . $userName . '" AND password = "'.$md5Password.'"';
-
         $user = $this->select($sql);
         return $user;
     }
