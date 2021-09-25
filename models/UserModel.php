30ea80c64fd03f87d47857c2a8f3d10afeeb5c8f
Nguyen Van Anh <{nguyenvananh231001@gmail.com}>
2021-09-25 17:49:43 +0700

    update 2

--- models/UserModel.php
+++ models/UserModel.php
@@ -45,6 +45,9 @@ class UserModel extends BaseModel {
     public function updateUser($input) {
         $sql = 'UPDATE users SET 
                  name = "' . $input['name'] .'", 
+                 fullname = "' . $input['fullname'] .'", 
+                 email = "' . $input['email'] .'", 
+                 type = "' . $input['type'] .'", 
                  password="'. md5($input['password']) .'"
                 WHERE id = ' . $input['id'];
         $user = $this->update($sql);
@@ -71,7 +74,7 @@ class UserModel extends BaseModel {
         if (!empty($params['keyword'])) {
             $sql = 'SELECT * FROM users WHERE name LIKE "%' . $params['keyword'] .'%"';
         } else {
-            $sql = 'SELECT * FROM users join types on users.type = types.id';
+            $sql = 'SELECT * FROM users join types on users.type = types.id_type';
         }
 
         $users = $this->select($sql);
