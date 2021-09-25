3dd39e1bebfbc8721dc53306903889ad5362bf39
sangtruong0317 <truonggioisang.tdc2019@gmail.com>
2021-09-25 16:09:33 +0700

    add email

--- form_user.php
+++ form_user.php
@@ -29,31 +29,38 @@ if (!empty($_POST['submit'])) {
     <?php include 'views/meta.php' ?>
 </head>
 <body>
-    <?php include 'views/header.php'?>
-    <div class="container">
+<?php include 'views/header.php' ?>
+<div class="container">
 
-            <?php if ($user || empty($id)) { ?>
-                <div class="alert alert-warning" role="alert">
-                    User form
-                </div>
-                <form method="POST">
-                    <input type="hidden" name="id" value="<?php echo $id ?>">
-                    <div class="form-group">
-                        <label for="name">Name</label>
-                        <input class="form-control" name="name" placeholder="Name" value="<?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?>">
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" class="form-control" placeholder="Password">
-                    </div>
+    <?php if ($user || empty($id)) { ?>
+        <div class="alert alert-warning" role="alert">
+            User form
+        </div>
+        <form method="POST">
+            <input type="hidden" name="id" value="<?php echo $id ?>">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input class="form-control" name="name" placeholder="Name"
+                       value="<?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?>">
+            </div>
 
-                    <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
-                </form>
-            <?php } else { ?>
-                <div class="alert alert-success" role="alert">
-                    User not found!
-                </div>
-            <?php } ?>
-    </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control" placeholder="Password">
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input class="form-control" name="email" placeholder="Email"
+                       value="<?php if (!empty($user[0]['email'])) echo $user[0]['email'] ?>">
+            </div>
+
+            <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
+        </form>
+    <?php } else { ?>
+        <div class="alert alert-success" role="alert">
+            User not found!
+        </div>
+    <?php } ?>
+</div>
 </body>
 </html>
