80418f3df914c3ea513c93752a5c5f244f5b636c
Van Lam <levanlam3447@gmail.com>
2021-09-25 16:16:51 +0700

    Fix function update

--- models/UserModel.php
+++ models/UserModel.php
@@ -45,6 +45,9 @@ class UserModel extends BaseModel {
     public function updateUser($input) {
         $sql = 'UPDATE users SET 
                  name = "' . $input['name'] .'", 
+                 fullname = "' . $input['fullname'] .'", 
+                 email = "' . $input['email'] .'", 
+                 type = "' . $input['type'] .'", 
                  password="'. md5($input['password']) .'"
                 WHERE id = ' . $input['id'];
         $user = $this->update($sql);
