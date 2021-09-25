73161e6323e328d597060544db05b6c84a7ba4b6
Longkunz <cuvip1325@gmail.com>
2021-09-25 14:25:32 +0700

    nothings

--- form_user.php
+++ form_user.php
@@ -67,6 +67,10 @@ if (!empty($_POST['submit'])) {
                     <input class="form-control" name="name" placeholder="Name" value="<?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?>">
                 </div>
                 <div class="form-group">
+                    <label for="fullname">Full Name</label>
+                    <input class="form-control" name="fullname" placeholder="Full Name" value="<?php if (!empty($user[0]['fullname'])) echo $user[0]['fullname'] ?>">
+                </div>
+                <div class="form-group">
                     <label for="password">Password</label>
                     <input type="password" name="password" class="form-control" placeholder="Password">
                 </div>
