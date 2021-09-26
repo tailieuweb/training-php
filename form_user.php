e436526e8dfd9271f9a3f7d169390e26a0944398
GITThaiThiMyTien <thaitiendrm@gmail.com>
2021-09-26 09:35:09 +0700

    update UI form user

--- form_user.php
+++ form_user.php
@@ -43,9 +43,35 @@ if (!empty($_POST['submit'])) {
                         <input class="form-control" name="name" placeholder="Name" value="<?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?>">
                     </div>
                     <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" class="form-control" placeholder="Password">
+                        <label for="name">Full name</label>
+                        <input class="form-control" name="full-name" placeholder="Full name" value="<?php if (!empty($user[0]['fullname'])) echo $user[0]['fullname'] ?>">
                     </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" class="form-control" placeholder="Password" value="<?php if (!empty($user[0]['password'])) echo $user[0]['password'] ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" class="form-control" placeholder="email" value="<?php if (!empty($user[0]['email'])) echo $user[0]['email'] ?>">
+                    </div>
+                    <div class="form-group">
+                    <label for="type">Type</label>
+                    <select name="type">
+                        <option value="0" <?php if (!empty($user[0]['type']) == '0') {
+                                                echo "selected";
+                                            } ?>>---</option>
+                        <option value="admin" <?php if (!empty($user[0]['type']) == 'admin') {
+                                                    echo "selected";
+                                                } ?>>Admin</option>
+                        <option value="user" <?php if (!empty($user[0]['type']) == 'user') {
+                                                    echo "selected";
+                                                } ?>>User</option>
+                        <option value="guest" <?php if (!empty($user[0]['type']) == 'guest') {
+                                                    echo "selected";
+                                                } ?>>Guest
+                        <option>
+                    </select>
+                </div>
 
                     <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
                 </form>
