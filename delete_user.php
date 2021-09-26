55fa199b3721c0d6bfe8e174de6dfe4b7ce35b56
tranthuong021001 <tranthuong021001@gmail.com>
2021-09-26 07:32:49 +0700

    hoan thanh sua loi bao mat chuc nang xoa user

--- delete_user.php
+++ delete_user.php
@@ -5,9 +5,14 @@ $userModel = new UserModel();
 $user = NULL; //Add new user
 $id = NULL;
 
+$user_arr = $userModel->getUsers();
 if (!empty($_GET['id'])) {
     $id = $_GET['id'];
-    $userModel->deleteUserById($id);//Delete existing user
+    foreach($user_arr as $user){
+        if($id === md5($user['id'])){
+            $userModel->deleteUserById($user['id']);//Delete existing user
+        }
+    }
 }
 header('location: list_users.php');
 ?>
