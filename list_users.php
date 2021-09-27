50463ef0d6b2dafb233db97bd63841b6349e9cf3
Việt Hoàng <tieutam1510@gmail.com>
2021-09-27 20:25:47 +0700

    update file list_user.php

--- list_users.php
+++ list_users.php
@@ -1,7 +1,8 @@
 <?php
 require_once 'models/UserModel.php';
 $userModel = new UserModel();
-
+$key = "kksraw2fd4g";
+var_dump($key);
 $users = $userModel->getUsers();
 ?>
 <!DOCTYPE html>
@@ -41,13 +42,13 @@ $users = $userModel->getUsers();
                                 <?php echo $user['type']?>
                             </td>
                             <td>
-                                <a href="form_user.php?id=<?php echo $user['id'] ?>">
+                                <a href="form_user.php?id=<?php echo base64_encode($key.$user['id'])?>">
                                     <i class="fa fa-pencil-square-o" aria-hidden="true" title="Update"></i>
                                 </a>
-                                <a href="view_user.php?id=<?php echo $user['id'] ?>">
+                                <a href="view_user.php?id=<?php echo base64_encode($key.$user['id']) ?>">
                                     <i class="fa fa-eye" aria-hidden="true" title="View"></i>
                                 </a>
-                                <a href="delete_user.php?id=<?php echo $user['id'] ?>">
+                                <a href="delete_user.php?id=<?php echo base64_encode($key.$user['id']) ?>">
                                     <i class="fa fa-eraser" aria-hidden="true" title="Delete"></i>
                                 </a>
                             </td>
