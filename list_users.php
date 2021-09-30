1404e3e313945f556cfe55a81525cb7e9f98f465
VoDangVinh <01686228222vinh@gmail.com>
2021-09-30 13:05:24 +0700

    update get ip address

--- list_users.php
+++ list_users.php
@@ -8,7 +8,23 @@ $params = [];
 if (!empty($_GET['keyword'])) {
     $params['keyword'] = $_GET['keyword'];
 }
-
+function getIPAddress() {  
+    //whether ip is from the share internet  
+     if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
+                $ip = $_SERVER['HTTP_CLIENT_IP'];  
+        }  
+    //whether ip is from the proxy  
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+     }  
+//whether ip is from the remote address  
+    else{  
+             $ip = $_SERVER['REMOTE_ADDR'];  
+     }  
+     return $ip;  
+}  
+$ip = getIPAddress();  
+echo 'User Real IP Address - '.$ip;  
 $users = $userModel->getUsers($params);
 ?>
 <!DOCTYPE html>
