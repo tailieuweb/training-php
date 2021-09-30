dcce372cc04107b256d9bdfce2f2257e49c97600
nhquyen <nguyenhuuquyen2001@gmail.com>
2021-09-30 14:52:34 +0700

    update

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -22,13 +22,10 @@ abstract class BaseModel
      * Query in database
      * @param $sql
      */
-<<<<<<< HEAD
+
     protected function query($sql)
     {
-=======
-    protected function query($sql) {
 
->>>>>>> 1-php-202109/1-master
         $result = self::$_connection->query($sql);
         return $result;
     }
