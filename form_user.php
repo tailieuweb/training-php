3759788d6eb35241e544aeddb90b5d37771af62e
sangtruong0317 <truonggioisang.tdc2019@gmail.com>
2021-09-30 13:50:25 +0700

    conflict code

--- form_user.php
+++ form_user.php
@@ -8,13 +8,11 @@ $user = NULL; //Add new user
 $_id = NULL;
 
 if (!empty($_GET['id'])) {
-<<<<<<< HEAD
+
     $id = $_GET['id'];
     $user = $userModel->findUserById($id); //Update existing user
-=======
-    $_id = $_GET['id'];
-    $user = $userModel->findUserById($_id);//Update existing user
->>>>>>> 1-php-202109/1-master
+
+
 }
 
 
@@ -38,65 +36,6 @@ if (!empty($_POST['submit'])) {
 </head>
 
 <body>
-<<<<<<< HEAD
-
-<?php include 'views/header.php' ?>
-<div class="container">
-
-    <?php if ($user || empty($id)) { ?>
-        <div class="alert alert-warning" role="alert">
-            User form
-        </div>
-        <form method="POST">
-            <input type="hidden" name="id" value="<?php echo $id ?>">
-            <div class="form-group">
-                <label for="name">Name</label>
-                <input class="form-control" name="name" placeholder="Name"
-                       value="<?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?>">
-            </div>
-            <div class="form-group">
-                <label for="name">Full Name</label>
-                <input class="form-control" name="fullname" placeholder="Full Name" value="<?php if (!empty($user[0]['fullname'])) echo $user[0]['fullname'] ?>" required>
-            </div>
-
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" name="password" class="form-control" placeholder="Password">
-            </div>
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input class="form-control" name="email" placeholder="Email"
-                       value="<?php if (!empty($user[0]['email'])) echo $user[0]['email'] ?>">
-            </div>
-            <div class="form-group">
-                <label for="type">Type</label>
-                <select class="form-control" name="type">
-                    <option value="admin" <?php if (!empty($user[0]['type'])){
-                        if ($user[0]['type'] == 'admin'){
-                            echo "selected";
-                        }}?>>Admin</option>
-                    <option value="user" <?php if (!empty($user[0]['type'])){
-                        if ($user[0]['type'] == 'user'){
-                            echo "selected";
-                        }}?>>User</option>
-                    <option value="guest" <?php if (!empty($user[0]['type'])){
-                        if ($user[0]['type'] == 'guest'){
-                            echo "selected";
-                        }}?>>Guest</option>
-                </select>
-            </div>
-
-            <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
-        </form>
-    <?php } else { ?>
-        <div class="alert alert-success" role="alert">
-            User not found!
-        </div>
-    <?php } ?>
-</div>
-
-
-=======
     <?php include 'views/header.php'?>
     <div class="container">
 
@@ -123,7 +62,6 @@ if (!empty($_POST['submit'])) {
                 </div>
             <?php } ?>
     </div>
->>>>>>> 1-php-202109/1-master
 </body>
 
 </html>
