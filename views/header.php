9878384c5d702ea3c0161484743f314fbbc60e6f
tailieuweb.com <trongtinh2k@gmail.com>
2021-10-01 12:23:31 +0700

    update

--- views/header.php
+++ views/header.php
@@ -32,8 +32,8 @@
                         <ul class="dropdown-menu">
                             <li><a href="#">Profile</a></li>
                             <li role="separator" class="divider"></li>
-                            <li><a href="#">Login</a></li>
-                            <li><a href="#">Logout</a></li>
+                            <li><a href="login.php">Login</a></li>
+                            <li><a href="login.php">Logout</a></li>
                         </ul>
                     </li>
                 </ul>
