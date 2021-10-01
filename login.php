f12128e7afff76867e5f758068f54cff61722541
Hoang Vu <phanvu000666@gmail.com>
2021-10-01 14:16:10 +0700

    update

--- login.php
+++ login.php
@@ -1,8 +1,10 @@
 <?php
+// Start the session
 session_start();
 
 require_once 'models/UserModel.php';
 $userModel = new UserModel();
+
 
 if (!empty($_POST['submit'])) {
     $users = [
@@ -11,32 +13,37 @@ if (!empty($_POST['submit'])) {
     ];
     $user = NULL;
     if ($user = $userModel->auth($users['username'], $users['password'])) {
-        //Login
-        $_SESSION['id'] = $users[0]['id'];
+        //Login successful
+        $_SESSION['id'] = $user[0]['id'];
 
         $_SESSION['message'] = 'Login successful';
         header('location: list_users.php');
-    } else {
+    }else {
         //Login failed
-        $_SESSION['message'] = 'Login failedddd';
+        $_SESSION['message'] = 'Login failed';
     }
+
 }
+
 ?>
 <!DOCTYPE html>
 <html>
-
+<head>
+    <title>User form</title>
+    <?php include 'views/meta.php' ?>
+</head>
 <body>
-    <?php include 'views/header.php' ?>
+<?php include 'views/header.php'?>
 
     <div class="container">
         <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-            <div class="panel panel-info">
+            <div class="panel panel-info" >
                 <div class="panel-heading">
                     <div class="panel-title">Login</div>
                     <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Forgot password?</a></div>
                 </div>
 
-                <div style="padding-top:30px" class="panel-body">
+                <div style="padding-top:30px" class="panel-body" >
                     <form method="post" class="form-horizontal" role="form">
 
                         <div class="margin-bottom-25 input-group">
@@ -64,10 +71,10 @@ if (!empty($_POST['submit'])) {
 
                         <div class="form-group">
                             <div class="col-md-12 control">
-                                Don't have an account!
-                                <a href="form_user.php">
-                                    Sign Up Here!
-                                </a>
+                                    Don't have an account!
+                                    <a href="form_user.php">
+                                        Sign Up Here
+                                    </a>
                             </div>
                         </div>
                     </form>
@@ -76,7 +83,5 @@ if (!empty($_POST['submit'])) {
         </div>
     </div>
 
-    >>>>>>> 1-php-202109/1-web-security
 </body>
-
 </html>
