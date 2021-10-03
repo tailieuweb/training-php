e907e13a9400ba08bc2f663914a7ae3d9cfc5018
leanhvu21042001@gmail.com <leanhvu1412>
2021-10-03 10:17:07 +0700

    Done feature insert user

--- README.md
+++ README.md
@@ -10,3 +10,4 @@
 - Cong Viec:
 - [ ] Loi bao mat IDOR (Insecure Direct Object References)
   - [x] xoa nguoi dung. 
+  - [x] them nguoi dung. 
--- models/UserModel.php
+++ models/UserModel.php
@@ -2,17 +2,20 @@
 
 require_once 'BaseModel.php';
 
-class UserModel extends BaseModel {
+class UserModel extends BaseModel
+{
 
-    public function findUserById($id) {
-        $sql = 'SELECT * FROM users WHERE id = '.$id;
+    public function findUserById($id)
+    {
+        $sql = 'SELECT * FROM users WHERE id = ' . $id;
         $user = $this->select($sql);
 
         return $user;
     }
 
-    public function findUser($keyword) {
-        $sql = 'SELECT * FROM users WHERE user_name LIKE %'.$keyword.'%'. ' OR user_email LIKE %'.$keyword.'%';
+    public function findUser($keyword)
+    {
+        $sql = 'SELECT * FROM users WHERE user_name LIKE %' . $keyword . '%' . ' OR user_email LIKE %' . $keyword . '%';
         $user = $this->select($sql);
 
         return $user;
@@ -24,9 +27,10 @@ class UserModel extends BaseModel {
      * @param $password
      * @return array
      */
-    public function auth($userName, $password) {
+    public function auth($userName, $password)
+    {
         $md5Password = md5($password);
-        $sql = 'SELECT * FROM users WHERE name = "' . $userName . '" AND password = "'.$md5Password.'"';
+        $sql = 'SELECT * FROM users WHERE name = "' . $userName . '" AND password = "' . $md5Password . '"';
 
         $user = $this->select($sql);
         return $user;
@@ -37,7 +41,8 @@ class UserModel extends BaseModel {
      * @param $id
      * @return mixed
      */
-    public function deleteUserUUById($uuid) {
+    public function deleteUserUUById($uuid)
+    {
         $sql = "DELETE FROM users WHERE uuid = '$uuid'; ";
         return $this->delete($sql);
     }
@@ -47,10 +52,11 @@ class UserModel extends BaseModel {
      * @param $input
      * @return mixed
      */
-    public function updateUser($input) {
+    public function updateUser($input)
+    {
         $sql = 'UPDATE users SET 
-                 name = "' . $input['name'] .'", 
-                 password="'. md5($input['password']) .'"
+                 name = "' . $input['name'] . '", 
+                 password="' . md5($input['password']) . '"
                 WHERE id = ' . $input['id'];
         $user = $this->update($sql);
 
@@ -62,9 +68,16 @@ class UserModel extends BaseModel {
      * @param $input
      * @return mixed
      */
-    public function insertUser($input) {
-        $sql = "INSERT INTO `app_web1`.`users` (`name`, `password`) VALUES (" .
-                "'" . $input['name'] . "', '".md5($input['password'])."')";
+    public function insertUser($input)
+    {
+        $name = $input['name'];
+        $fullname = $input['fullname'];
+        $email = $input['email'];
+        $type = $input['type'];
+        $password = md5($input['password']);
+        $uuid = md5($name . $fullname . $email . $type . $password);
+        $sql = "INSERT INTO `users` (`uuid`, `name`, `fullname`, `email`, `type`, `password`)
+                VALUES ('$uuid', '$name ', '$fullname', '$email', '$type', '$password');";
 
         $user = $this->insert($sql);
 
@@ -76,10 +89,11 @@ class UserModel extends BaseModel {
      * @param array $params
      * @return array
      */
-    public function getUsers($params = []) {
+    public function getUsers($params = [])
+    {
         //Keyword
         if (!empty($params['keyword'])) {
-            $sql = 'SELECT * FROM users WHERE name LIKE "%' . $params['keyword'] .'%"';
+            $sql = 'SELECT * FROM users WHERE name LIKE "%' . $params['keyword'] . '%"';
 
             //Keep this line to use Sql Injection
             //Don't change
