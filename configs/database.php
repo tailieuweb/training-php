b8a207252f276bd70e936ce22109cd6c40c23f18
duc622 <duc.phan622@gmail.com>
2021-10-07 22:44:35 +0700

    add docker to src code

--- configs/database.php
+++ configs/database.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
+define('DB_HOST', 'db');
+define('DB_USER', 'MYSQL_USER');
+define('DB_PASSWORD', 'MYSQL_PASSWORD');
 define('DB_PORT', 3306);
-define('DB_NAME', 'app_web1');
+define('DB_NAME', 'MYSQL_DATABASE');
