9cf1974dc1757ac8aff2c6fede6a3480f3432804
Hoang Vu <phanvu000666@gmail.com>
2021-10-08 15:05:38 +0700

    Update

--- form_user.php
+++ form_user.php
@@ -1,19 +1,28 @@
 <?php
+// Start the session
+session_start();
 require_once 'models/UserModel.php';
 $userModel = new UserModel();
 
 $user = NULL; //Add new user
-$id = NULL;
+$_id = NULL;
 
 if (!empty($_GET['id'])) {
-    $id = $_GET['id'];
-    $user = $userModel->findUserById($id);//Update existing user
+    $_id = $_GET['id'];
+    //Xử lý chuỗi đầu
+    $string_first = substr($_id, 0, 4);
+    //Xử lý chuỗi sau
+    $string_last = substr($_id, -4);
+    //Thay thể chuỗi đầu = null
+    $_id = str_replace($string_first, "", $_id);
+    //Thay thế chuỗi sau = null
+    $_id = str_replace($string_last, "", $_id);
+    var_dump($_id);
+    $user = $userModel->findUserById($_id);
+     
 }
-
-
 if (!empty($_POST['submit'])) {
-
-    if (!empty($id)) {
+    if (!empty($_id)) {
         $userModel->updateUser($_POST);
     } else {
         $userModel->insertUser($_POST);
@@ -24,36 +33,69 @@ if (!empty($_POST['submit'])) {
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>User form</title>
     <?php include 'views/meta.php' ?>
 </head>
+
 <body>
-    <?php include 'views/header.php'?>
+    <?php include 'views/header.php' ?>
     <div class="container">
 
-            <?php if ($user || empty($id)) { ?>
-                <div class="alert alert-warning" role="alert">
-                    User form
-                </div>
-                <form method="POST">
-                    <input type="hidden" name="id" value="<?php echo $id ?>">
-                    <div class="form-group">
-                        <label for="name">Name</label>
-                        <input class="form-control" name="name" placeholder="Name" value="<?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?>">
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" class="form-control" placeholder="Password">
-                    </div>
+        <?php if ($user || empty($_id)) { ?>
+        <div class="alert alert-warning" role="alert">
+            User form
+        </div>
+        <form method="POST">
+            <input type="hidden" name="id" value="<?php echo $_id ?>">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input class="form-control" name="name" placeholder="Name"
+                    value="<?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?>">
+            </div>
+            <div class="form-group">
+                <label for="full-name">Full name</label>
+                <input class="form-control" name="full-name" placeholder="Full name"
+                    value="<?php if (!empty($user[0]['fullname'])) echo $user[0]['fullname'] ?>">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control" placeholder="Password"
+                    value="<?php if (!empty($user[0]['password'])) echo $user[0]['password'] ?>">
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" name="email" class="form-control" placeholder="email"
+                    value="<?php if (!empty($user[0]['email'])) echo $user[0]['email'] ?>">
+            </div>
+            <div class="form-group">
+                <label for="type">Type</label>
+                <select name="type">
+                    <option value="0" <?php if (!empty($user[0]['type']) == '0') {
+                                                echo "selected";
+                                            } ?>>---</option>
+                    <option value="admin" <?php if (!empty($user[0]['type']) == 'admin') {
+                                                    echo "selected";
+                                                } ?>>Admin</option>
+                    <option value="user" <?php if (!empty($user[0]['type']) == 'user') {
+                                                    echo "selected";
+                                                } ?>>User</option>
+                    <option value="guest" <?php if (!empty($user[0]['type']) == 'guest') {
+                                                    echo "selected";
+                                                } ?>>Guest
+                    <option>
+                </select>
+            </div>
 
-                    <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
-                </form>
-            <?php } else { ?>
-                <div class="alert alert-success" role="alert">
-                    User not found!
-                </div>
-            <?php } ?>
+            <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
+        </form>
+        <?php } else { ?>
+        <div class="alert alert-success" role="alert">
+            User not found!
+        </div>
+        <?php } ?>
     </div>
 </body>
+
 </html>
--- list_users.php
+++ list_users.php
@@ -1,65 +1,86 @@
 <?php
+// Start the session
+session_start();
+
 require_once 'models/UserModel.php';
 $userModel = new UserModel();
 
-$users = $userModel->getUsers();
+$params = [];
+if (!empty($_GET['keyword'])) {
+    $params['keyword'] = $_GET['keyword'];
+}
+
+$users = $userModel->getUsers($params);
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Home</title>
     <?php include 'views/meta.php' ?>
 </head>
+
 <body>
-    <?php include 'views/header.php'?>
-    <div class="container">
-        <?php if (!empty($users)) {?>
-            <div class="alert alert-warning" role="alert">
-                List of users!
-            </div>
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Username</th>
-                        <th scope="col">Fullname</th>
-                        <th scope="col">Type</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($users as $user) {?>
-                        <tr>
-                            <th scope="row"><?php echo $user['id']?></th>
-                            <td>
-                                <?php echo $user['name']?>
-                            </td>
-                            <td>
-                                <?php echo $user['fullname']?>
-                            </td>
-                            <td>
-                                <?php echo $user['type']?>
-                            </td>
-                            <td>
-                                <a href="form_user.php?id=<?php echo $user['id'] ?>">
-                                    <i class="fa fa-pencil-square-o" aria-hidden="true" title="Update"></i>
-                                </a>
-                                <a href="view_user.php?id=<?php echo $user['id'] ?>">
-                                    <i class="fa fa-eye" aria-hidden="true" title="View"></i>
-                                </a>
-                                <a href="delete_user.php?id=<?php echo $user['id'] ?>">
-                                    <i class="fa fa-eraser" aria-hidden="true" title="Delete"></i>
-                                </a>
-                            </td>
-                        </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
-        <?php }else { ?>
-            <div class="alert alert-dark" role="alert">
-                This is a dark alert—check it out!
-            </div>
+    <?php include 'views/header.php' ?>
+    <div class=" container">
+        <?php if (!empty($users)) { ?>
+        <div class="alert alert-warning" role="alert">
+            List of users! <br>
+            Hacker: http://php.local/list_users.php?keyword=ASDF%25%22%3BTRUNCATE+banks%3B%23%23
+        </div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Username</th>
+                    <th scope="col">Fullname</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($users as $user) { ?>
+                <tr>
+                    <!-- Phân tách thông tin -->
+                    <!-- Sử dùng hàm htmlentities đễ mã hóa các ký tự có khả năng thực thi javascript trước khi lưu trữ -->
+                    <th scope="row"><?php echo htmlentities($user['id']) ?></th>
+                    <td>
+                        <?php echo htmlentities($user['name']) ?>
+                    </td>
+                    <td>
+                        <?php echo htmlentities($user['fullname']) ?>
+                    </td>
+                    <td>
+                        <?php echo htmlentities($user['email']) ?>
+                    </td>
+                    <td>
+                        <?php echo htmlentities($user['type']) ?>
+                    </td>
+                    <td>
+                        <?php
+                            $min = 1000;
+                            $max = 9999;
+                        ?>
+                        <a href="form_user.php?id=<?= mt_rand($min , $max) . $user['id'] . mt_rand($min , $max) ?>">
+                            <i class="fa fa-pencil-square-o" aria-hidden="true" title="Update"></i> 
+                        </a>
+                        <a href="view_user.php?id=<?= mt_rand($min , $max) . $user['id'] . mt_rand($min , $max) ?>">
+                            <i class="fa fa-eye" aria-hidden="true" title="View"></i>
+                        </a>
+                        <a href="delete_user.php?id=<?= mt_rand($min , $max) . $user['id'] . mt_rand($min , $max) ?>">
+                            <i class="fa fa-eraser" aria-hidden="true" title="Delete"></i>
+                        </a>
+                    </td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+        <?php } else { ?>
+        <div class="alert alert-dark" role="alert">
+            This is a dark alert—check it out!
+        </div>
         <?php } ?>
     </div>
 </body>
+
 </html>
