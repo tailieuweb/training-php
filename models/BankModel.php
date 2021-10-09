6b165296ad74f3348515868922f12faa9c2e9b0b
Longkunz <cuvip1325@gmail.com>
2021-10-09 15:38:13 +0700

    Bank list view

--- models/BankModel.php
+++ models/BankModel.php
@@ -4,4 +4,15 @@ require_once 'BaseModel.php';
 
 class BankModel extends BaseModel
 {
+    /**
+     * Get bank account
+     * @param array $params
+     * @return array
+     */
+    public function getBank($params = [])
+    {
+        $sql = "SELECT * FROM `banks` INNER JOIN users ON banks.user_id=users.id";
+        $banks = $this->select($sql);
+        return $banks;
+    }
 }
