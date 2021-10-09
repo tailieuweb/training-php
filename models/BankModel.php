ec50326e2dc782531ab361b5889b437db7bd039a
Vo Thanh Dat <vothanhdat.tdc2019@gmail.com>
2021-10-09 15:36:49 +0700

    fix error add bank

--- models/BankModel.php
+++ models/BankModel.php
@@ -18,27 +18,7 @@ class BankModel extends BaseModel {
         return $bank;
     }
 
-     /**
-     * Search banks
-     * @param array $params
-     * @return array
-     */
-    public function getBanks($params = []) {
-        //Keyword
-        if (!empty($params['keyword'])) {
-            $sql = 'SELECT * FROM banks WHERE name LIKE "%' . $params['keyword'] .'%"';
-
-            //Keep this line to use Sql Injection
-            //Don't change
-            //Example keyword: abcef%";TRUNCATE banks;##
-            $banks = self::$_connection->multi_query($sql);
-        } else {
-            $sql = 'SELECT * FROM banks';
-            $banks = $this->select($sql);
-        }
-
-        return $banks;
-    }
+    
 	/**
      * Delete bank by id
      * @param $id
