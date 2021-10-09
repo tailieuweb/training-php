0af1a3a6a4fe6ad647c6c91368f8365cfac18bf3
Longkunz <cuvip1325@gmail.com>
2021-10-09 15:40:25 +0700

    Delete bank account balance function

--- models/BankModel.php
+++ models/BankModel.php
@@ -5,6 +5,16 @@ require_once 'BaseModel.php';
 class BankModel extends BaseModel
 {
     /**
+     * Delete bank account balance
+     * @param $id
+     * @return mixed
+     */
+    public function deleteBalanceById($id)
+    {
+        $sql = 'UPDATE `banks` SET `cost`="0" WHERE `user_id` ='  . $id;
+        return $this->update($sql);
+    }
+    /**
      * Get bank account
      * @param array $params
      * @return array
