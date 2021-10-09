6641082febe835a654d347b94cb6d720cce83587
Vo Thanh Dat <vothanhdat.tdc2019@gmail.com>
2021-10-09 13:34:29 +0700

    normal

--- list_users.php
+++ list_users.php
@@ -22,52 +22,52 @@ $users = $userModel->getUsers($params);
     <?php include 'views/header.php'?>
     <div class="container">
         <?php if (!empty($users)) {?>
-            <div class="alert alert-warning" role="alert">
-                List of users! <br>
-                Hacker: http://php.local/list_users.php?keyword=ASDF%25%22%3BTRUNCATE+banks%3B%23%23
-            </div>
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Username</th>
-                        <th scope="col">Fullname</th>
-                        <th scope="col">Type</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($users as $user) {?>
-                        <tr>
-                            <th scope="row"><?php echo $user['id']?></th>
-                            <td>
-                                <?php echo $user['name']?>
-                            </td>
-                            <td>
-                                <?php echo $user['fullname']?>
-                            </td>
-                            <td>
-                                <?php echo $user['type']?>
-                            </td>
-                            <td>
-                                <a href="form_user.php?id=<?php echo md5($user['id'].'TeamJ-TDC') ?>">
-                                    <i class="fa fa-pencil-square-o" aria-hidden="true" title="Update"></i>
-                                </a>
-                                <a href="view_user.php?id=<?php echo md5($user['id'].'TeamJ-TDC') ?>">
-                                    <i class="fa fa-eye" aria-hidden="true" title="View"></i>
-                                </a>
-                                <a href="delete_user.php?id=<?php echo md5($user['id'].'TeamJ-TDC') ?>">
-                                    <i class="fa fa-eraser" aria-hidden="true" title="Delete"></i>
-                                </a>
-                            </td>
-                        </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
+        <div class="alert alert-warning" role="alert">
+            List of users! <br>
+            Hacker: http://php.local/list_users.php?keyword=ASDF%25%22%3BTRUNCATE+banks%3B%23%23
+        </div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Username</th>
+                    <th scope="col">Fullname</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($users as $user) {?>
+                <tr>
+                    <th scope="row"><?php echo $user['id']?></th>
+                    <td>
+                        <?php echo $user['name']?>
+                    </td>
+                    <td>
+                        <?php echo $user['fullname']?>
+                    </td>
+                    <td>
+                        <?php echo $user['type']?>
+                    </td>
+                    <td>
+                        <a href="form_user.php?id=<?php echo md5($user['id'].'TeamJ-TDC') ?>">
+                            <i class="fa fa-pencil-square-o" aria-hidden="true" title="Update"></i>
+                        </a>
+                        <a href="view_user.php?id=<?php echo md5($user['id'].'TeamJ-TDC') ?>">
+                            <i class="fa fa-eye" aria-hidden="true" title="View"></i>
+                        </a>
+                        <a href="x?id=<?php echo md5($user['id'].'TeamJ-TDC') ?>">
+                            <i class="fa fa-eraser" aria-hidden="true" title="Delete"></i>
+                        </a>
+                    </td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
         <?php }else { ?>
-            <div class="alert alert-dark" role="alert">
-                This is a dark alert—check it out!
-            </div>
+        <div class="alert alert-dark" role="alert">
+            This is a dark alert—check it out!
+        </div>
         <?php } ?>
     </div>
 </body>
