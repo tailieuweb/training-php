0c5aba1f15f40cb7e1f47fdb81cd180660e95640
Longkunz <cuvip1325@gmail.com>
2021-10-09 15:36:17 +0700

    Bank model init & define in FactoryPattern.php

---
+++ models/BankModel.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'BaseModel.php';
+
+class BankModel extends BaseModel
+{
+}
--- models/FactoryPattern.php
+++ models/FactoryPattern.php
@@ -1,13 +1,15 @@
 <?php
 require_once 'models/UserModel.php';
-class FactoryPattern {
+require_once 'models/BankModel.php';
+class FactoryPattern
+{
 
-    public function make($model) {
+    public function make($model)
+    {
         if ($model == 'user') {
             return new UserModel();
         } else if ($model == 'bank') {
-            //
+            return new BankModel();
         }
     }
-
 }
