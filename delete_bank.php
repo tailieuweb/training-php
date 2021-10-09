89a1511fc41a59c7422d9d4c5041df1c336f8bbe
leliem523 <dragking005@gmail.com>
2021-10-09 16:07:18 +0700

    update by branch Liem

---
+++ delete_bank.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'models/FactoryPattern.php';
+$factory = new FactoryPattern();
+
+$bankModel = $factory->make('bank');
+
+$id = NULL;
+
+if (!empty($_GET['id'])) {
+    $id = $_GET['id'];
+    //Decode id param
+
+    //Get first number
+    $start = substr($id, 0, 5);
+
+    //Get last number
+    $end = substr($id, -5);
+
+    //Replace first number with null
+    $id = str_replace($start, "", $id);
+
+    //Replace last number with null
+    $id = str_replace($end, "", $id);
+    $bankModel->deleteBalanceById($id);
+}
+header('location: index.php');
