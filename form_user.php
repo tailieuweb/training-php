fd7a49ee0d94aa4a6b0d4425c3ed8db6a04b49dd
huy <dhuy9916@gmail.com>
2021-10-09 15:49:33 +0700

    update form user

--- form_user.php
+++ form_user.php
@@ -1,6 +1,8 @@
 <?php
-require_once 'models/UserModel.php';
-$userModel = new UserModel();
+require_once 'models/FactoryPattern.php';
+$factory = new FactoryPattern();
+
+$userModel = $factory->make('user');
 
 $user = NULL; //Add new user
 
@@ -10,32 +12,19 @@ if (!empty($_GET['id'])) {
     $id = base64_decode($_GET['id']);
     $newid = substr($id,3,-2);
     $user = $userModel->findUserById($newid);//Update existing user 
-
-$id = NULL;
-
-if (!empty($_GET['id'])) {
-    $id = base64_decode($_GET['id']);
-    $newid = substr($id,3,-2);
-    $user = $userModel->findUserById($newid);//Update existing user
-}
 if (!empty($_GET['version'])) {
     $version =  $_GET['version'];   
 }
 function phpAlert($msg) {
     echo '<script type="text/javascript">alert("' . $msg . '")</script>';
 }
-//  && $_SESSION['_token'] == $_POST['_token']
 if (!empty($_POST['submit'])) {
 
     if (!empty($id) && !empty($version)) {     
         if($userModel->getVersion($newid) == $version){           
             $userModel->updateUser($_POST);          
             $userModel->updateVersion($_POST);
-            // var_dump($userModel->getVersion($newid));
-            // var_dump($version);
-         
         }else{
-           // phpAlert(   "dữ liệu vừa được cập nhật vui lòng load lại trang") ;
            echo '<script>alert("Phiên bản đã hết hạn, vui lòng cập nhật")</script>';
 
         }      
@@ -48,32 +37,23 @@ if (!empty($_POST['submit'])) {
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>User form</title>
-    <?php include 'views/meta.php' ?>
-</head>
-<body>
-    <?php include 'views/header.php'?>
+    <head>
+        <title>User form</title>
+        <?php include 'views/meta.php' ?>
+    </head>
+    <body>
+        <?php include 'views/header.php'?>
+        
+        <?php if ($user || !isset($_id)) { ?>
 
-    <div class="container">           
-    <?php if ($user || !isset($_id)) { ?>
-
-    <div class="container">
+        <div class="container">
 
             <?php if ($user || isset($newsid)) { ?>
                 <div class="alert alert-warning" role="alert">
                     User form
                 </div>
-                <form method="POST">
-
-                    <input type="hidden" name="id" value="<?php if(!$id){
-                        echo $newid;}else{ echo $id;
-                        }?>">
+                <form method="POST">                
                     <input type="hidden" name="id" value="<?php if(!empty($newid)){echo $newid;}else{echo $id;}?>">
-                    <div class="form-group">
-                        <label for="name">User Name</label>
-                        <input class="form-control" name="name" placeholder="User Name" value="<?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?>">
-                    </div>
                     <div class="form-group">
                         <label for="name">Name</label>
                         <input class="form-control" name="name" placeholder="Name" value="<?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?>">
@@ -106,7 +86,8 @@ if (!empty($_POST['submit'])) {
                 <div class="alert alert-success" role="alert">
                     User not found!
                 </div>
+                
             <?php } ?>
-    </div>
-</body>
+        </div>
+    </body>
 </html>
