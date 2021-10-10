39a8975e40b9f9a7561cdf2db3910d65ae1d7525
Nguyen Trong Tinh <trongtinh2k@gmail.com>
2021-10-10 09:39:26 +0700

    Add bank

--- views/header.php
+++ views/header.php
@@ -25,7 +25,7 @@ if(!empty($_GET['keyword'])) {
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li><a href="form_user.php">Add new user</a></li>
+                    <li><a href="form_user_bank.php">Add new user</a></li>
 
                 </ul>
                 <form class="navbar-form navbar-left">
