ea5ad294eea8804694223b711081fba4bd3cb19d
Hoang Vu <phanvu000666@gmail.com>
2021-10-10 09:00:21 +0700

    update

--- models/BankModel.php
+++ models/BankModel.php
@@ -2,29 +2,7 @@
 
 require_once 'BaseModel.php';
 
-class BankModel extends BaseModel
+class UserModel extends BaseModel
 {
     
-    /**
-     * Search banks
-     * @param array $params
-     * @return array
-     */
-    public function getBanks($params = [])
-    {
-        //Keyword
-        if (!empty($params['keyword'])) {
-            $sql = 'SELECT * FROM bank WHERE name LIKE "%' . $params['keyword'] . '%"';
-
-            //Keep this line to use Sql Injection
-            //Don't change
-            //Example keyword: abcef%";TRUNCATE banks;##
-            $banks = self::$_connection->multi_query($sql);
-        } else {
-            $sql = 'SELECT * FROM bank';
-            $banks = $this->select($sql);
-        }
-
-        return $banks;
-    }
 }
