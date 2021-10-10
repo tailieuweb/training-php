1dafee538ff695b885b7ccb23bb42eca0cc891f1
BayHen <80869653+BayHen@users.noreply.github.com>
2021-10-10 11:15:11 +0700

    Update insert Bank

--- models/FactoryPattern.php
+++ models/FactoryPattern.php
@@ -1,11 +1,12 @@
 <?php
 require_once 'models/UserModel.php';
+require_once 'models/BankModel.php';
 class FactoryPattern{
     public function make($model){
         if($model=='user'){
             return UserModel::getInstance();
         }elseif($model=='bank'){
-            //return new BankModel();
+            return BankModel::getInstance();
         }
     }
 }
