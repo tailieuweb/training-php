6827763c19fae797673fdb7dc5419384c045e211
hieu294 <tranthehieu12>
2021-10-10 09:08:25 +0700

    update file and password

--- list_users.php
+++ list_users.php
@@ -27,9 +27,9 @@ $users = $userModel->getUsers($params);
                     <tr>
                         <th scope="col">ID</th>
                         <th scope="col">Username</th>
-                        <th scope="col">Fullname</th>
+                       
                         <th scope="col">Mail</th>
-                        <th scope="col">Type</th>
+                      
                         <th scope="col">Actions</th>
                     </tr>
                 </thead>
@@ -40,15 +40,11 @@ $users = $userModel->getUsers($params);
                             <td>
                                 <?php echo $user['name']?>
                             </td>
-                            <td>
-                                <?php echo $user['fullname']?>
-                            </td>
+                            
                             <td>
                                 <?php echo $user['email']?>
                             </td>
-                            <td>
-                                <?php echo $user['type']?>
-                            </td>
+                           
                             <td>
                                 <a href="form_user.php?id=<?php echo $user['id'] ?>">
                                     <i class="fa fa-pencil-square-o" aria-hidden="true" title="Update"></i>
--- models/UserModel.php
+++ models/UserModel.php
@@ -47,7 +47,7 @@ class UserModel extends BaseModel {
                  name = "' . $input['name'] .'", 
                  email ="' . $input['email'] .'",
                  password="'. md5($input['password']) .'"
-                WHERE id = ' . $input['id'];
+                WHERE id = ' . ($input['id']);
         $user = $this->update($sql);
 
         return $user;
@@ -59,8 +59,8 @@ class UserModel extends BaseModel {
      * @return mixed
      */
     public function insertUser($input) {
-        $sql = "INSERT INTO `app_web1`.`users` (`name`, `password`) VALUES (" .
-                "'" . $input['name'] . "', '".$input['password']."')";
+        $sql = "INSERT INTO `app_web1`.`users` (`name`,`email`, `password`) VALUES (" .
+                "'" . $input['name'] . "','".$input['email']."', '".md5($input['password'])."')";
 
         $user = $this->insert($sql);
 
