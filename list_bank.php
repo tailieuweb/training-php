3855176c718abcb53f7a07344528cea8f531b0fb
Trung Nguyen Huynh Quoc <19211tt0744@mail.tdc.edu.vn>
2021-10-10 11:28:25 +0700

    Add bank

---
+++ list_bank.php
@@ -0,0 +1,66 @@
+<?php
+require_once 'models/BankModel.php';
+$bankModel = new BankModel();
+
+$params = [];
+if (!empty($_GET['keyword'])) {
+    $params['keyword'] = $_GET['keyword'];
+}
+
+$users = $bankModel->getBanks($params);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <?php include 'views/meta.php' ?>
+</head>
+<body>
+    <?php include 'views/header.php'?>
+    <div class="container">
+        <?php if (!empty($users)) {?>
+            <div class="alert alert-warning" role="alert">
+                List of Banks!
+            </div>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">User_ID</th>
+                        <th scope="col">Cost</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($users as $user) {?>
+                        <tr>
+                            <th scope="row"><?php echo $user['id']?></th>
+                            <td>
+                                <?php echo $user['user_id']?>
+                            </td>
+                            <td>
+                                <?php echo $user['cost']?>
+                            </td>
+                            <td>
+                                <a href="form_user.php?id=<?php echo $user['id'] ?>">
+                                    <i class="fa fa-pencil-square-o" aria-hidden="true" title="Update"></i>
+                                </a>
+                                <a href="view_bank.php?id=<?php echo $user['id'] ?>">
+                                    <i class="fa fa-eye" aria-hidden="true" title="View"></i>
+                                </a>
+                                <a href="delete_user.php?id=<?php echo $user['id'] ?>">
+                                    <i class="fa fa-eraser" aria-hidden="true" title="Delete"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        <?php }else { ?>
+            <div class="alert alert-dark" role="alert">
+                This is a dark alert—check it out!
+            </div>
+        <?php } ?>
+    </div>
+</body>
+</html>
