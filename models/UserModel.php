d257286c4a399803dde546e071386216d6367f38
sangtm97 <minhsangtrieu94@gmail.com>
2021-10-13 10:10:57 +0700

    update

--- models/UserModel.php
+++ models/UserModel.php
@@ -51,7 +51,7 @@ class UserModel extends BaseModel
     {
         $sql = 'UPDATE users SET 
 
-                 name = "' . mysqli_real_escape_string(self::$_connection, $input['name']) .'", 
+                 name = "' . $input['name'] .'", 
                  password="'. md5($input['password']) .'"
                 WHERE id = ' . $input['id'];
 
