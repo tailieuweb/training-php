95061a8606635829aafcfbaac28544dde7ea7d0d
nhquyen <nguyenhuuquyen2001@gmail.com>
2021-10-14 18:56:58 +0700

    Fix

--- list_users.php
+++ list_users.php
@@ -71,7 +71,7 @@ $users = $userModel->getUsers($params);
                 </tr>
                 <?php } ?>
             </tbody>
-            <div> AHihi</div>
+            <div> AHihi đây không phải quyền</div>
         </table>
         <?php } else { ?>
         <div class="alert alert-dark" role="alert">
