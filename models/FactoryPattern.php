d2d6c881664d960934f25de180269554e6e13820
mrluongdz <vdluong2k.tdc.vn@gmail.com>
2021-10-14 14:57:18 +0700

    comit

--- models/FactoryPattern.php
+++ models/FactoryPattern.php
@@ -1,18 +1,14 @@
 <?php
 require_once 'models/UserModel.php';
+require_once 'models/BankModel.php';
 class FactoryPattern {
 
     public function make($model) {
-        if ($model == 'user') {
-<<<<<<< HEAD
+        if ($model == 'user') 
+        {
             return new UserModel();
         } else if ($model == 'bank') {
-            //
-=======
-            return UserModel::getInstance();
-        } else if ($model == 'bank') {
-            //return new BankModel();
->>>>>>> 1-php-202109/2-groups/11-K/master
+            return new BankModel();
         }
     }
 
