412b0ee9a5a98d937bda7550c47d2b2538d99e76
Ngoc Tam <68436770+TranNgocTam-19211TT2494@users.noreply.github.com>
2021-10-16 23:18:48 +0700

    Update BaseModel.php
    New baseModel

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -4,7 +4,7 @@ require_once 'configs/database.php';
 abstract class BaseModel {
     // Database connection
     protected static $_connection;
-
+    protected static $_instance;
     public function __construct() {
 
         if (!isset(self::$_connection)) {
