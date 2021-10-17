fb15d72f9168338634fe883fc89937e19b0e3c0c
Trong Hieu <tronghieu60s@gmail.com>
2021-10-17 11:33:11 +0700

    Update redux posts

---
+++ src/redux/actions/postsActions.js
@@ -0,0 +1,17 @@
+//Action Types
+export const SET_POSTS = "LOAD_POSTS";
+
+//Action Creator
+export const setPosts = (posts) => ({ type: SET_POSTS, posts });
+
+// Action Load Posts
+export const actLoadPosts = () => {
+  return (dispatch) => {
+    return apiCaller("products", "GET", null).then((res) => {
+      if (res.success) {
+        const postsData = res.data.reverse();
+        dispatch(setPosts(postsData));
+      }
+    });
+  };
+};
