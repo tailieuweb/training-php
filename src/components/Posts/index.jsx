000c8f77513818962ae9ccdf2ac06af21be1661e
Trong Hieu <tronghieu60s@gmail.com>
2021-10-17 11:05:28 +0700

    Update pagination

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,10 +1,17 @@
+import { useRouter } from "next/dist/client/router";
 import { useEffect, useState } from "react";
 import apiCaller from "../../utils/apiCaller";
+import Pagination from "../Base/Pagination";
 import PostsAddForm from "./PostsAddForm";
 import PostsDelete from "./PostsDelete";
 import PostsItem from "./PostsItem";
 
+const ITEM_PER_PAGE = 5;
+
 export default function Posts() {
+  const router = useRouter();
+  const { pageNum } = router.query;
+
   const [posts, setPosts] = useState([]);
   const [postsBase, setPostsBase] = useState([]);
   const [postSelected, setPostSelected] = useState([]);
@@ -13,11 +20,21 @@ export default function Posts() {
     apiCaller("products").then((res) => {
       if (res.success) {
         const postsData = res.data.reverse();
-        setPosts(postsData);
+        setPosts(
+          postsData.splice((pageNum - 1) * ITEM_PER_PAGE, ITEM_PER_PAGE)
+        );
         setPostsBase(postsData);
       }
     });
   }, []);
+
+  useEffect(() => {
+    const postsData = [...postsBase].splice(
+      (pageNum - 1) * ITEM_PER_PAGE,
+      ITEM_PER_PAGE
+    );
+    setPosts(postsData);
+  }, [pageNum]);
 
   const onDeletePost = () => {};
 
@@ -32,6 +49,15 @@ export default function Posts() {
           <PostsItem post={post} onSelectPost={() => setPostSelected(post)} />
         </div>
       ))}
+      <div className="col-md-12">
+        {posts.length > 0 && (
+          <Pagination
+            baseUrl="/posts"
+            maxSize={postsBase.length}
+            itemSize={ITEM_PER_PAGE}
+          />
+        )}
+      </div>
     </div>
   );
 }
