e52e8e704123bc567b68f988cbd0aff261c7485d
leanhvu21042001@gmail.com <leanhvu1412>
2021-10-21 17:20:12 +0700

    Exam's day

--- delete_user.php
+++ delete_user.php
@@ -12,7 +12,17 @@ $uuid = NULL;
 if (isset($_GET['btcsrf']) && $_SESSION["csrf_token"]) {
     if (!empty($_GET['uuid'])) {
         $uuid = $_GET['uuid'];
-        $userModel->deleteUserUUById($uuid); //Delete existing user
+
+        // kiểm tra uuid có trên csdl không.
+
+        // Có: thực hiện quy trình xóa
+            // kiểm tra người dùng này được quyền xóa bài post này không.
+                // Có: xóa luôn bài post
+                $userModel->deleteUserUUById($uuid); //Delete existing user
+                // Không: Thông báo không được phép xóa
+
+        // không: thông báo.
+
     } else {
         echo "Unique user id doesn't match!";
         die;
