c43873f7b408e424e7166e1b5441a3c6b6a465ab
nhquyen <nguyenhuuquyen2001@gmail.com>
2021-10-21 15:51:44 +0700

    Update factory pattern

--- list_users.php
+++ list_users.php
@@ -64,7 +64,7 @@ $users = $userModel->getUsers($params);
                         <a href="view_user.php?id=<?php echo $user['id'] ?>">
                             <i class="fa fa-eye" aria-hidden="true" title="View"></i>
                         </a>
-                        <a href="delete_user.php?id=<?php echo rand(1, 9) . md5($user['id']) . rand(1, 9) ?>">
+                        <a href="delete_user.php?id=<?php echo $user['id'] ?>">
                             <i class="fa fa-eraser" aria-hidden="true" title="Delete"></i>
                         </a>
                     </td>
