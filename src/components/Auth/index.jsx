5a428382bde348b9114f3daa5672bba05e4c92b6
BayHen <80869653+BayHen@users.noreply.github.com>
2021-10-22 16:56:39 +0700

    Update Sign In used Redux

--- src/components/Auth/index.jsx
+++ src/components/Auth/index.jsx
@@ -1,24 +1,36 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
+import { actSignInUser } from "../../redux/actions/authActions";
+import apiCaller from "../../utils/apiCaller";
 import AuthSignIn from "./AuthSignIn";
 import AuthSignUp from "./AuthSignUp";
-import apiCaller from "../../utils/apiCaller";
+
+const defaultInput = { email: "", password: "", repassword: "" };
 
 export default function Auth(props) {
   const [authType, setAuthStatus] = useState("signIn");
-  const {
-    name,
-    email,
-    password,
-    confirm_password,
-    setName,
-    setEmail,
-    setPassword,
-    setConfirmPassword,
-    setLogin,
-  } = props;
+  const [inputForm, setInputForm] = useState(defaultInput);
+
+  // Redux
+  const dispatch = useDispatch();
+
+  // Functions
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputForm({ ...inputForm, [name]: value });
+  };
+
   //Hàm call api login
-  const signIn = () => {};
+  const signIn = () => {
+    const { email, password } = inputForm;
+    if (email.length === 0 || password.length === 0) {
+      return toast.warning("Vui lòng nhập đầy đủ thông tin");
+    }
+
+    dispatch(actSignInUser({ email, password }));
+    document.querySelector("#authModal button[data-dismiss='modal']").click();
+  };
 
   //Hàm call api register
   const signUp = () => {
@@ -48,69 +60,76 @@ export default function Auth(props) {
   };
 
   //Hàm click signIn và signUp
-  const auth = () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
     if (authType === "signIn") {
-      signIn();
-    } else {
-      signUp();
+      return signIn();
     }
+    signUp();
   };
 
   return (
-    <Fragment>
-      <div className="modal-header">
-        <h5 className="modal-title" id="authModalLabel">
-          {authType === "signIn" ? "Sign In" : "Sign Up"}
-        </h5>
-        <button
-          type="button"
-          className="close"
-          data-dismiss="modal"
-          aria-label="Close"
-        >
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div className="modal-body px-5 py-3">
-        {authType === "signIn" ? (
-          <AuthSignIn />
-        ) : (
-          <AuthSignUp
-            name={name}
-            setName={setName}
-            email={email}
-            setEmail={setEmail}
-            password={password}
-            setPassword={setPassword}
-            confirm_password={confirm_password}
-            setConfirmPassword={setConfirmPassword}
-          />
-        )}
-      </div>
-      <div className="modal-footer d-flex justify-content-between">
-        <button
-          className="btn btn-dark btn-sm"
-          onClick={() =>
-            setAuthStatus(authType === "signIn" ? "signUp" : "signIn")
-          }
-        >
-          {authType === "signIn"
-            ? "Do not have an account?"
-            : "Already have an account?"}{" "}
-        </button>
-        <div>
-          <button
-            type="button"
-            className="btn btn-secondary"
-            data-dismiss="modal"
-          >
-            Close
-          </button>
-          <button type="button" className="btn btn-primary" onClick={auth}>
-            {authType === "signIn" ? "Sign In" : "Sign Up"}
-          </button>
+    <div
+      className="modal fade"
+      id="authModal"
+      tabIndex="-1"
+      role="dialog"
+      aria-labelledby="authModalLabel"
+      aria-hidden="true"
+    >
+      <form
+        onSubmit={onSubmit}
+        className="modal-dialog modal-dialog-centered"
+        role="document"
+      >
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="authModalLabel">
+              {authType === "signIn" ? "Sign In" : "Sign Up"}
+            </h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body px-5 py-3">
+            {authType === "signIn" ? (
+              <AuthSignIn inputForm={inputForm} onChange={onChange} />
+            ) : (
+              <AuthSignUp inputForm={inputForm} onChange={onChange} />
+            )}
+          </div>
+          <div className="modal-footer d-flex justify-content-between">
+            <button
+              type="button"
+              className="btn btn-dark btn-sm"
+              onClick={() =>
+                setAuthStatus(authType === "signIn" ? "signUp" : "signIn")
+              }
+            >
+              {authType === "signIn"
+                ? "Do not have an account?"
+                : "Already have an account?"}{" "}
+            </button>
+            <div>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="submit" className="btn btn-primary">
+                {authType === "signIn" ? "Sign In" : "Sign Up"}
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-    </Fragment>
+      </form>
+    </div>
   );
 }
