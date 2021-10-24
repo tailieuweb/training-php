962133412ba0c48d6d417f9e9f9a15fccdc6ad57
leanhvu21042001@gmail.com <leanhvu1412>
2021-10-24 18:03:07 +0700

    Test repository pattern

--- form_user.php
+++ form_user.php
@@ -3,7 +3,7 @@
 session_start();
 
 require_once 'models/FactoryPattern.php';
-    require 'models/Repositories.php';
+require 'models/Repositories.php';
 
 $repositories = new Repositories();
 //
@@ -60,7 +60,7 @@ if (!empty($_POST['submit'])) {
         $repositories->createAUser($_POST);
         header('location: list_users.php');
     }
-    
+
 }
 
 ?>
@@ -73,69 +73,79 @@ if (!empty($_POST['submit'])) {
 </head>
 
 <body>
-    <?php include 'views/header.php' ?>
-    <div class="container">
+<?php include 'views/header.php' ?>
+<div class="container">
 
-        <?php if ($user || !isset($uuid)) { ?>
-            <div class="alert alert-warning" role="alert">
-                User form
+    <?php if ($user || !isset($uuid)) { ?>
+        <div class="alert alert-warning" role="alert">
+            User form
+        </div>
+        <form method="POST">
+            <?php if (!empty($uuid)) : ?>
+                <input type="hidden" name="uuid" value="<?php echo $uuid ?>">
+            <?php endif; ?>
+            <?php if (!empty($user[0]['id'])) : ?>
+                <input type="hidden" name="id"
+                       value="<?php echo rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . strip_tags($user[0]['id']) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) ?>">
+            <?php endif; ?>
+            <?php if (!empty($user[0]['version'])) : ?>
+                <input type="hidden" name="version" value="<?php echo md5($user[0]['version']) ?>">
+            <?php endif; ?>
+            <?php if (!empty($user[0])) : ?>
+                <input type="hidden" name="value_not_change" value="<?php echo base64_encode(json_encode($user[0])) ?>">
+            <?php endif; ?>
+
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input class="form-control" name="name" placeholder="Name"
+                       value="<?php if (!empty($user[0]['name'])) echo strip_tags($user[0]['name']) ?>">
             </div>
-            <form method="POST" >
-                <?php if (!empty($uuid)) : ?>
-                    <input type="hidden" name="uuid" value="<?php echo $uuid ?>">
-                <?php endif; ?>
-                <?php if (!empty($user[0]['id'])) : ?>
-                    <input type="hidden" name="id" value="<?php echo rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . strip_tags($user[0]['id']) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) ?>">
-                <?php endif; ?>
-                <?php if (!empty($user[0]['version'])) : ?>
-                    <input type="hidden" name="version" value="<?php echo md5($user[0]['version']) ?>">
-                <?php endif; ?>
-                <?php if (!empty($user[0])) : ?>
-                    <input type="hidden" name="value_not_change" value="<?php echo base64_encode(json_encode($user[0]))  ?>">
-                <?php endif; ?>
-                
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input class="form-control" name="name" placeholder="Name" value="<?php if (!empty($user[0]['name'])) echo strip_tags($user[0]['name']) ?>">
-                </div>
-                <div class="form-group">
-                    <label for="fullname">Fullname</label>
-                    <input name="fullname" class="form-control" placeholder="Fullname" value="<?php if (!empty($user[0]['fullname'])) echo strip_tags($user[0]['fullname']) ?>">
-                </div>
-
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <!-- <input type="password" name="password" class="form-control" placeholder="password" value="<?php //if (!empty($user[0]['password'])) echo $user[0]['password'] 
-                                                                                                                    ?>"> -->
-                    <input type="password" name="password" class="form-control" placeholder="password" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" name="email" class="form-control" placeholder="Email" value="<?php if (!empty($user[0]['email'])) echo strip_tags($user[0]['email']) ?>">
-                </div>
-                <div class="form-group">
-                    <label for="type">Type</label>
-                    <select name="type" uuid="type" class="form-control">
-
-                        <option value="" selected disabled>Select type</option>
-
-                        <option value="admin" <?php if (!empty($user[0]['type']) && $user[0]['type'] == "admin") echo "selected" ?>>Admin</option>
-
-                        <option value="user" <?php if (!empty($user[0]['type']) && $user[0]['type'] == "user") echo "selected" ?>>User</option>
-
-                        <option value="guest" <?php if (!empty($user[0]['type']) && $user[0]['type'] == "guest") echo "selected" ?>>Guest</option>
-
-                    </select>
-                </div>
-                <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
-            </form>
-        <?php } else { ?>
-            <div class="alert alert-success" role="alert">
-                User not found!
+            <div class="form-group">
+                <label for="fullname">Fullname</label>
+                <input name="fullname" class="form-control" placeholder="Fullname"
+                       value="<?php if (!empty($user[0]['fullname'])) echo strip_tags($user[0]['fullname']) ?>">
             </div>
-        <?php } ?>
-    </div>
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <!-- <input type="password" name="password" class="form-control" placeholder="password" value="<?php //if (!empty($user[0]['password'])) echo $user[0]['password']
+                ?>"> -->
+                <input type="password" name="password" class="form-control" placeholder="password" required>
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" name="email" class="form-control" placeholder="Email"
+                       value="<?php if (!empty($user[0]['email'])) echo strip_tags($user[0]['email']) ?>">
+            </div>
+            <div class="form-group">
+                <label for="type">Type</label>
+                <select name="type" uuid="type" class="form-control">
+
+                    <option value="" selected disabled>Select type</option>
+
+                    <option value="admin" <?php if (!empty($user[0]['type']) && $user[0]['type'] == "admin") echo "selected" ?>>
+                        Admin
+                    </option>
+
+                    <option value="user" <?php if (!empty($user[0]['type']) && $user[0]['type'] == "user") echo "selected" ?>>
+                        User
+                    </option>
+
+                    <option value="guest" <?php if (!empty($user[0]['type']) && $user[0]['type'] == "guest") echo "selected" ?>>
+                        Guest
+                    </option>
+
+                </select>
+            </div>
+            <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
+        </form>
+    <?php } else { ?>
+        <div class="alert alert-success" role="alert">
+            User not found!
+        </div>
+    <?php } ?>
+</div>
 
 
 </body>
