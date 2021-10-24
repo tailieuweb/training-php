239f83dd2fcdf57bfca003866149b19c86d9ec87
Trung Nguyen Huynh Quoc <19211tt0744@mail.tdc.edu.vn>
2021-10-24 09:59:49 +0700

    test sum cases

--- tests/UserModelTest.php
+++ tests/UserModelTest.php
@@ -22,7 +22,7 @@ class UserModelTest extends TestCase
     /**
      * Test case Not good
      */
-    public function testSumNg()
+    public function testSumDuong()
     {
         $userModel = new UserModel();
         $a = 1;
@@ -35,5 +35,79 @@ class UserModelTest extends TestCase
         } else {
             $this->assertTrue(true);
         }
+    }
+        /**
+     * Test case Okie
+     */
+    public function testSumAm()
+    {
+        $userModel = new UserModel();
+        $a = -1;
+        $b = -2;
+        $expected = -3;
+
+        $actual = $userModel->sumb($a, $b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Test case Okie
+     */
+    public function testSumAmDuong()
+    {
+        $userModel = new UserModel();
+        $a = -1;
+        $b = 2;
+        $expected = 1;
+
+        $actual = $userModel->sumb($a, $b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Test case Okie
+     */
+    public function testSumSoThuc()
+    {
+        $userModel = new UserModel();
+        $a = 1.5;
+        $b = 2.1;
+        $expected = 3.6;
+
+        $actual = $userModel->sumb($a, $b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Test case Okie
+     */
+    public function testSumSoChuoi()
+    {
+        $userModel = new UserModel();
+        $a = 1;
+        $b = "2";
+        $expected = 3;
+
+        $actual = $userModel->sumb($a, $b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Test case Okie
+     */
+    public function testSumChuoi()
+    {
+        $userModel = new UserModel();
+        $a = "1";
+        $b = "2";
+        $expected = 3;
+
+        $actual = $userModel->sumb($a, $b);
+
+        $this->assertEquals($expected, $actual);
     }
 }
