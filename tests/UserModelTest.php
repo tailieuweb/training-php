cae7e0b48a4a806cf9df19a03d38cefa7e9d9b5a
Trong Hieu <tronghieu60s@gmail.com>
2021-10-24 09:02:43 +0700

    Update composer 9, test sumb cases

--- tests/UserModelTest.php
+++ tests/UserModelTest.php
@@ -36,4 +36,79 @@ class UserModelTest extends TestCase
             $this->assertTrue(true);
         }
     }
+
+    /**
+     * Test case Okie
+     */
+    public function testSum2()
+    {
+        $userModel = new UserModel();
+        $a = -1;
+        $b = -2;
+        $expected = -3;
+
+        $actual = $userModel->sumb($a, $b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Test case Okie
+     */
+    public function testSum3()
+    {
+        $userModel = new UserModel();
+        $a = -1;
+        $b = 2;
+        $expected = 1;
+
+        $actual = $userModel->sumb($a, $b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Test case Okie
+     */
+    public function testSum4()
+    {
+        $userModel = new UserModel();
+        $a = 1.5;
+        $b = 2.1;
+        $expected = 3.6;
+
+        $actual = $userModel->sumb($a, $b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Test case Okie
+     */
+    public function testSum5()
+    {
+        $userModel = new UserModel();
+        $a = 1;
+        $b = "2";
+        $expected = 3;
+
+        $actual = $userModel->sumb($a, $b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Test case Okie
+     */
+    public function testSum6()
+    {
+        $userModel = new UserModel();
+        $a = "1";
+        $b = "2";
+        $expected = 3;
+
+        $actual = $userModel->sumb($a, $b);
+
+        $this->assertEquals($expected, $actual);
+    }
 }
