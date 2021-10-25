c904d5e1aca41fceb087616ebd6e87b716b2f826
Nguyễn Tuấn <67812579+tuancutedeptrai@users.noreply.github.com>
2021-10-25 12:04:18 +0700

    Update Test case

--- tests/UserModelTest.php
+++ tests/UserModelTest.php
@@ -19,6 +19,85 @@ class UserModelTest extends TestCase
        $this->assertEquals($expected, $actual);
     }
 
+    public  function  testCong2SoAm()
+    {
+        $userModel = new UserModel();
+        $a = -5;
+        $b = -5;
+        $expected = -10;
+
+        $actual = $userModel->sumb($a,$b);
+
+        $this->assertEquals($expected, $actual);
+
+
+    }
+
+
+    public  function  testCong1Amva1Duong()
+    {
+        $userModel = new UserModel();
+        $a = -8;
+        $b = 5;
+        $expected = -3;
+
+        $actual = $userModel->sumb($a,$b);
+
+        $this->assertEquals($expected, $actual);
+
+
+    }
+
+
+    public  function  testCong2sothucDuong()
+    {
+        $userModel = new UserModel();
+        $a = 5.3;
+        $b = 8.5;
+        $expected = 13.8;
+
+        $actual = $userModel->sumb($a,$b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+
+    public  function  testCong2sothucAm()
+    {
+        $userModel = new UserModel();
+        $a = -5.3;
+        $b = -8.5;
+        $expected = -13.8;
+
+        $actual = $userModel->sumb($a,$b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public  function  testCongchuoivaso(){
+        $userModel = new UserModel();
+        $a = 'abc';
+        $b = 8;
+        $expected = 'error';
+
+        $actual = $userModel->sumb($a,$b);
+        $this->assertEquals($expected, $actual);
+
+    }
+
+    public  function  testCongchuoivachuoi(){
+        $userModel = new UserModel();
+        $a = 'abc';
+        $b = 'xyz';
+        $expected = 'abcxyz';
+
+        $actual = $userModel->sumb($a,$b);
+        $this->assertEquals($expected, $actual);
+
+    }
+
+
+
     /**
      * Test case Not good
      */
