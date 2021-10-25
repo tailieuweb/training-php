f938efc833bfbe4b6676e4f421a002e99a205a05
Longkunz <Cuvip1325@gmail.com>
2021-10-25 13:39:55 +0700

    Create controller & model

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PhpParser\Node\Expr\FuncCall;
+
+class PageController extends Controller
+{
+    public function index(){
+
+    }
+    //Hiển thị trang login của khách hàng
+    public function userLogin(){
+        
+    }
+}
