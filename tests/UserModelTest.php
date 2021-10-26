c988aa5e3d8c259970f236f0a9085538fe2e782b
trinhduyen1505 <trinhthimyduyen1505@gmail.com>
2021-10-26 20:24:37 +0700

    testNumber

--- tests/UserModelTest.php
+++ tests/UserModelTest.php
@@ -19,6 +19,64 @@ class UserModelTest extends TestCase
        $this->assertEquals($expected, $actual);
     }
 
+    public function testSumNega(){
+        $userModel = new UserModel();
+        $a = -1;
+        $b = -3;
+        $expected = -4;
+
+        $actual = $userModel->sumb($a,$b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testSumAmDuong(){
+        $userModel = new UserModel();
+        $a = 5;
+        $b = -3;
+
+        $expected = 2;
+
+        $actual = $userModel->sumb($a,$b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testSumbRealNumber(){
+        $userModel = new UserModel();
+        $a = 5.4;
+        $b = 3.0;
+
+        $expected = 8.4;
+
+        $actual = $userModel->sumb($a,$b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testSumbRealNumberNega(){
+        $userModel = new UserModel();
+        $a = -2.8;
+        $b = -3.2;
+
+        $expected = -6.0;
+
+        $actual = $userModel->sumb($a,$b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testSumbRealNumberAmDuong(){
+        $userModel = new UserModel();
+        $a = -4.2;
+        $b = 3.2;
+
+        $expected = -1.0;
+
+        $actual = $userModel->sumb($a,$b);
+
+        $this->assertEquals($expected, $actual);
+    }
     /**
      * Test case Not good
      */
@@ -31,6 +89,81 @@ class UserModelTest extends TestCase
         $actual = $userModel->sumb($a,$b);
 
         if ($actual != 3) {
+            $this->assertTrue(false);
+        } else {
+            $this->assertTrue(true);
+        }
+    }
+
+    public function testSumNegaNg()
+    {
+        $userModel = new UserModel();
+        $a = -1;
+        $b = -3;
+
+        $actual = $userModel->sumb($a,$b);
+
+        if ($actual != -4) {
+            $this->assertTrue(false);
+        } else {
+            $this->assertTrue(true);
+        }
+    }
+
+    public function testSumAmDuongNg()
+    {
+        $userModel = new UserModel();
+        $a = 5;
+        $b = -3;
+
+        $actual = $userModel->sumb($a,$b);
+
+        if ($actual != 2) {
+            $this->assertTrue(false);
+        } else {
+            $this->assertTrue(true);
+        }
+    }
+
+    public function testSumRealNumberNg()
+    {
+        $userModel = new UserModel();
+        $a = 5.4;
+        $b = 3.0;
+
+        $actual = $userModel->sumb($a,$b);
+
+        if ($actual != 8.4) {
+            $this->assertTrue(false);
+        } else {
+            $this->assertTrue(true);
+        }
+    }
+
+    public function testSumRealNumberNegaNg()
+    {
+        $userModel = new UserModel();
+        $a = -2.8;
+        $b = -3.2;
+
+        $actual = $userModel->sumb($a,$b);
+
+        if ($actual != -6.0) {
+            $this->assertTrue(false);
+        } else {
+            $this->assertTrue(true);
+        }
+    }
+
+    public function testSumRealNumberAmDuongNg()
+    {
+        $userModel = new UserModel();
+        $a = -4.2;
+        $b = 3.2;
+
+        $actual = $userModel->sumb($a,$b);
+
+        if ($actual != -1.0) {
             $this->assertTrue(false);
         } else {
             $this->assertTrue(true);
