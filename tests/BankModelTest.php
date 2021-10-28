2b4901f8b04f8e2a52f3a544fbf48a7b9d1d2ba9
VoDangVinh2000 <01686228222vinh@gmail.com>
2021-10-28 18:17:32 +0700

    update unittest

---
+++ tests/BankModelTest.php
@@ -0,0 +1,16 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class BankModelTest extends TestCase
+{
+
+    /**
+     * Test case Okie
+     */
+
+    //auth
+    public function testAuth(){
+        var_dump('ok');exit();
+    }
+    //deleteUserById
+}
