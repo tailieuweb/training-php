31125df9adfe268ec73a6ab73e92319780965fa0
Van Lam <levanlam3447@gmail.com>
2021-10-28 16:54:52 +0700

    Test function findUserByID, auth

--- tests/UserModelTest.php
+++ tests/UserModelTest.php
@@ -1,9 +1,59 @@
 <?php
 
-use phpDocumentor\Reflection\PseudoTypes\True_;
 use PHPUnit\Framework\TestCase;
 
 class UserModelTest extends TestCase {
+
+    /**
+     * Test function findUserById
+     */
+    public function testFindUserById(){
+        $user = new UserModel();
+        $userId = 26;
+
+        $expected = "LE VAN LAM";
+        $actual = $user->findUserById($userId);
+
+        $this->assertEquals($expected,$actual[0]['name']);
+    }
+    /**
+     * Test function findUserById
+     */
+    // public function testFindUser(){
+    //     $user = new UserModel();
+    //     $keys = "a";
+
+    //     // $expected = "LE VAN LAM";
+    //     $actual = $user->findUser($keys);
+
+    //     // var_dump($actual);die();
+    // }
+
+    /**
+     * Test function findUserById
+     */
+    public function testAuth(){
+        $user = new UserModel();
+        $name = "Le LAM";
+        $pass = "123456";
+
+        // $expected = "LE VAN LAM";
+        $actual = $user->auth($name, $pass);
+
+        if($name == $actual[0]['name'] && $pass == $actual[0]['password']){
+            return $this->assertTrue(true); 
+        }
+        else{
+            return $this->assertTrue(false); 
+        }
+
+        // var_dump($actual);die();
+    }
+
+    /**
+     * Eg: Test function Sum a, b
+     * Test characters
+     */
     public function testSumOk()
     {
         $userModel = new UserModel();
@@ -48,7 +98,7 @@ class UserModelTest extends TestCase {
       
     }
     public function testSumAmAndDuong()
-      {
+    {
           $userModel = new UserModel();
           $a = -1;
           $b = 2;
@@ -64,7 +114,7 @@ class UserModelTest extends TestCase {
         }
     }
     public function testSumThucDuong()
-      {
+    {
           $userModel = new UserModel();
           $a = 1.5;
           $b = 1.5;
@@ -81,76 +131,98 @@ class UserModelTest extends TestCase {
         
     }
     public function testSumThucAm()
+    {
+        $userModel = new UserModel();
+        $a = -1.5;
+        $b = -1.5;
+        $actual = $userModel->sumb($a,$b);
+
+        if($actual != -3)
         {
-            $userModel = new UserModel();
-            $a = -1.5;
-            $b = -1.5;
-            $actual = $userModel->sumb($a,$b);
-    
-          if($actual != -3)
-          {
-              $this->assertTrue(false); 
-          }
-          else
-          {
-              $this->assertTrue(true); 
-          }
+            $this->assertTrue(false); 
         }
-        public function testSumThucAmAndDuong()
+        else
         {
-            $userModel = new UserModel();
-            $a = -1.5;
-            $b = 1.5;
-            $actual = $userModel->sumb($a,$b);
-    
-          if($actual != 0)
-          {
-              $this->assertTrue(false); 
-          }
-          else
-          {
-              $this->assertTrue(true); 
-          }
+            $this->assertTrue(true); 
         }
-      
-        public function testSumChuoiAndSo()
+    }
+    public function testSumThucAmAndDuong()
+    {
+        $userModel = new UserModel();
+        $a = -1.5;
+        $b = 1.5;
+        $actual = $userModel->sumb($a,$b);
+
+        if($actual != 0)
         {
-            $userModel = new UserModel();
-            $a = 'a';
-            $b = 1.5;
-            $actual = $userModel->sumb($a,$b);
-    
-          if($actual != 'error')
-          {
-              $this->assertTrue(false); 
-          }
-          else
-          {
-              $this->assertTrue(true); 
-          }
+            $this->assertTrue(false); 
         }
-        public function testSumChuoiAndChuoi()
+        else
         {
-            $userModel = new UserModel();
-            $a = 'a';
-            $b = 'a';
-            $actual = $userModel->sumb($a,$b);
-    
-          if($actual != 'error')
-          {
-              $this->assertTrue(false); 
-          }
-          else
-          {
-              $this->assertTrue(true); 
-          }
+            $this->assertTrue(true); 
         }
-/**
+    }
+    
+    public function testSumChuoiAndSo()
+    {
+        $userModel = new UserModel();
+        $a = 'a';
+        $b = 1.5;
+        $actual = $userModel->sumb($a,$b);
+
+        if($actual != 'error')
+        {
+            $this->assertTrue(false); 
+        }
+        else
+        {
+            $this->assertTrue(true); 
+        }
+    }
+    public function testSumChuoiAndChuoi()
+    {
+        $userModel = new UserModel();
+        $a = 'a';
+        $b = 'a';
+        $actual = $userModel->sumb($a,$b);
+
+        if($actual != 'error')
+        {
+            $this->assertTrue(false); 
+        }
+        else
+        {
+            $this->assertTrue(true); 
+        }
+    }
+    /**
      * Check string
      */
     public function testCheckString(){
         $userModel = new UserModel();
-        $str = "jksfhsa$@#lfsa";
+        $str = "jksfhsalfsa";
+        $check = $userModel->checkString($str);
+        if($check == true){
+            $this->assertTrue(true);
+        }
+        else{
+            $this->assertTrue(false);
+        }
+    }
+    public function testCheckStringF(){
+        $userModel = new UserModel();
+        $str = "jksfhsa%lfsa";
+        $check = $userModel->checkString($str);
+        if($check == true){
+            $this->assertTrue(false);
+        }
+        else{
+            $this->assertTrue(true);
+        }
+    }
+    public function testCheckStringNg(){
+        $userModel = new UserModel();
+        $str = 2;
         $check = $userModel->checkString($str);
         if($check == true){
             $this->assertTrue(true);
