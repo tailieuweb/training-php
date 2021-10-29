490ca828cf080a60f11e71e6d09f7afb8196c574
NguyenMinhThien <nguyenminhthien263@gmail.com>
2021-10-29 17:22:28 +0700

    update for singleton

--- models/UserModel.php
+++ models/UserModel.php
@@ -95,6 +95,13 @@ class UserModel extends BaseModel {
 
         return $users;
     }
+    public static function getInstance(){
+        if (self::$_instance !== null) {
+            return self::$_instance;
+        }
+        self::$_instance = new self();
+        return self::$_instance;
+    }
     /**
      * Convert to number
      * @param $input
