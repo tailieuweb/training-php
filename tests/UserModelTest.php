8e11dc1b4bb82bcdc6b146dd023bf26114f4090a
ngothanhthai297 <ngothai653@gmail.com>
2021-10-29 15:55:38 +0700

    Set up PHPUnit

--- tests/UserModelTest.php
+++ tests/UserModelTest.php
@@ -1,4 +1,6 @@
 <?php
+
+use phpDocumentor\Reflection\Types\Object_;
 use PHPUnit\Framework\TestCase;
 
 class UserModelTest extends TestCase
@@ -7,16 +9,40 @@ class UserModelTest extends TestCase
     /**
      * Test case Okie
      */
+    public function testInsertUser()
+    {
+        $input = [
+            'name' => 'ngo thanh thai 76',
+            'password' => 'sahuynh1',
+        ];
+
+        $userModel = new UserModel();
+        $actual = $userModel->insertUser($input);
+        $expected = true;
+        $this->assertEquals($actual, $expected);
+    }
+    public function testGetUser()
+    {
+        $userModel = new UserModel();
+        $actual = $userModel->getUsers();
+        if (is_array($actual)) {
+            $actual = true;
+        } else {
+            $actual = false;
+        }
+        $expected = true;
+        $this->assertEquals($actual, $expected);
+    }
     public function testSumOk()
     {
-       $userModel = new UserModel();
-       $a = 1;
-       $b = 2;
-       $expected = 3;
+        $userModel = new UserModel();
+        $a = 1;
+        $b = 2;
+        $expected = 3;
 
-       $actual = $userModel->sumb($a,$b);
+        $actual = $userModel->sumb($a, $b);
 
-       $this->assertEquals($expected, $actual);
+        $this->assertEquals($expected, $actual);
     }
 
     /**
@@ -28,7 +54,7 @@ class UserModelTest extends TestCase
         $a = 1;
         $b = 2;
 
-        $actual = $userModel->sumb($a,$b);
+        $actual = $userModel->sumb($a, $b);
 
         if ($actual != 3) {
             $this->assertTrue(false);
