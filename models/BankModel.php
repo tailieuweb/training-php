0d55f50c135b898b8da29d90a1bc6bb6134656bf
khang19211 <trannhikhang964587@gmail.com>
2021-10-30 13:01:56 +0700

    BankModel

---
+++ models/BankModel.php
@@ -0,0 +1,120 @@
+<?php
+
+require_once 'BaseModel.php';
+
+class BankModel extends BaseModel {
+
+    public function findUserById($id) {
+        $sql = 'SELECT * FROM users WHERE id = '.$id;
+        $user = $this->select($sql);
+
+        return $user;
+    }
+
+    public function findUser($keyword) {
+        $sql = 'SELECT * FROM users WHERE user_name LIKE %'.$keyword.'%'. ' OR user_email LIKE %'.$keyword.'%';
+        $user = $this->select($sql);
+
+        return $user;
+    }
+
+    /**
+     * Authentication user
+     * @param $userName
+     * @param $password
+     * @return array
+     */
+    public function auth($userName, $password) {
+        $md5Password = md5($password);
+        $sql = 'SELECT * FROM users WHERE name = "' . $userName . '" AND password = "'.$md5Password.'"';
+        $user = $this->select($sql);
+        return $user;
+    }
+
+    /**
+     * Delete user by id
+     * @param $id
+     * @return mixed
+     */
+    public function deleteBankById($id) {
+      
+        $sql = 'DELETE FROM banks WHERE id = '.$id;
+        return $this->delete($sql);
+
+    }
+
+
+
+    /**
+     * Update user
+     * @param $input
+     * @return mixed
+     */
+    public function updateUser($input) {
+      
+        $temp = 'SELECT version FROM users WHERE id = '.$input['id'].'';
+        $newTemp = $this->select($temp);
+        
+        if($newTemp[0]['version'] == $input['version']){
+            $newV = $input['version']+1;
+             $sql = 'UPDATE users SET 
+                 name = "' . $input['name'] .'", 
+                 email = "'.$input['email'].'",
+                 fullname = "'.$input['fullname'].'",
+                 password="'. md5($input['password']) .'", type = "'.$input['type'].'", version = "'.$newV.'"
+                WHERE id = ' . $input['id'] ;
+            $user = $this->update($sql);  
+            header('location: list_users.php?success');  
+            return $user;         
+        } 
+        else{                
+           header('location: list_users.php?err');  
+        }
+        
+        
+    }
+
+    /**
+     * Insert user
+     * @param $input
+     * @return mixed
+     */
+    public function insertUser($input) {
+        $sql = "INSERT INTO `app_web1`.`users` (`name`, `password`,`fullname`,`email`,`type`) VALUES (" .
+        "'" . $input['name'] . "', '"
+        . md5($input['password']) . "', '"
+        . $input['fullname'] . "', '"
+        . $input['email'] . "', '"
+        . $input['type']
+        . "')";
+        $user = $this->insert($sql);
+      
+        return $user;
+                
+    }
+
+    /**
+     * Search users
+     * @param array $param
+     * @return array
+     */
+    public function getBanks($params = []) {
+        //Keyword
+       
+        if (!empty($params['keyword'])) {
+           
+            $sql = 'SELECT * FROM banks WHERE name LIKE "%' . $params['keyword'] .'%"';
+
+            //Keep this line to use Sql Injection
+            //Don't change
+            //Example keyword: abcef%";TRUNCATE banks;##
+            $banks = self::$_connection->multi_query($sql);
+            
+        } else {
+            $sql = 'SELECT * FROM banks';
+            $banks = $this->select($sql);
+        }
+        return $banks;
+       
+    }
+}
