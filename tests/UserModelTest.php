969c4c04a64e3b4e3e90869c4c300ff44601cdea
Tan Le <19211tt1603@mail.tdc.edu.vn>
2021-10-30 14:00:15 +0700

    commit usermodeltest

--- tests/UserModelTest.php
+++ tests/UserModelTest.php
@@ -96,4 +96,46 @@ class UserModelTest extends TestCase
 
        $this->assertEquals($expected, $actual);
     }
+
+    public function testFindByIdOk()
+    {
+       $userModel = new UserModel();
+       $userId = 2;
+       $userName = 'test2';
+
+       $user = $userModel->findUserById($userId);
+       $actual = $user[0]['name'];
+
+       $this->assertEquals($userName, $actual);
+    }
+
+    public function testFindByIdNg()
+    {
+       $userModel = new UserModel();
+       $userId = 222;
+       
+       $expected = null;
+
+       $user = $userModel->findUserById($userId);
+
+       if(empty($user)){
+          $this->assertTrue(true);
+       }
+       else
+       {
+         $this->assertFalse(false);
+       }
+    }
+
+    public function testInsertUserOk()
+    {
+      $userModel = new UserModel();
+       $userId = 3;
+       $userName = 'test3';
+
+       $user = $userModel->insertUser($userId);
+       $actual = $user[0]['name'];
+
+       $this->assertEquals($userName, $actual);
+    }
 }
