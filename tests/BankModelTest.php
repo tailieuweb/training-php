8d47cc67134276f1897dfca75bf9a50c23fe123b
Tan Le <19211tt1603@mail.tdc.edu.vn>
2021-10-31 15:39:24 +0700

    test findBankById

--- tests/BankModelTest.php
+++ tests/BankModelTest.php
@@ -3,6 +3,37 @@ use PHPUnit\Framework\TestCase;
 
 class BankModelTest extends TestCase
 {
-    //Đặt tên function bắt đầu bằng test nha quý zị
-    //Ví dụ: testFindUser()
+    /**
+     * Test case  findBankById OK
+     */
+    public function testFindBankByIdOk()
+    {
+        $bankModel = new BankModel();
+        $bankId = 2;
+        $cost = 11;
+
+        $bank = $bankModel->findBankById($bankId);
+        $actual = $bank[0]['cost'];
+
+        $this->assertEquals($cost, $actual);
+    }
+
+     /**
+     * Test case findUserById Not good
+     */
+    public function testFindBankByIdNg()
+    {
+        $bankModel = new BankModel();
+        $bankId = 9999;
+        $expected = null;
+
+        $bank = $bankModel->findBankById($bankId);
+
+        if(empty($bank)){
+            $this->assertTrue(true);
+        }
+        else{
+            $this->assertFalse(false);
+        }
+    }
 }
