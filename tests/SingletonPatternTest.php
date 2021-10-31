9e07fe00f4f7a4d8bd84218ec80ea4126b249853
Tan Le <19211tt1603@mail.tdc.edu.vn>
2021-10-31 15:40:42 +0700

    test singtonPattern

--- tests/SingletonPatternTest.php
+++ tests/SingletonPatternTest.php
@@ -2,7 +2,56 @@
 use PHPUnit\Framework\TestCase;
 
 class SingletonPatternTest extends TestCase
-{
-    //Đặt tên function bắt đầu bằng test nha quý zị
-    //Ví dụ: testFindUser()
+{   
+    /**
+     * Test case singletonUser Ok
+     */
+     public function testSingletonUserOK()
+     {
+         $singleton = new SingletonPattern();
+         $userModel = new UserModel();
+
+         $singleton = $singleton->make('user');
+         $expected = $userModel;
+         $actual = $singleton;
+
+         $this->assertEquals($expected, $actual);
+     }
+
+     /**
+     * Test case singletonBank Ok
+     */
+     public function testSingletonBankOK()
+     {
+         $singleton = new SingletonPattern();
+         $bankModel = new BankModel();
+
+         $singleton = $singleton->make('bank');
+         $expected = $bankModel;
+         $actual = $singleton;
+
+         $this->assertEquals($expected, $actual);
+     }
+
+     /**
+     * Test case singleton Not good
+     */
+     public function testSingletonPatternNg()
+     {
+        $singleton = new SingletonPattern();
+        $userModel = new UserModel();
+        $bankModel = new BankModel();
+
+        $user = 'user';
+        $bank = 'bank';
+
+        if($singleton->make('user') == $userModel && $singleton->make('bank') == $bankModel)
+        {
+            $this->assertTrue(true);
+        }
+        else
+        {
+            $this->assertFalse(false);
+        }
+     }
 }
