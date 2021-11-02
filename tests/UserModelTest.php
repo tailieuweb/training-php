c0611c43694e1c995529ad0e20349a3a21590812
Longkunz <Cuvip1325@gmail.com>
2021-11-02 16:10:52 +0700

    Test function findUser()

--- tests/UserModelTest.php
+++ tests/UserModelTest.php
@@ -1,99 +1,151 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
 
 class UserModelTest extends TestCase
 {
 
-    /**
-     * Test case Okie
+   /**
+    * Test case Okie
+    */
+   public function testSumOk()
+   {
+      $userModel = new UserModel();
+      $a = 1;
+      $b = 2;
+      $expected = 3;
+
+      $actual = $userModel->sumb($a, $b);
+
+      $this->assertEquals($expected, $actual);
+   }
+
+   /**
+    * Test case Not good
+    */
+   public function testSumNg()
+   {
+      $userModel = new UserModel();
+      $a = 1;
+      $b = 2;
+
+      $actual = $userModel->sumb($a, $b);
+
+      if ($actual != 3) {
+         $this->assertTrue(false);
+      } else {
+         $this->assertTrue(true);
+      }
+   }
+
+   public function testSumOkam()
+   {
+      $userModel = new UserModel();
+      $a = -1;
+      $b = -2;
+      $expected = -3;
+
+      $actual = $userModel->sumb($a, $b);
+
+      $this->assertEquals($expected, $actual);
+   }
+
+   public function testSumOkad()
+   {
+      $userModel = new UserModel();
+      $a = -1;
+      $b = 2;
+      $expected = 1;
+
+      $actual = $userModel->sumb($a, $b);
+
+      $this->assertEquals($expected, $actual);
+   }
+
+   public function testSumOkDouble()
+   {
+      $userModel = new UserModel();
+      $a = 1.5;
+      $b = 2.5;
+      $expected = 4;
+
+      $actual = $userModel->sumb($a, $b);
+
+      $this->assertEquals($expected, $actual);
+   }
+
+   public function testStr()
+   {
+      $userModel = new UserModel();
+      $a = 1;
+      $b = 'a';
+      $expected = 'error';
+
+      $actual = $userModel->sumb($a, $b);
+
+      $this->assertEquals($expected, $actual);
+   }
+
+   public function testString()
+   {
+      $userModel = new UserModel();
+      $a = 'a';
+      $b = 'b';
+      $expected = 'error';
+
+      $actual = $userModel->sumb($a, $b);
+
+      $this->assertEquals($expected, $actual);
+   }
+   /*
+     * Test function: findUser()
+     * Author: Long
      */
-    public function testSumOk()
-    {
-       $userModel = new UserModel();
-       $a = 1;
-       $b = 2;
-       $expected = 3;
+   //Test find user with valid keyword
+   public function testFindUserValid()
+   {
+      $userModel = new UserModel();
+      $keyword = 'test1';
+      $actual = $userModel->findUser($keyword);
+      //var_dump($actual); die;
+      $this->assertEquals($keyword, $actual[0]['name']);
+   }
+   //Test find user with valid keyword true
+   public function testFindUserValidTrue()
+   {
+      $userModel = new UserModel();
+      $keyword = 'test1';
+      $actual = $userModel->findUser($keyword);
 
-       $actual = $userModel->sumb($a,$b);
-
-       $this->assertEquals($expected, $actual);
-    }
-
-    /**
-     * Test case Not good
-     */
-    public function testSumNg()
-    {
-        $userModel = new UserModel();
-        $a = 1;
-        $b = 2;
-
-        $actual = $userModel->sumb($a,$b);
-
-        if ($actual != 3) {
-            $this->assertTrue(false);
-        } else {
-            $this->assertTrue(true);
-        }
-    }
-
-    public function testSumOkam()
-    {
-       $userModel = new UserModel();
-       $a = -1;
-       $b = -2;
-       $expected = -3;
-
-       $actual = $userModel->sumb($a,$b);
-
-       $this->assertEquals($expected, $actual);
-    }
-
-    public function testSumOkad()
-    {
-       $userModel = new UserModel();
-       $a = -1;
-       $b = 2;
-       $expected = 1;
-
-       $actual = $userModel->sumb($a,$b);
-
-       $this->assertEquals($expected, $actual);
-    }
-
-    public function testSumOkDouble()
-    {
-       $userModel = new UserModel();
-       $a = 1.5;
-       $b = 2.5;
-       $expected = 4;
-
-       $actual = $userModel->sumb($a,$b);
-
-       $this->assertEquals($expected, $actual);
-    }
-
-    public function testStr()
-    {
-       $userModel = new UserModel();
-       $a = 1;
-       $b = 'a';
-       $expected = 'error';
-
-       $actual = $userModel->sumb($a,$b);
-
-       $this->assertEquals($expected, $actual);
-    }
-
-    public function testString()
-    {
-       $userModel = new UserModel();
-       $a = 'a';
-       $b = 'b';
-       $expected = 'error';
-
-       $actual = $userModel->sumb($a,$b);
-
-       $this->assertEquals($expected, $actual);
-    }
+      if ($actual[0]['name'] != $keyword) {
+         $this->assertTrue(false);
+      } else {
+         $this->assertTrue(true);
+      }
+   }
+   //Test find user with invalid keyword
+   public function testFindUserInvalid()
+   {
+      $userModel = new UserModel();
+      $keyword = 'test5';
+      $actual = $userModel->findUser($keyword);
+      if ($actual == null) {
+         $this->assertTrue(true);
+      } else {
+         $this->assertTrue(false);
+      }
+   }
+   //Test find user with multi result
+   public function testFindUserMultiResult()
+   {
+      $userModel = new UserModel();
+      $keyword = 'test';
+      $actual = $userModel->findUser($keyword);
+      // var_dump($actual); die;
+      if ($actual[0]['name'] == 'test1' && $actual[1]['name'] == 'test3') {
+         $this->assertTrue(true);
+      } else {
+         $this->assertTrue(false);
+      }
+   }
 }
