f03d60b5727065b36f0890b0d3d9ac6878528043
nhikhang2000 <trannhikhang964587@gmail.com>
2021-11-01 23:26:31 -0700

    upunittest

--- tests/BankModelTest.php
+++ tests/BankModelTest.php
@@ -3,13 +3,13 @@ use PHPUnit\Framework\TestCase;
 
 class BankModelTest extends TestCase
 {
-    public function testKhangFindBankByIdOk(){
-        $bankModel = new BankModel();
-        $bankId  = 1;
-        $bank = 'DAB';
-        $banks = $bankModel->findBankById($bankId);
-        $actual = $banks[0]['bank'];
+    // public function testKhangFindBankByIdOk(){
+    //     // $bankModel = new BankModel();
+    //     // $bankId  = 1;
+    //     // $bank = 'DAB';
+    //     // $banks = $bankModel->findBankById($bankId);
+    //     // $actual = $banks[0]['bank'];
 
-        $this->assertEquals($bank, $actual);
-    }
+    //     // $this->assertEquals($bank, $actual);
+    // }
 }
