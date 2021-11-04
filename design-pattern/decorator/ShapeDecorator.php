be032bbfa225490bf77c1bd485f95ec8d755dfae
Hieu Trung <nguyenhieutrung0412@gmail.com>
2021-11-04 12:52:17 +0700

    decorator pattern

---
+++ design-pattern/decorator/ShapeDecorator.php
@@ -0,0 +1,16 @@
+<?php
+require_once('Shape.php');
+
+abstract class ShapeDecorator implements Shape
+{
+    protected $decoratedShape;
+
+    function __construct(Shape $decoratedShape){
+        $this->decoratedShape = $decoratedShape;
+    }
+
+    public function draw()
+    {
+        echo "Shape: Rectangle";
+    }
+}
