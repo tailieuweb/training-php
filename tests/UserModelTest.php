16cecd9b38d96df1e3ccd1ef6e27de624a0d99fa
sangtm97 <minhsangtrieu94@gmail.com>
2021-11-05 23:19:15 +0700

    update testInsertUserOK

--- tests/UserModelTest.php
+++ tests/UserModelTest.php
@@ -10,9 +10,9 @@ class UserModelTest extends TestCase
     public function testSumOk()
     {
        $userModel = new UserModel();
-       $a = 1;
-       $b = 2;
-       $expected = 3;
+       $a = -1;
+       $b = -1;
+       $expected = -2;
 
        $actual = $userModel->sumb($a,$b);
 
@@ -36,4 +36,82 @@ class UserModelTest extends TestCase
             $this->assertTrue(true);
         }
     }
+
+
+    /**
+     * Test case Not good
+     */
+    public function testStr()
+    {
+        $userModel = new UserModel();
+        $a = 1;
+        $b = 'a';
+
+        $expected = 'error';
+        $actual = $userModel->sumb($a, $b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testCongSoAm()
+    {
+        $userModel = new UserModel();
+        $a = -10;
+        $b = -8;
+
+        $expected = '-18';
+        $actual = $userModel->sumb($a, $b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Test case Not good
+     */
+    public function testchuoi()
+    {
+        $userModel = new UserModel();
+        $a = 'Hello';
+        $b = 'word';
+
+        $expected = 'Helloword';
+        $actual = $userModel->sumb($a, $b);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    //test findUserByID
+    public function testFindUserById(){
+        $userModel = new UserModel();
+        $userId = 2;
+        $userName = 'test2';
+
+        $user = $userModel ->findUserById($userId);
+        $actual = $user[0]['name'];
+
+        $this->assertEquals($userName, $actual);
+    }
+
+    // Test case insertUserOk 
+    public function testInsertUserOK()
+    {
+        $userModel = new UserModel();
+        $input = [];
+        $input['name'] = 'Sang';
+        $input['fullname'] = "TM Sang";
+        $input['email'] = "sang@gmail.com";
+        $input['type'] = 0;
+        $input['password'] = "12345";
+      
+        $actual = $userModel->insertUser( $input);
+        if($actual != true)
+        {
+            $this->assertTrue(false); 
+        }
+        else
+        {
+            $this->assertTrue(true); 
+        }
+    }
+
 }
