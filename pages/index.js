5e6e126098c04f064ff0bdf9383e04178e57a33c
Trong Hieu <tronghieu60s@gmail.com>
2021-11-05 15:15:31 +0700

    Config i18n

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
-import DefaultHomePage from '../src/components/HomePage';
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import DefaultHomePage from "../src/components/HomePage";
 
 function HomePage() {
   return <DefaultHomePage />;
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      // Will be passed to the page component as props
+    },
+  };
 }
 
 export default HomePage;
