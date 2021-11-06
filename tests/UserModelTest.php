d26b5c35998af24b88d751f5acd26575360f0b29
TranVanKiett2001 <19211tt1739@mail.tdc.edu.vn>
2021-11-06 15:13:31 +0700

    Config PHPUnit

--- tests/UserModelTest.php
+++ tests/UserModelTest.php
@@ -16,78 +16,24 @@ class UserModelTest extends TestCase
 
        $actual = $userModel->sumb($a,$b);
 
-       $this->assertEquals(3, $actual);
-    }
-
-    // /**
-    //  * Test case Not good
-    //  */
-    // public function testSumNg()
-    // {
-    //     $userModel = new UserModel();
-    //     $a = 1;
-    //     $b = 2;
-
-    //     $actual = $userModel->sumb($a,$b);
-
-    //     if ($actual != 3) {
-    //         $this->assertTrue(false);
-    //     } else {
-    //         $this->assertTrue(true);
-    //     }
-    // }
-    public function testSumOk2()
-    {
-       $userModel = new UserModel();
-       $a = 1;
-       $b = 2;
-       $expected = 3;
-
-       $actual = $userModel->sumb($a,$b);
-
        $this->assertEquals($expected, $actual);
     }
-    
-    public function testFindUserOk(){
-        // $userModel = new UserModel();
-        // $user_name = 'user1';
-        // $actual = 'user1';
-        // $user = $userModel->findUser($user_name);
-        // $this->assertEquals($user,$actual);
 
+    /**
+     * Test case Not good
+     */
+    public function testSumNg()
+    {
         $userModel = new UserModel();
+        $a = 1;
+        $b = 2;
 
-        $user_name = 'user1';      
-        $mongDoiUsername = 'user1';   
-        $keyword = $userModel->findUser($user_name);
-        $this->assertEquals($mongDoiUsername, $keyword[0]['name']);
-        
-    }     
-    public function testFindUserByIdOk() {
-        $userModel = new UserModel();
+        $actual = $userModel->sumb($a,$b);
 
-        $id = 2;
-        $mongDoiUsername = 'user1';
-
-        $user = $userModel->findUserById($id);
-
-        $this->assertEquals($mongDoiUsername, $user[0]['name']);
-
-    }
-    public function testFindUserByIdNg() {
-        $userModel = new UserModel();
-
-        $id = 999999;
-
-
-        $user = $userModel->findUserById($id);
-
-        if (empty($user)) {
-            $this->assertTrue(true);
-        } else {
+        if ($actual != 3) {
             $this->assertTrue(false);
+        } else {
+            $this->assertTrue(true);
         }
     }
-
-    
 }
