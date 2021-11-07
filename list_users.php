9832f9aa729365c09a1c3176a108656ab1ba5416
TruongHoangHai123 <truonghoanghai.tdc2019@gmail.com>
2021-11-07 23:31:01 +0700

    Create Factory

--- list_users.php
+++ list_users.php
@@ -1,7 +1,7 @@
 <?php
 // Start the session
 session_start();
-
+// Factory pattern:
 require_once 'models/FactoryPattern.php';
 $factory = new FactoryPattern();
 
--- models/FactoryPattern.php
+++ models/FactoryPattern.php
@@ -1,17 +1,14 @@
 <?php
+// Factory pattern:
 require_once 'UserModel.php';
 require_once 'BankModel.php';
 class FactoryPattern {
 
     public function make($model) {
-        // Singleton pattern:
         if ($model == 'user') {
             return UserModel::getInstance();
         } else if ($model == 'bank') {
             return BankModel::getInstance();
-        }
-        else{
-            return null;
         }
     }
 
