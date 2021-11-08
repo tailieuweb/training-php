ed9bd7efded11571b3c222e969b741995a422f36
Ho Hoang Phu <hohoangphu.tdc2019@gmail.com>
2021-11-08 09:55:58 +0700

    update test singleton

---
+++ testsingleton.php
@@ -0,0 +1,62 @@
+
+<?php
+$factory = new Factory();
+$user = $factory -> make('user');
+$bank = $factory -> make('bank');
+$user->print();
+$bank->print();
+// var_dump($user);
+// $user->print();
+// $bank->print();
+abstract class Base {
+
+    const Bank = 'bank';
+    const User = 'user';
+    protected static $_instance;
+}
+class User extends Base
+{
+    public static function getInstance()
+    {
+        if (!empty(self::$_instance[self::User])){
+            return self::$_instance[self::User];
+        }
+        self::$_instance[self::User] = new self();
+        return self::$_instance[self::User];
+    }
+    public function print()
+    {
+        echo ' Lớp User';
+    }
+}
+class Bank extends Base
+{
+    public static function getInstance()
+    {
+        if (!empty(self::$_instance[self::Bank])){
+            return self::$_instance[self::Bank];
+        }
+        self::$_instance[self::Bank] = new self();
+        return self::$_instance[self::Bank];
+    }
+    public function print()
+    {
+        echo ' Lớp Bank';
+    }
+}
+
+class Factory
+{
+    public function make($model) {
+        // Singleton pattern:
+        if ($model == 'user') {
+            return User::getInstance();
+        } else if ($model == 'bank') {
+            return Bank::getInstance();
+        }
+        else{
+            return null;
+        }
+    }
+}
+
