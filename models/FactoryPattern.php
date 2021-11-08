ce3fb03dec1ab4545df2fde5676ee29631b84e70
nhquyen <nguyenhuuquyen2001@gmail.com>
2021-11-08 13:41:30 +0700

    Update factory

--- models/FactoryPattern.php
+++ models/FactoryPattern.php
@@ -5,7 +5,7 @@ class FactoryPattern {
 
     public function make($model) {
         if ($model == 'user') {
-            return UserModel::getInstance();
+            return new UserModel();
         } else if ($model == 'bank') {
             return new BankModel();
         }
