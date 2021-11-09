ada301b139718cfb6573746a3ce129667d3a8e5e
Van Nghi <19211tt1981@mail.tdc.edu.vn>
2021-11-09 10:03:16 +0700

    update

--- tests/UserModelTest.php
+++ tests/UserModelTest.php
@@ -120,4 +120,68 @@ class UserModelTest extends TestCase
 
        $this->assertEquals($expected, $actual);
     }
+    public function testFindUserByIdOk() {
+      $userModel = new UserModel();
+      
+      $id = 8;
+      $mongDoiUsername = 'asdf';
+      
+      $user = $userModel->findUserById($id);
+      
+      $this->assertEquals($mongDoiUsername, $user[0]['name']);
+      
+      }
+            
+      public function testFindUserByIdNg() {
+      $userModel = new UserModel();
+      
+      $id = 999999;
+      
+      
+      $user = $userModel->findUserById($id);
+      
+      if (empty($user)) {
+      $this->assertTrue(true);
+      } else {
+      $this->assertTrue(false);
+      }
+      
+      }
+      
+      public function testFindUserByIdStr() {
+      $userModel = new UserModel();
+      
+      $id = 'asdf';
+      
+      
+      $expected = 'error';
+      $actual = $userModel->findUserById($id);
+      
+      $this->assertEquals($expected, $actual);
+      
+      }
+      
+      
+      public function testFindUserByIdNull() {
+      $userModel = new UserModel();
+      $id = '';
+      $expected = 'error';
+      $actual = $userModel->findUserById($id);
+      
+      $this->assertEquals($expected, $actual);
+      
+      }
+      
+      public function testFindUserByIdObject() {
+      $userModel = new UserModel();
+      
+      $id = new stdClass();
+      $expected = 'error';
+      $actual = $userModel->findUserById($id);
+      
+      $this->assertEquals($expected, $actual);
+      
+      
+      
+      }
 }
