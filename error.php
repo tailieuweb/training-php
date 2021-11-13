436ae7eaa3281a1911b963969e4c8fa81bbb4f2b
nguyenhuuthang-tdc <nguyenhuuthang12c8@gmail.com>
2021-11-13 19:41:38 +0700

    sixth commit - try catch connect database 100%

---
+++ error.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <?php include 'views/meta.php' ?>
+</head>
+<body>
+    <?php include 'views/header.php'?>
+    <div class="container">
+        <div class="alert alert-warning" role="alert">
+            No connect database! <br>
+        </div>    
+    </div>
+</body>
+</html>
