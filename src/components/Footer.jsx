26fc91843d1b23114a787391dde479655ca2a335
BayHen <80869653+BayHen@users.noreply.github.com>
2021-11-13 10:14:12 +0700

    Loading Action

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
-import Loading from '../components/Base/Loading';
+import Loading from "../components/Base/Loading";
+import { useSelector } from "react-redux";
 
 export default function Footer() {
+  const common = useSelector((state) => state.common);
+  const loading = common?.loading;
   return (
     <div className="text-center my-5">
-      {/* <Loading /> */}
+      {loading && <Loading />}
       Copyright &copy; {new Date().getFullYear()} - All rights reserved.
     </div>
   );
--- src/redux/reducers/commonReducer.js
+++ src/redux/reducers/commonReducer.js
@@ -1,6 +1,6 @@
 import { START_LOADING, STOP_LOADING } from "../actions/commonActions";
 
-const commonReducer = (state = { loading: 0 }, action) => {
+const commonReducer = (state = { loading: false }, action) => {
   switch (action.type) {
     case START_LOADING:
       return { ...state, loading: true };
