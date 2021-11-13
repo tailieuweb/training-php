0221a0a3af6bdb291c708c519c0c15afb882fce7
Trung Nguyen Huynh Quoc <19211tt0744@mail.tdc.edu.vn>
2021-11-13 11:24:31 +0700

    Detail-Post-Content

--- src/components/PostsDetail/PostsDetailContent.jsx
+++ src/components/PostsDetail/PostsDetailContent.jsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
-import { useState } from "react";
 export default function PostsDetailContent(props) {
   const { post } = props;
-  const [isReadMore] = useState(post.description.length < 350);
   return (
     <div className="card bg-white rounded-lg p-4">
       <div className="card-body py-2">
@@ -18,31 +16,16 @@ export default function PostsDetailContent(props) {
               <a>
                 <h5 className="card-title mb-0">Unknown User</h5>
               </a>
-              <small className="card-text">{props.post.created_at}</small>
+              <small className="card-text">{post?.created_at}</small>
             </div>
           </div>
         </div>
-        <Link href={`/posts/${post.id}`}>
+        <Link href={`/posts/${post?.id}`}>
           <a>
-            <h2 className="text-primary my-4"># {post.title}</h2>
+            <h2 className="text-primary my-4"># {post?.title}</h2>
           </a>
         </Link>
-        <p className="card-text">
-        {isReadMore
-            ? post.description
-            : `${post.description.slice(0, 350)}...`}
-          {!isReadMore && (
-            <Link href={`/posts/${post.id}`}>
-              <a
-                style={{ fontSize: 12, color: "#000" }}
-                className="font-weight-bold"
-              >
-                {" "}
-                Read more
-              </a>
-            </Link>
-          )}
-        </p>
+        <p className="card-text">{post?.description}</p>
       </div>
     </div>
   );
