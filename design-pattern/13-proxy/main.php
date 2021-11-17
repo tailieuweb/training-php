192de5a00bb7dc52c627bb8d60214a14263f4f62
TranHuuPhuc <tranhuuphuc.tdc2019@gmail.com>
2021-11-17 13:43:17 +0700

    update design pattern

--- design-pattern/13-proxy/main.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "Image.php";
-require_once "RealImage.php";
-require_once "ProxyImage.php";
-$image = new ProxyImage("hinhne.jpg");
-$image->display();
-echo "<br>";
-$image->display();
-?>
