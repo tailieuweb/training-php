c4a0d12108ddbf590e3dd99650357fcbfec44df1
mrluongdz <vdluong2k.tdc.vn@gmail.com>
2021-11-18 22:00:09 +0700

    phpunittest

--- tests/BankModelTest.php
+++ tests/BankModelTest.php
@@ -3,5 +3,17 @@ use PHPUnit\Framework\TestCase;
 
 class BankModelTest extends TestCase
 {
+    //Tim id tại ví trí đầu tiên
+    public function testFindBankbyIdOk()
+    {
+        $bankModel = new BankModel();
+        $bankId = 1;
+        $bankCost = '11';
+        $bank = $bankModel->findBankById($bankId);
+       
+        $actual = $bank[0]['cost'];
+        $this->assertEquals($bankCost, $actual);
+    }
 
+    
 }
