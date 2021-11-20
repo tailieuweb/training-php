03549ddb19708edf84f7ec41eae0367947f3ee95
sangtm97 <minhsangtrieu94@gmail.com>
2021-11-21 00:04:58 +0700

    update code phpunit

---
+++ tests/BankModelTest.php
@@ -0,0 +1,105 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class BankModelTest extends TestCase
+{
+    //test findBankByID
+    public function testFindBankByIdOK(){
+         $bankModel = new BankModel();
+         $Id = 1;
+         $fullname = 'Nguyen A';
+
+         $bank = $bankModel ->findBankById($Id);
+         $actual = $bank[0]['fullname'];
+
+         $this->assertEquals($fullname, $actual);
+    }
+    //test findBankByIDng
+     public function testFindBankByIdNg(){
+        $bankModel = new BankModel();
+         $Id = 9999;
+
+         $bank = $bankModel ->findBankById($Id);
+         if(empty($bank)) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
+    }
+    //test findBankByIdStr
+    public function testFindBankByIdStr(){
+        $bankModel = new BankModel();
+         $Id = "adafa";
+
+         $bank = $bankModel ->findBankById($Id);
+         if(empty($bank)) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
+    }
+
+    //test findBankById string empty
+    public function testFindBankByIdStrEmpty(){
+        $bankModel = new BankModel();
+         $Id = "";
+
+         $bank = $bankModel ->findBankById($Id);
+         if(empty($bank)) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
+    }
+
+    //test findBankByIdNull
+    public function testFindBankByIdNull(){
+        $bankModel = new BankModel();
+         $Id = null;
+
+         $bank = $bankModel ->findBankById($Id);
+         if(empty($bank)) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
+    }
+    //test findBankByIdArr
+    public function testFindBankByIdArr(){
+        $bankModel = new BankModel();
+         $Id = [];
+
+         $bank = $bankModel ->findBankById($Id);
+         if(empty($bank)) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
+    }
+
+    //test deletBankByIdOk
+    public function testDeleteBankByIdOk(){
+        $bankModel = new BankModel();
+        $Id = 5;
+        $excuted = true;
+        $actual = $bankModel->deleteBanksById($Id);
+        $this->assertEquals($excuted,$actual);
+    }
+
+    //test deleteBankByIdNg
+    public function testDeleteBankByIdNg(){
+        $bankModel = new BankModel();
+        $Id = 999;
+        $excuted = true;
+        $actual = $bankModel->deleteBanksById($Id);
+        if($actual == false) {
+            $this->assertTrue(false);
+        } else {
+            $this->assertTrue(true);
+        }
+    }
+
+    
+    
+
+}
