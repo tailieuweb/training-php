5cd58f0eb823d1f71eb4bc77308bed22fe5d179b
PhanNhatQuy <phannhatquy.tdc2019@gmail.com>
2021-11-25 20:32:57 +0700

    fix-update test

--- tests/QuyUserModelTest.php
+++ tests/QuyUserModelTest.php
@@ -8,230 +8,89 @@ class QuyUserModelTest extends TestCase
     /**
      * Test case Okie
      */
-    public function testSumOk()
+    public function testGetUsersGood()
     {
         $userModel = new UserModel();
-        $a = 1;
-        $b = 2;
-        $expected = 3;
-
-        $actual = $userModel->sumb($a, $b);
-
+        $params['keyword']  = 'hackerasfasf';
+        $expected = 'hackerasfasf';
+        $user = $userModel->getUsers($params);
+        $actual = $user[0]['name'];
         $this->assertEquals($expected, $actual);
     }
-
     /**
      * Test case Not good
      */
-    public function testSumNg()
+    public function testGetUsersNg()
     {
         $userModel = new UserModel();
-        $a = 1;
-        $b = 2;
-
-        $actual = $userModel->sumb($a, $b);
-
-        if ($actual != 3) {
-            $this->assertTrue(false);
+        $params['keyword']  = 'qhuh';
+        $user = $userModel->getUsers($params);
+        if (empty($user[0])) {
+            return $this->assertTrue(true);
         } else {
-            $this->assertTrue(true);
+            return $this->assertTrue(false);
         }
     }
+   
     /**
-     * Test case so am
+     * Test keyword là null
      */
-    public function testSumAm()
+    public function testGetUsersNull()
     {
         $userModel = new UserModel();
-        $a = -1;
-        $b = -2;
-
-        $actual = $userModel->sumb($a, $b);
-
-        if ($actual != -3) {
-            $this->assertTrue(false);
-        } else {
-            $this->assertTrue(true);
-        }
-    }
-    /**
-     * Test case am duong
-     */
-    public function testSumAD()
-    {
-        $userModel = new UserModel();
-        $a = -1;
-        $b = 2;
-
-        $actual = $userModel->sumb($a, $b);
-
-        if ($actual != 1) {
-            $this->assertTrue(false);
-        } else {
-            $this->assertTrue(true);
-        }
-    }
-    /**
-     * Test case so thuc
-     */
-    public function testSumSoThuc()
-    {
-        $userModel = new UserModel();
-        $a = 3.2;
-        $b = 1.2;
-
-        $actual = $userModel->sumb($a, $b);
-
-        if ($actual != 4.4) {
-            $this->assertTrue(false);
-        } else {
-            $this->assertTrue(true);
-        }
-    }
-    /**
-     * Test case str
-     */
-    public function testStr()
-    {
-        $userModel = new UserModel();
-        $a = 1;
-        $b = 'a';
-
-        $expected = 'error';
-        $actual = $userModel->sumb($a, $b);
-
+        $params['keyword']  = null;
+        $expected = 'test2';
+        $user = $userModel->getUsers($params);
+        $actual = $user[0]['name'];
         $this->assertEquals($expected, $actual);
     }
-
     /**
-     * Test case strvsstr
+     * Test keyword bang gia tri dung hoac sai[boolean(true/false)]
      */
-    public function testChuoivsChuoi()
+    public function testGetUsersBoolean()
     {
         $userModel = new UserModel();
-        $a = 'bb';
-        $b = 'aa';
-
-        $expected = 'bbaa';
-        $actual = $userModel->sumb($a, $b);
-
+        $params['keyword']  = true;
+        $expected =[];
+        $actual = $userModel->getUsers($params);
         $this->assertEquals($expected, $actual);
     }
-    
-    // public function testInsertUserOk()
-    // {
-    //     $userModel = new UserModel();
-    //     $user = array(
-    //         'id' => 2,
-    //         'name' => 'test2',
-    //         'fullname' => '',
-    //         'type' => '',
-    //         'email' => '',
-    //         'password' => ''
-    //     );
-    //     $actual = $userModel->insertUser($user);
-    //     if ($actual == true) {
-    //         $this->assertTrue(true);
-    //     } else {
-    //         $this->assertTrue(false);
-    //     }
-    // }
-    
-
-    public function testGetUserOk()
-    {
-        $userModel = new UserModel();
-
-        $count_array = 4;
-        $actual = $userModel->getUsers();
-
-        $this->assertEquals($count_array, count($actual));
-    }
-    //test getUser khi khong co du lieu oke
-    public function testGetUserNullOK()
-    {
-        $userModel = new UserModel();
-
-        $actual = $userModel->getUsers();
-
-        if ($actual != null) {
-            $this->assertTrue(true);
-        } else {
-            $this->assertTrue(false);
-        }
-    }
-      //test getUser khi khong co du lieu false
-      public function testGetUserNullF()
-    {
-        $userModel = new UserModel();
-        $actual = null;
-        $actual = $userModel->getUsers();
-
-        if ($actual != null) {
-            $this->assertTrue(true);
-        } else {
-            $this->assertTrue(false);
-        }
-    }
-      //test getUser khi strNull ok
-      public function testGetUserStrNullOke()
-     {
-        $userModel = new UserModel();
-        $keyword = array(
-            'keyword' => null,
-        );
-        $actual = $userModel->getUsers($keyword);
-
-        if ($actual != []) {
-            $this->assertTrue(true);
-        } else {
-            $this->assertTrue(false);
-        }
-     }
-     //test getUser khi strNull F
-     public function testGetUserStrNullF()
-     {
-        $userModel = new UserModel();
-
-        $keyword = array(
-            'keyword' => 'id',
-            'keyword' => 'Username'
-        );
-        $actual = $userModel->getUsers($keyword);
-
-        if ($actual != []) {
-            $this->assertTrue(false);
-        } else {
-            $this->assertTrue(true);
-        }
-     }
-    public function testGetUserbyKeyWordOk()
-    {
-        $userModel = new UserModel();
-        $params= [];
-        $params['keyword'] = '';
-        $count_array = 4;
-        $actual = $userModel->getUsers( $params);
-
-        $this->assertEquals($count_array,count($actual));
-    }
-    // public function testDeleteUserByIdNg()
-    // {
-    //     $userModel = new UserModel();
-    //     $id = 20;
-
-
-    //     $actual = $userModel->deleteUserById($id);
-
-
-    //     // $this->assertEquals($userModel->findUserById($id),$actual);
-    //     if ($actual == true) {
-    //         $this->assertTrue(true);
-    //     } else {
-    //         $this->assertTrue(false);
-    //     }
-    // }
     /**
-     * Test findUserById Ok
+     * Test keyword bang ky tu dac biet
      */
+    public function testGetUsersKyTuDb()
+    {
+        $userModel = new UserModel();
+        $params['keyword']  = '#$$@';
+        $user = $userModel->getUsers($params);
+        if (empty($user[0])) {
+            return $this->assertTrue(true);
+        } else {
+            return $this->assertTrue(false);
+        }
+    }
+    
+    /**
+     * Test keyword là mang Array
+     */
+    public function testGetUsersArray()
+    {
+        $userModel = new UserModel();
+        $params['keyword'] = ['test2'];
+        $expected =[];
+        $actual = $userModel->getUsers($params[]);
+        $this->assertEquals($expected, $actual);
+    }
+    /**
+     * Test keyword là object
+     */
+
+    public function testGetUsersObject()
+    {
+        $userModel = new UserModel();
+        $params['keyword'] = new UserModel();
+        $expected = 'Invalid';
+        $actual = $userModel->getUsers($params);
+        $this->assertEquals($expected, $actual);
+    }
 }
