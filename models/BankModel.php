038a3aee004d71ff477323d15a3d1e180feec70e
leanhvu21042001@gmail.com <leanhvu1412>
2021-11-25 09:14:34 +0700

    Make testOK with insertUser_bank function

--- models/BankModel.php
+++ models/BankModel.php
@@ -2,22 +2,22 @@
 
 require_once 'BaseModel.php';
 $ds       = DIRECTORY_SEPARATOR;
-$base_dir = realpath(dirname(__FILE__).$ds.'..').$ds;
+$base_dir = realpath(dirname(__FILE__) . $ds . '..') . $ds;
 require_once("{$base_dir}models{$ds}IBank.php");
 class BankModel extends BaseModel implements IBank
 {
     public static function getInstance()
     {
-        if(self::$_instance !== null){
+        if (self::$_instance !== null) {
             return self::$_instance;
         }
         self::$_instance = new self();
         return self::$_instance;
     }
-    
+
     public function findBankById($id)
     {
-        
+
         $sql = 'SELECT * FROM bank WHERE id = ' . $id;
         $banks = $this->select($sql);
 
@@ -31,7 +31,7 @@ class BankModel extends BaseModel implements IBank
      */
     public function getBanks($params = [])
     {
-        
+
         //Keyword
         if (!empty($params['keyword'])) {
             $sql = 'SELECT * FROM bank WHERE name LIKE "%' . $params['keyword'] . '%"';
@@ -40,41 +40,92 @@ class BankModel extends BaseModel implements IBank
             //Don't change
             //Example keyword: abcef%";TRUNCATE banks;##
             $banks = $this->select($sql);
-            
         } else {
             $sql = 'SELECT * FROM bank';
             $banks = $this->select($sql);
-
         }
 
         return $banks;
     }
 
-    public function insertUser_bank($input) {
-        $sql = "INSERT INTO `bank` (`name`, `fullname`, `sdt`, `email`, `stk`) VALUES (" .
-            "'" . $input['name'] . "', '".$input['fullname']."','".$input['sdt']."', '".$input['email']."','".$input['stk']."')";
+    public function insertUser_bank($input)
+    {
+        // bắt lỗi số lượng value request.
+        if (count($input) !== 5) {
+            return "Số lượng tham số truyền vào không phù hợp";
+        }
 
-        $user = $this->insert($sql);
-        return $user;
+        // bắt lỗi câu truy vấn.
+        $name = mysqli_real_escape_string(parent::$_connection, $input["name"]);
+        $fullname = mysqli_real_escape_string(parent::$_connection, $input["fullname"]);
+        $sdt = mysqli_real_escape_string(parent::$_connection, $input["sdt"]);
+        $email = mysqli_real_escape_string(parent::$_connection, $input["email"]);
+        $stk = mysqli_real_escape_string(parent::$_connection, $input["stk"]);
+
+        // bắt lỗi độ dài value.
+        if (strlen($name) < 8 || strlen($name) > 120) {
+            return "Name must be between 8 and 120";
+        }
+
+        // bắt lỗi độ dài value.
+        if (strlen($fullname) < 8 || strlen($fullname) > 120) {
+            return "Fullname must be between 8 and 120";
+        }
+
+        // bắt lỗi email không hợp lệ (cũng như độ dài chuỗi)
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return "Email is not valid";
+        }
+
+        // stk bị rỗng.
+        if (strlen($stk) == 0) {
+            return "Card number is not valid";
+        }
+
+        // bắt lỗi stk không phải là số.
+        if (!is_numeric($stk)) {
+            return "Card number is not valid";
+        }
+
+        // bắt lỗi tồn tại "số tài khoản".
+        $sql = "SELECT * FROM bank WHERE `stk` = $stk";
+        $banks = $this->select($sql);
+        if (count($banks) > 0) {
+            return "Card number is exists";
+        }
+
+        $sql = "INSERT INTO `bank` (`name`, `fullname`, `sdt`, `email`, `stk`) 
+        VALUES (?, ?, ?, ?, ?)";
+        $result = false;
+
+        // bắt lỗi câu truy vấn.
+        $stmt = mysqli_stmt_init(parent::$_connection);
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+            $result = false;
+        } else {
+            mysqli_stmt_bind_param($stmt, "sssss", $name, $fullname, $sdt, $email, $stk);
+            $result = mysqli_stmt_execute($stmt);
+        }
+
+        return $result;
     }
 
-    public function updateUser_bank($input) {
+    public function updateUser_bank($input)
+    {
         $sql = 'UPDATE bank SET 
-                 name = "' . $input['name'] .'", 
-                 fullname = "'. $input['fullname'].'",
-                 email = "' . $input['email'] .'", 
-                 sdt = "' . $input['sdt'] .'", 
-                 stk="'. $input['stk'].'"
+                 name = "' . $input['name'] . '", 
+                 fullname = "' . $input['fullname'] . '",
+                 email = "' . $input['email'] . '", 
+                 sdt = "' . $input['sdt'] . '", 
+                 stk="' . $input['stk'] . '"
                 WHERE id = ' . $input['id'];
-        $user = $this->update($sql);
+        $result = $this->update($sql);
 
-        return $user;
+        return $result;
     }
 
     public function cost()
     {
         return $this->getBanks(null);
     }
-
-
 }
--- tests/BankModelTest.php
+++ tests/BankModelTest.php
@@ -4,5 +4,217 @@ use PHPUnit\Framework\TestCase;
 
 class BankModelTest extends TestCase
 {
-    
+
+  // tham số truyền vào nhiều hơn 5.
+  public function testInsertUser_bank_more_than_5_paramOk()
+  {
+    $bankModel = new BankModel();
+    $input = array(
+      "name" => "lav",
+      "fullname" => "le anh vu",
+      "sdt" => "1234",
+      "email" => "lav@gmail.com",
+      "stk" => "123",
+      "bank" => 'test'
+    );
+
+    $expected = "Số lượng tham số truyền vào không phù hợp";
+    $actual = $bankModel->insertUser_bank($input);
+
+    $this->assertEquals($expected, $actual);
+  }
+
+  // tham số truyền vào ít hơn 5.
+  public function testInsertUser_bank_less_than_5_paramOk()
+  {
+    $bankModel = new BankModel();
+    $input = array(
+      "name" => "lav",
+      "fullname" => "le anh vu",
+      "sdt" => "1234",
+      "email" => "lav@gmail.com",
+    );
+
+    $expected = "Số lượng tham số truyền vào không phù hợp";
+    $actual = $bankModel->insertUser_bank($input);
+
+    $this->assertEquals($expected, $actual);
+  }
+
+  // độ dài name lớn hơn 8
+  public function testInsertUser_bank_name_length_more_than_8_paramOk()
+  {
+    $bankModel = new BankModel();
+    $input = array(
+      "name" => "lavla",
+      "fullname" => "le anh vu",
+      "sdt" => "1234",
+      "email" => "lav@gmail.com",
+      "stk" => "123"
+    );
+
+    $actual = $bankModel->insertUser_bank($input);
+    $expected = "Name must be between 8 and 120";
+    $this->assertEquals($expected, $actual);
+  }
+  // độ dài name bé hơn 120
+  public function testInsertUser_bank_name_length_less_than_120_paramOk()
+  {
+    $bankModel = new BankModel();
+
+    // độ dài name đang 240
+    $input = array(
+      "name" => "lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345",
+      "fullname" => "le anh vu",
+      "sdt" => "1234",
+      "email" => "lav@gmail.com",
+      "stk" => "123"
+    );
+
+    $actual = $bankModel->insertUser_bank($input);
+    $expected = "Name must be between 8 and 120";
+    $this->assertEquals($expected, $actual);
+  }
+
+  // độ dài fullname lớn hơn 8
+  public function testInsertUser_bank_fullname_length_more_than_8_paramOk()
+  {
+    $bankModel = new BankModel();
+    $input = array(
+      "name" => "lavla",
+      "fullname" => "le vu",
+      "sdt" => "1234",
+      "email" => "lav@gmail.com",
+      "stk" => "123"
+    );
+
+    $actual = $bankModel->insertUser_bank($input);
+    $expected = "Name must be between 8 and 120";
+    $this->assertEquals($expected, $actual);
+  }
+
+  // độ dài fullname bé hơn 120
+  public function testInsertUser_bank_fullname_length_less_than_120_paramOk()
+  {
+    $bankModel = new BankModel();
+
+    // độ dài name đang 240
+    $input = array(
+      "name" => "lavla",
+      "fullname" => "lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345lavla12345",
+      "sdt" => "1234",
+      "email" => "lav@gmail.com",
+      "stk" => "123"
+    );
+
+    $actual = $bankModel->insertUser_bank($input);
+    $expected = "Name must be between 8 and 120";
+    $this->assertEquals($expected, $actual);
+  }
+
+  // email khoong hợp lệ.
+  public function testInsertUserBankWithErrorEmailOk()
+  {
+    $bankModel = new BankModel();
+    $input = array(
+      "name" => "leanhvu1412",
+      "fullname" => "le anh vu",
+      "sdt" => "1234",
+      "email" => "lav@gmail",
+      "stk" => "123"
+    );
+
+    $expected = "Email is not valid";
+    $actual = $bankModel->insertUser_bank($input);
+    $this->assertEquals($expected, $actual);
+  }
+  
+  // email bị rỗng.
+  public function testInsertUserBankWithEmptyEmailOk()
+  {
+    $bankModel = new BankModel();
+    $input = array(
+      "name" => "leanhvu1412",
+      "fullname" => "le anh vu",
+      "sdt" => "1234",
+      "email" => "",
+      "stk" => "123"
+    );
+
+    $expected = "Email is not valid";
+    $actual = $bankModel->insertUser_bank($input);
+    $this->assertEquals($expected, $actual);
+  }
+
+  // stk bị rỗng.
+  public function testInsertUserBankWithEmptyStkOk()
+  {
+    $bankModel = new BankModel();
+    $input = array(
+      "name" => "leanhvu1412",
+      "fullname" => "le anh vu",
+      "sdt" => "1234",
+      "email" => "leanhvu@gmail.com",
+      "stk" => ""
+    );
+
+    $expected = "Card number is not valid";
+    $actual = $bankModel->insertUser_bank($input);
+    $this->assertEquals($expected, $actual);
+  }
+
+  // stk không phải kiểu số.
+  public function testInsertUserBankWithStkIsNotNumberOk()
+  {
+    $bankModel = new BankModel();
+    $input = array(
+      "name" => "leanhvu1412",
+      "fullname" => "le anh vu",
+      "sdt" => "1234",
+      "email" => "leanhvu@gmail.com",
+      "stk" => "123aA"
+    );
+
+    $expected = "Card number is not valid";
+    $actual = $bankModel->insertUser_bank($input);
+
+    $this->assertEquals($expected, $actual);
+  }
+
+  // trùng stk.
+  public function testInsertUserBankWithSameStkOk()
+  {
+    $bankModel = new BankModel();
+    $input = array(
+      "name" => "leanhvu1412",
+      "fullname" => "le anh vu",
+      "sdt" => "1234",
+      "email" => "leanhvu@gmail.com",
+      "stk" => "123"
+    );
+
+    $expected = "Card number is exists";
+    $actual = $bankModel->insertUser_bank($input);
+
+    $this->assertEquals($expected, $actual);
+  }
+
+  // kiểm tra trường hợp câu truy vấn không bình thường.
+  // giá trị truyền vào không bình thường.
+  public function testInsertUserBankWithErrorParamsOk()
+  {
+    $bankModel = new BankModel();
+    $input = array(
+      "name" => "';######",
+      "fullname" => ";;##------",
+      "sdt" => "1234",
+      "email" => "lav@gmail.com",
+      "stk" => "12314567"
+    );
+
+    $expected = true;
+    $actual = $bankModel->insertUser_bank($input);
+
+    $this->assertEquals($expected, $actual);
+  }
 }
