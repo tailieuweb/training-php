f381bb05f600499851e0bdeec5817dc76aba9fad
duc622 <duc.phan622@gmail.com>
2021-11-26 12:02:09 +0700

    update

--- configs/database.php
+++ configs/database.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
+define('DB_HOST', 'remotemysql.com');
+define('DB_USER', 'c2tqLmlZ7r');
+define('DB_PASSWORD', 'IOVdO2KKId');
 define('DB_PORT', 3306);
-define('DB_NAME', 'app_web1_new');
+define('DB_NAME', 'c2tqLmlZ7r');
