5e682f12d8ff06d4df635b583087ea62c773a7ef
My Trinh <lemytrinh021@gmail.com>
2021-12-02 08:26:48 +0700

    update testcase

--- tests/BankModelTest.php
+++ tests/BankModelTest.php
@@ -81,136 +81,151 @@ class BankModelTest extends TestCase
     }
     //---------------------------------------------------
     /**
-     * Test function GetBanks
+     * test function GetBank
      */
-    //Test getBankOk
+    //test getBankOk
     //test trường hợp lấy ra tất cả id của bank
-    public function testGetBanksOk()
-    {
+    public function testGetBanksOk(){
         $bankModel = new BankModel();
-
-        for ($i = 0, $max = 4; $i <= $max; $i++) {
+        
+        for($i = 0,$max = 4; $i <= $max;$i++){
             $count_array = $i;
         }
         //$i += 1;
-        $actual = $bankModel->getBanks();
+        $actual = $bankModel-> getBanks();
         //var_dump($actual);die();
         //Count() goi tong gia tri trong mang
-        $this->assertEquals($count_array, count($actual));
+        $this->assertEquals($count_array,count($actual));
     }
     //lấy đối ra danh sách của Bank theo đối tượng
-    public function testGetBankObject()
-    {
+    public function testGetBankObject(){
         $bank = new BankModel();
         $bankId = new stdClass();
-        try {
+        try{
             $bank->getBanks($bankId);
-        } catch (Throwable $ex) {
+        }
+            catch(Throwable $ex){
             $this->assertTrue(True);
         }
-    }
+     }
     //test kiểm tra gia trị có tồn tại trong mảng hay không
-    public function testGetBanksNg1()
-    {
+    public function testGetBanksNg1(){
         $bankModel = new BankModel();
         $UserIdInBank = 3;
-        $array = array("bank_id" => 22, "user_id" => 3, "cost" => 1);
+        $array = array("id"=>22,"user_id"=>3,"cost"=>1);
         //kiểm tra giá trị tồn tại trong mang
         $expected = in_array(3, $array);
-        $actual = $bankModel->getBanks($expected);
+        $actual = $bankModel -> getBanks($expected);
         //var_dump($actual);die();
 
-        if ($actual == true) {
-            $this->assertTrue(true);
-        } else {
-            $this->assertTrue(false);
+        if( $actual == true)
+        {
+            $this->assertTrue(true); 
+        }
+        else{
+            $this->assertTrue(false); 
         }
     }
     // nếu trả về giá tri trùng lặp cụ thể là user_id thì sẽ bị xóa-> thì sẽ báo sai
-    public function testGetBanksNg2()
-    {
+    public function testGetBanksNg2(){
         $bankModel = new BankModel();
-        $array1 = array("bank_id" => 22, "user_id" => 3, "cost" => 1);
-        $array2 = array("bank_id" => 26, "user_id" => 3, "cost" => 1);
+        $array1 = array("id"=>22,"user_id"=>3,"cost"=>1);
+        $array2 = array("id"=>26,"user_id"=>3,"cost"=>1);
         //kiểm tra giá trị trùng
-        $expected = array_diff_key($array1, $array2);
-        $actual = $bankModel->getBanks($expected);
+        $expected = array_diff_key($array1,$array2);
+        $actual = $bankModel -> getBanks($expected);
         //var_dump($actual);die();
-        if ($actual != true) {
-            $this->assertTrue(false);
-        } else {
-            $this->assertTrue(true);
+        if( $actual != true)
+        {
+            $this->assertTrue(false); 
+        }
+        else{
+            $this->assertTrue(true); 
         }
     }
     //test keyword ok in banks
-    //Kiểm tra giá trị tìm kiếm có tồn tại trong mảng hay không
+    //kiem tra gia trị trim kiem co ton tai trong mang kkhong
     public function testGetBanksByKeyWordOk()
     {
         $bankModel = new BankModel();
-        $params = [];
+        $params= [];
         $params['keyword'] = 22;
         $count_array = 8;
         $actual = $bankModel->getBanks($params);
-
-        $this->assertEquals($count_array, $actual);
+         
+        $this->assertEquals($count_array,$actual);
     }
-    //Kiểm tra giá trị tìm kiếm trong mảng đã được nhập chưa
+    //kiem tra gia tri tim kiem trong mang da duocj nhap chưa
     //isset T or empty F
     public function testGetBanksByKeyWordOk2()
     {
         $bankModel = new BankModel();
-        $params = [];
+        $params= [];
         $params['keyword'] = '22';
         $count_array = 8;
         $actual = $bankModel->getBanks($params);
-        //ham insset kiem tra bien da dược tạo trong bộ nhớ máy hay chưa
-        $this->assertEquals($count_array, isset($actual));
+         //ham insset kiem tra bien da dược tạo trong bộ nhớ máy hay chưa
+        $this->assertEquals($count_array,isset($actual));
     }
     // test nhập giá trị value và tìm kiếm giá trị trong mảng
-    public function testGetBanksByKeyWordNg1()
-    {
+    public function testGetBanksByKeyWordNg1(){
         $bankModel = new BankModel();
         $params = [];
         $params["keyword"] = 22;
-        $sreachArrayId = array("bank_id" => 22, "user_id" => 3, "cost" => 1);
+        $sreachArrayId = array("id"=>22,"user_id"=>3,"cost"=>1);
         //array_sreach tìm kiếm giá trị
         $expecSreach = array_search($params, $sreachArrayId);
         //$expecSreach = array_key_exists($params, $sreachArrayId);
-        $actual = $bankModel->getBanks($expecSreach);
-        //var_dump($actual);die();
-        if ($actual != true) {
-            $this->assertTrue(false);
-        } else {
-            $this->assertTrue(true);
+        try {
+            $actual = $bankModel -> getBanks($expecSreach);
+            //var_dump($actual);die();
+            if( $actual != true)
+            {
+                $this->assertTrue(false); 
+            }
+            else{
+                $this->assertTrue(true); 
+            }
+        } catch (Throwable $th) {
+            $this->assertTrue(true); 
         }
     }
     //test giá trị nhập vào là rỗng su dung empty
-    public function testGetBanksByKeyWordNg2()
-    {
+    public function testGetBanksByKeyWordNg2(){
         $bankModel = new BankModel();
         $params = [];
-        $params["keyword"] = "";
-        $actual = $bankModel->getBanks($params);
-        //nếu kiểm tra gia tri nhập vào khác rỗng là T
-        if (!empty($actual)) {
-            return $this->assertTrue(true);
-        } else {
-            return $this->assertTrue(false);
+        $params["keyword"] = " ";
+        try {
+            $actual = $bankModel ->getBanks($params);
+            //nếu kiểm tra gia tri nhập vào khác rỗng là T
+            if(!empty($actual)){
+                return $this -> assertTrue(true);
+            }
+            else{
+                return $this -> assertTrue(false);
+            }
+        } catch (Throwable $th) {
+            return $this -> assertTrue(true);
         }
     }
     //test trường hợp xác đinh từ khóa trong mảng cần tìm có tồn tại hay không
-    public function testGetBanksByKeyWordNg3()
-    {
+    public function testGetBanksByKeyWordNg3(){
         $bankModel = new BankModel();
-        $sreachArrayId = array("bank_id" => 3, "user_id" => 2, "cost" => 100);
-        //array_key_exists xác định gia tri mang co ton tai hay khong va can tim
+        $sreachArrayId = array("id"=>3,"user_id"=>2,"cost"=>100);
+         //array_key_exists xác định gia tri mang co ton tai hay khong va can tim
         $expecSreach = array_key_exists("user_id", $sreachArrayId);
-        $actual = $bankModel->getBanks($expecSreach);
-        //var_dump($actual);die();
-        if ($actual != true) {
-            $this->assertTrue(false);
-        } else {
-            $this->assertTrue(true);
+        try {
+            $actual = $bankModel -> getBanks($expecSreach);
+            //var_dump($actual);die();
+            if( $actual != true)
+            {
+                $this->assertTrue(false); 
+            }
+            else{
+                $this->assertTrue(true); 
+            }
+        } catch (Throwable $ex) {
+            $this->assertTrue(true); 
         }
     }
     //---------------------------------------------------
@@ -224,9 +239,9 @@ class BankModelTest extends TestCase
         // $idbank = 3;
 
         $input = [];
-        $input['bank_id'] = 3;
+        $input['id'] = 3;
         //note user_id == id of users 
-        $input['id'] = 2;
+        $input['user_id'] = 2;
         $input['cost'] = 123456789;
         $actual = $bankModel->updateBank($input);
         if ($actual != true) {
@@ -239,8 +254,8 @@ class BankModelTest extends TestCase
     public function testUpdateBankNG2()
     {
         $bankModel = new BankModel();
-        $input = array("bank_id" => 6, "id" => 3, "cost" => 11111);
-        $input["bank_id"] = 654321;
+        $input = array("id" => 1, "user_id" => 1, "cost" => 2222);
+        $input["user_id"] = 654321;
 
         $actual = $bankModel->updateBank($input);
         if ($actual != true) {
@@ -254,7 +269,7 @@ class BankModelTest extends TestCase
     public function testUpdateBankNG3()
     {
         $bankModel = new BankModel();
-        $input = array("bank_id" => 19, "id" => 40, "cost" => 11111);
+        $input = array("id" => 2, "user_id" => 1, "cost" => 11111);
         $input["cost"] = 654321;
         $checkCost = is_numeric($input['cost']);
 
@@ -270,7 +285,7 @@ class BankModelTest extends TestCase
     public function testUpdateBankNG4()
     {
         $bankModel = new BankModel();
-        $input = array("bank_id" => 19, "id" => 40, "cost" => 11111);
+        $input = array("id" => 19, "user_id" => 40, "cost" => 11111);
         $input["cost"] = 654321;
         $valueFill = array_fill(3, 1, $input["cost"]);
 
@@ -281,4 +296,86 @@ class BankModelTest extends TestCase
             $this->assertTrue(true);
         }
     }
+    /*updateNG5*/
+    /*cập nhật gái trị id thành giá trị rỗng */
+    public function testUpdateBankNG5() {
+        $bank = new BankModel();
+        $input = [];
+        $input['id'] = null;
+        $input['user_id'] = 2;
+        $input['cost'] = 544;
+        $actual = $bank->updateBank($input);
+
+        // var_dump($actual);die();
+        if ($actual == true) {
+            return $this->assertFalse(true);
+        }
+        return $this->assertFalse(false);
+		  
+	}
+    /*cập nhật userId có giá trị rỗng */
+
+    public function testUpdateBankNG6() {
+        $bank = new BankModel();
+        $input = [];
+        $input['id'] = 3 ;
+        $input['user_id'] = null ;
+        $input['cost'] = 88888;
+        $actual = $bank->updateBank($input);
+
+        // var_dump($actual);die();
+        if ($actual == true) {
+            return $this->assertFalse(true);
+        }
+        return $this->assertFalse(false);
+		  
+	}
+    /*cập nhật giá trị cost null */
+    public function testUpdateBankNG7() {
+        $bank = new BankModel();
+        $input = [];
+        $input['id'] = 3 ;
+        $input['user_id'] = 2 ;
+        $input['cost'] = null;
+        $actual = $bank->updateBank($input);
+
+        // var_dump($actual);die();
+        if ($actual == true) {
+            return $this->assertFalse(true);
+        }
+        return $this->assertFalse(false);
+		  
+	}
+    /*cả 3 đều rỗng */
+    public function testUpdateBankNG8() {
+        $bank = new BankModel();
+        $input = [];
+        $input['id'] = null ;
+        $input['user_id'] = null ;
+        $input['cost'] = null;
+        $actual = $bank->updateBank($input);
+
+        // var_dump($actual);die();
+        if ($actual == true) {
+            return $this->assertFalse(true);
+        }
+        return $this->assertFalse(false);
+		  
+	}
+    /*câp nhật vs giá trị mảng truyền vào là chuỗi */
+    public function testUpdateBankNG9() {
+        $bank = new BankModel();
+        $input = [];
+        $input['id'] = 'null' ;
+        $input['user_id'] = 'null' ;
+        $input['cost'] = 'null';
+        $actual = $bank->updateBank($input);
+
+        // var_dump($actual);die();
+        if ($actual == true) {
+            return $this->assertFalse(true);
+        }
+        return $this->assertFalse(false);
+		  
+	}
 }
--- tests/UserModelTest.php
+++ tests/UserModelTest.php
@@ -1,4 +1,5 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
 
 class UserModelTest extends TestCase
@@ -9,14 +10,14 @@ class UserModelTest extends TestCase
      */
     public function testSumOk()
     {
-       $userModel = new UserModel();
-       $a = 1;
-       $b = 2;
-       $expected = 3;
+        $userModel = new UserModel();
+        $a = 1;
+        $b = 2;
+        $expected = 3;
 
-       $actual = $userModel->sumb($a,$b);
+        $actual = $userModel->sumb($a, $b);
 
-       $this->assertEquals($expected, $actual);
+        $this->assertEquals($expected, $actual);
     }
 
     /**
@@ -28,7 +29,7 @@ class UserModelTest extends TestCase
         $a = 1;
         $b = 2;
 
-        $actual = $userModel->sumb($a,$b);
+        $actual = $userModel->sumb($a, $b);
 
         if ($actual != 3) {
             $this->assertTrue(false);
@@ -39,62 +40,62 @@ class UserModelTest extends TestCase
 
     public function testSumOkam()
     {
-       $userModel = new UserModel();
-       $a = -1;
-       $b = -2;
-       $expected = -3;
+        $userModel = new UserModel();
+        $a = -1;
+        $b = -2;
+        $expected = -3;
 
-       $actual = $userModel->sumb($a,$b);
+        $actual = $userModel->sumb($a, $b);
 
-       $this->assertEquals($expected, $actual);
+        $this->assertEquals($expected, $actual);
     }
 
     public function testSumOkad()
     {
-       $userModel = new UserModel();
-       $a = -1;
-       $b = 2;
-       $expected = 1;
+        $userModel = new UserModel();
+        $a = -1;
+        $b = 2;
+        $expected = 1;
 
-       $actual = $userModel->sumb($a,$b);
+        $actual = $userModel->sumb($a, $b);
 
-       $this->assertEquals($expected, $actual);
+        $this->assertEquals($expected, $actual);
     }
 
     public function testSumOkDouble()
     {
-       $userModel = new UserModel();
-       $a = 1.5;
-       $b = 2.5;
-       $expected = 4;
+        $userModel = new UserModel();
+        $a = 1.5;
+        $b = 2.5;
+        $expected = 4;
 
-       $actual = $userModel->sumb($a,$b);
+        $actual = $userModel->sumb($a, $b);
 
-       $this->assertEquals($expected, $actual);
+        $this->assertEquals($expected, $actual);
     }
 
     public function testStr()
     {
-       $userModel = new UserModel();
-       $a = 1;
-       $b = 'a';
-       $expected = 'error';
+        $userModel = new UserModel();
+        $a = 1;
+        $b = 'a';
+        $expected = 'error';
 
-       $actual = $userModel->sumb($a,$b);
+        $actual = $userModel->sumb($a, $b);
 
-       $this->assertEquals($expected, $actual);
+        $this->assertEquals($expected, $actual);
     }
 
     public function testString()
     {
-       $userModel = new UserModel();
-       $a = 'a';
-       $b = 'b';
-       $expected = 'error';
+        $userModel = new UserModel();
+        $a = 'a';
+        $b = 'b';
+        $expected = 'error';
 
-       $actual = $userModel->sumb($a,$b);
+        $actual = $userModel->sumb($a, $b);
 
-       $this->assertEquals($expected, $actual);
+        $this->assertEquals($expected, $actual);
     }
 
     /**
@@ -102,39 +103,200 @@ class UserModelTest extends TestCase
      */
     public function testInsertUserOk()
     {
-      $userModel = new UserModel();
-      $userId = 6;
-      $input['name'] = 'le';
-      $input['password']  = '1234';
-      $input['fullname'] = 'lenguyentan';
-      $input['email'] = 'tanle123@gmail.com';
-      $input['type'] = 'admin';
-      $userModel->insertUser($input);
-      $expected = $userModel->findUserById($userId);
-      $actual = $expected[4]['name']['password']['fullname']['email']['type'];
-      //var_dump($actual); die();
-      $this->assertEquals($input['name']['password']['fullname']['email']['type'], $actual);
+        $userModel = new UserModel();
+        $userId = 6;
+        $input['name'] = 'le';
+        $input['password']  = '1234';
+        $input['fullname'] = 'lenguyentan';
+        $input['email'] = 'tanle123@gmail.com';
+        $input['type'] = 'admin';
+        $userModel->insertUser($input);
+        $expected = $userModel->findUserById($userId);
+        $actual = $expected[4]['name']['password']['fullname']['email']['type'];
+        //var_dump($actual); die();
+        $this->assertEquals($input['name']['password']['fullname']['email']['type'], $actual);
     }
 
-   /**
+    /**
      * Test case insertUser Not good
      */
     public function testInsertUserNg()
     {
-      $userModel = new UserModel();
-      $input['name'] = 'tanle';
-      $input['password']  = '12345';
-      $input['fullname'] = 'nguyentanle';
-      $input['email'] = 'tanle@gmail.com';
-      $input['type'] = 'user';
-      
-      $user = $userModel->insertUser($input);
-      $expected = $userModel->findUserById(6);
-      if($expected != null){
-         $this->assertTrue(true);
-      }
-      else{
-         $this->assertFalse(false);
-      }
+        $userModel = new UserModel();
+        $input['name'] = 'tanle';
+        $input['password']  = '12345';
+        $input['fullname'] = 'nguyentanle';
+        $input['email'] = 'tanle@gmail.com';
+        $input['type'] = 'user';
+
+        $user = $userModel->insertUser($input);
+        $expected = $userModel->findUserById(6);
+        if ($expected != null) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertFalse(false);
+        }
+    }
+    //-------------------------------
+    /**
+     * Test function auth is right
+     */
+    public function testAuthOk()
+    {
+        $user = new UserModel();
+        $name = "Trinh";
+        $pass = "11111";
+
+        $actual = $user->auth($name, $pass);
+
+        if ($actual == true) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
+    }
+    /**
+     * Test function auth with name worng
+     */
+    public function testAuthNameNg()
+    {
+        $user = new UserModel();
+        $name = "Trinh";
+        $pass = "11111";
+
+        $actual = $user->auth($name, $pass);
+        $expected = true;
+        $this->assertEquals($expected, $actual);
+    }
+    /**
+     * Test function auth with password worng
+     */
+
+    /**
+     * Các trường hợp cần test
+     * truyền đúng u & p
+     * truyền sai u
+     * truyền sai pass
+     * truyền sai u và p
+     * truyền sai kiểu dữ liệu u (array, object)
+     * truyền sai kiểu dữ liệu p (array, object)
+     * truyền sai kiểu dữ liệu u & p (array, object)
+     */
+    public function testAuthPassGood()
+    {
+        $user = new UserModel();
+        $name = "trinhnulo";
+        $pass = "123";
+        //Setup input for insert new user
+        $input = [
+            'name' => "trinhnulo",
+            'password' => "123",
+            'fullname' => "le my trinh",
+            'email' => "123",
+            'type' => "admin"
+        ];
+        //Start transaction
+        $user->startTransaction();
+
+        //Execute function insert user
+        $user->insertUser($input);
+        // $newUser = $user->findUserById(6);
+        // var_dump($newUser);
+
+        //Test
+        $actual = $user->auth($name, $pass);
+        $this->assertEquals($actual[0]['name'], "trinhnulo");
+        $user->rollback();
+
+        //Test
+
+        // die();
+        // $actual = $user->auth($name, $pass);
+        // var_dump(md5($pass));die();
+        // $expected = true;
+        // $this->assertEquals($expected, $actual);
+    }
+    //Test auth invalid input name
+    public function testAuthInvalidUser()
+    {
+        $user = new UserModel();
+        $name = "trinhnulo";
+        $pass = "123";
+        //Setup input for insert new user
+        $input = [
+            'name' => "trinhnulo",
+            'password' => "123",
+            'fullname' => "le my trinh",
+            'email' => "123",
+            'type' => "admin"
+        ];
+        //Start transaction
+        $user->startTransaction();
+
+        //Execute function insert user
+        $user->insertUser($input);
+
+        //Test
+        $actual = $user->auth($name, $pass);
+        if ($actual[0]['name'] == "trinhnulo") {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
+        $user->rollback();
+    }
+
+    //Test auth invalid input pass
+    public function testAuthPassNG()
+    {
+        $user = new UserModel();
+        $name = "trinhnulo";
+        $pass = "123";
+        //Setup input for insert new user
+        $input = [
+            'name' => "trinhnulo",
+            'password' => "123",
+            'fullname' => "le my trinh",
+            'email' => "123",
+            'type' => "admin"
+        ];
+        //Start transaction
+        $user->startTransaction();
+
+        //Execute function insert user
+        $user->insertUser($input);
+        // $newUser = $user->findUserById(6);
+        // var_dump($newUser);
+
+        //Test
+        $actual = $user->auth($name, $pass);
+        $this->assertEquals($actual[0]['password'], (md5("123")));
+        $user->rollback();
+    }
+    public function testAuthWithAllNull()
+    {
+        $user = new UserModel();
+        $name = "trinhnulo";
+        $pass = "123";
+        //Setup input for insert new user
+        $input = [
+            'name' => "trinhnulo",
+            'password' => "123",
+            'fullname' => "le my trinh",
+            'email' => "123",
+            'type' => "admin"
+        ];
+        //Start transaction
+        $user->startTransaction();
+
+        //Execute function insert user
+        $user->insertUser($input);
+        // $newUser = $user->findUserById(6);
+        // var_dump($newUser);
+
+        //Test
+        $actual = $user->auth($name, $pass);
+        $this->assertEquals($actual[]['name'], "trinhnulo");
+        $user->rollback();
     }
 }
