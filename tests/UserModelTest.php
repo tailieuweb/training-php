65ba2987cb2ec5548594ff8add52d90fb5105818
mrluongdz <vdluong2k.tdc.vn@gmail.com>
2021-12-03 10:48:52 +0700

    update

--- tests/UserModelTest.php
+++ tests/UserModelTest.php
@@ -291,5 +291,92 @@ class UserModelTest extends TestCase
           $actual = $userModel->findUser($key);
           $this->assertEquals($expected, $actual);
       }
-      
+      public function testUpdateUserOK(){
+        $userModel = new UserModel();
+        $user = array(  
+            'id' => 4,        
+            'name' => 'dangduyhuy',
+            'fullname' => 'DangDuyHuy',
+            'email' => 'duyhuy@gmail.com',
+            'type' => 'admin',
+            'password' => md5('dangduyhuy')
+        );         
+        $expected = true;
+        $actual = $userModel->updateUser($user);
+        $this->assertEquals($actual, $expected);   
+    }
+    public function testUpdateUserNGNull(){
+        $userModel = new UserModel();
+        $user = array(
+            'id' => '',        
+            'name' => '',
+            'fullname' => '',
+            'type' => '',
+            'email' => '',
+            'password' => md5('')
+        );
+        $expected = true;
+        $actual = $userModel->updateUser($user);
+        $this->assertEquals($expected,$actual); 
+        if(!empty($user['name']) && !empty($user['fullname']) && !empty($user['type']) && !empty($user['email']) && !empty($user['password'])){            
+            $this->assertTrue(false);
+        }else{
+            $this->assertTrue(true);
+        }   
+    }
+    public function testUpdateUserSpecialCharactersNg(){
+        $userModel = new UserModel();
+        $pattern = '/[0-9A-Za-z]/';
+        $user = array(
+            'id' => 5,
+            'name' => "@#$",
+            'fullname' =>'DangDuyHuy',
+            'email' => 'duyhuy@gmail.com',
+            'type' => 'admin',
+            'password' => md5('user3')
+        );
+        $userModel->updateUser($user);
+        if(!preg_match($pattern, $user['name'])){  
+            
+            $this->assertTrue(true);
+        }else{
+            $this->assertTrue(false);
+        }   
+    }
+    public function testUpdateUserFloatNg(){
+        $userModel = new UserModel();
+        $user = array(
+            'id' => 5,
+            'name' => 2.5,
+            'fullname' =>'DangDuyHuy',
+            'email' => 'duyhuy@gmail.com',
+            'type' => 'admin',
+            'password' => md5('user3')
+        );
+        $userModel->updateUser($user);
+        if(is_float($user['name']) || is_float($user['fullname']) || is_float($user['email']) || is_float($user['type']) || is_float($user['password'])){  
+            
+            $this->assertTrue(true);
+        }else{
+            $this->assertTrue(false);
+        }   
+    }
+    public function testUpdateUserBool(){
+        $userModel = new UserModel();
+        $user = array(
+            'id' => 74,
+            'name' => true,
+            'fullname' =>false,
+            'email' => 'duyhuy@gmail.com',
+            'type' => 'admin',
+            'password' => md5('user3')
+        );
+        $userModel->updateUser($user);
+        if(is_bool($user['name']) || is_bool($user['fullname']) || is_bool($user['email']) || is_bool($user['type']) || is_bool($user['password'])){  
+            
+            $this->assertTrue(true);
+        }else{
+            $this->assertTrue(false);
+        }   
+    }
 }
