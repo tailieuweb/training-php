1871e514c867f82688e277be184a76f32bc51028
nguyenminhantdc1999 <toithap1999@gmail.com>
2021-12-03 09:13:46 +0700

    c

--- tests/BankModelTest.php
+++ tests/BankModelTest.php
@@ -3,6 +3,100 @@ use PHPUnit\Framework\TestCase;
 
 class BankModelTest extends TestCase
 {
+
+ /*Test  insert nhập sai Bank Not OK*/
+     public function testInsertBankNG()
+    {
+     
+    $bankModel = new BankModel();
+    $actual = null;
+     $bank = array(
+        'user_id' => '6',
+        'cost' => '123',
+    );
+    try {
+      $actual = $bankModel->insertBanks('abcdefgh',   $bankModel);
+   } catch (Throwable $e) {
+     $excute = false;
+    }
+     $this->assertEquals($excute, $actual);
+}
+/*Test insert bank truyền vào chuỗi*/
+    public function testInsertBankStringNotOK()
+     {
    
-   
+   $bankModel = new BankModel();
+   $actual = null;
+   $bank = array(
+        'user_id' => '6',
+        'cost' => '123',
+   );
+   try {
+    $actual = $bankModel->insertBank('abcdefgh',   $bankModel);
+   } catch (Throwable $e) {
+   $excute = false;
+  }
+    $this->assertEquals($excute, $actual);
+  }
+   /*Test insert bank truyền vào số nguyên*/
+   public function testInsertBankIntegerNG()
+   {
+     
+     $bankModel = new BankModel();
+     $actual = null;
+     $bank = array(
+        'user_id' => '1',
+        'cost' => '123',
+     );
+     try {
+        $actual = $bankModel->insertBank(111,   $bankModel);
+     } catch (Throwable $e) {
+         $excute = false;
+     }
+     $this->assertEquals($excute, $actual);
+   }
+   /*Test insert bank truyền vào số nguyên*/
+   public function testInsertBankRealnumberNG()
+   {
+    
+     $bankModel = new BankModel();
+     $actual = null;
+     $bank = array(
+        'user_id' => '1',
+        'cost' => '123',
+     );
+     try {
+        $actual = $bankModel->insertBank(12.2,   $bankModel);
+     } catch (Throwable $e) {
+         $excute = false;
+     }
+     $this->assertEquals($excute, $actual);
+   }
+
+
+
+
+
+       /*Test  insert user nhập đúng OK*/
+    //    public function testInsertBankOk(){
+    //     $bankModel = new BankModel();
+    
+    //     $bank = array(
+           
+    //         'user_id' => '1',
+    //         'cost' => '1111',
+    //     );
+    //     $excute = true;
+    //     $actual = $bankModel->insertUser($bank, $bankModel);
+    //     $this->assertEquals($excute, $actual); 
+    // }
+    // public function testInsertBankOK(){
+    //     $bankModel = new BankModel();
+    //     $bankUserid ='1';
+    //     $bankCost = '1111';
+    //     $bank = $bankModel->insertBank($bankUserid);
+       
+    //     $actual = $bank[1]['cost'];
+    //     $this->assertEquals($bankCost, $actual);
+    // }
 }
--- tests/UserModelTest.php
+++ tests/UserModelTest.php
@@ -38,5 +38,167 @@ class UserModelTest extends TestCase
     }
      
        
-   
+    /*Test  insert user nhập đúng OK*/
+    public function testInsertUserOK()
+    {
+        $userModel = new UserModel();
+        $bankModel = new BankModel();
+        $user = array(
+            'name' => 'minh an',
+            'fullname' => 'minhan',
+            'type' => 'user',
+            'email' => 'toithap1999@gmail.com',
+            'password' => '12345'
+        );
+        $excute = true;
+
+        $actual = $userModel->insertUser($user, $bankModel);
+        $this->assertEquals($excute, $actual);
+    }
+     /*Test  insert nhập sai user Not OK*/
+ public function testInsertUserNG()
+ {
+     $userModel = new UserModel();
+     $bankModel = new BankModel();
+     $actual = null;
+     $user = array(
+         'name' => 'minh an',
+         'fullname' => 'minhan',
+         'type' => 'user',
+         'email' => 'toithap1999@gmail.com',
+         'password' => '12345'
+     );
+     try {
+         $actual = $userModel->insertUser('abcdefgh',  $bankModel);
+     } catch (Throwable $e) {
+         $excute = false;
+     }
+     $this->assertEquals($excute, $actual);
+ }
+/*Test insert user truyền vào chuỗi*/
+public function testInsertUserStringNotOK()
+{
+   $userModel = new UserModel();
+   $bankModel = new BankModel();
+   $actual = null;
+   $user = array(
+       'name' => 'minh an',
+           'fullname' => 'minhan',
+           'type' => 'user',
+           'email' => 'toithap1999@gmail.com',
+           'password' => '12345'
+   );
+   try {
+       $actual = $userModel->insertUser("sssdasad",  $bankModel);
+   } catch (Throwable $e) {
+       $excute = false;
+   }
+   $this->assertEquals($excute, $actual);
+}
+     /*Test insert user truyền vào số nguyên*/
+     public function testInsertUserIntegerNotOK()
+     {
+         $userModel = new UserModel();
+         $bankModel = new BankModel();
+         $actual = null;
+         $user = array(
+             'name' => 'minh an',
+             'fullname' => 'minhan',
+             'type' => 'user',
+             'email' => 'toithap1999@gmail.com',
+             'password' => '12345'
+         );
+         try {
+             $actual = $userModel->insertUser(111,  $bankModel);
+         } catch (Throwable $e) {
+             $excute = false;
+         }
+         $this->assertEquals($excute, $actual);
+     }
+ 
+      /*Test insert user truyền vào số nguyên*/
+      public function testInsertUserRealnumberNotOK()
+      {
+          $userModel = new UserModel();
+          $bankModel = new BankModel();
+          $actual = null;
+          $user = array(
+              'name' => 'minh an',
+              'fullname' => 'minhan',
+              'type' => 'user',
+              'email' => 'toithap1999@gmail.com',
+              'password' => '12345'
+          );
+          try {
+              $actual = $userModel->insertUser(14.1,  $bankModel);
+          } catch (Throwable $e) {
+              $excute = false;
+          }
+          $this->assertEquals($excute, $actual);
+      }
+  
+
+  /*  Test hàm getUser khi không có dữ liệu truyền vào  OK */
+public function testGetUserNotDataOK()
+{
+    $userModel = new UserModel();
+
+    $actual = $userModel->getUsers();
+
+    if ($actual != null) {
+        $this->assertTrue(true);
+    } else {
+        $this->assertTrue(false);
+    }
+}
+
+
+     /*Test hàm getUser khi có dữ liệu truyền vào là kiểu đối tượng*/
+     public function testGetUserObjectNotOK()
+     {
+         $userModel = new UserModel();
+         $actual = null;
+ 
+         $keyword = array(
+             'keyword' => $userModel,
+         );
+         try {
+             $actual = $userModel->getUsers($keyword);
+         } catch (Throwable $e) {
+             $excute = false;
+         }
+         $this->assertEquals($excute, $actual);
+     }
+
+
+ /*Test hàm getUser khi có dữ liệu truyền vào là Array*/
+ public function testGetUserArrayNotOK()
+ {
+     $userModel = new UserModel();
+     $actual = null;
+
+     $keyword = array(
+         'keyword' => ['ad'],
+     );
+     try {
+         $actual = $userModel->getUsers($keyword);
+     } catch (Throwable $e) {
+         $excute = false;
+     }
+     $this->assertEquals($excute, $actual);
+ }
+  /* Test hàm getUser khi có dữ liệu truyền vào  OK  */
+  public function testGetUserOK()
+  {
+      $userModel = new UserModel();
+      $keyword = array(
+          'keyword' => 'admin'
+      );
+      $actual = $userModel->getUsers($keyword);
+      if ($actual != []) {
+          $this->assertTrue(true);
+      } else {
+          $this->assertTrue(false);
+      }
+  }
 }
