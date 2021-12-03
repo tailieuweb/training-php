338dbb5054c3ffd17bc46bc3f8d1a38ba846bb67
nguyenminhantdc1999 <toithap1999@gmail.com>
2021-12-03 08:25:57 +0700

    s

--- tests/BankModelTest.php
+++ tests/BankModelTest.php
@@ -3,27 +3,6 @@ use PHPUnit\Framework\TestCase;
 
 class BankModelTest extends TestCase
 {
-    //Tim id tại vị có id hợp lệ
-    public function testFindBankbyIdOk()
-    {
-        $bankModel = new BankModel();
-        $bankId = 1;
-        $bankCost = '11';
-        $bank = $bankModel->findBankById($bankId);
-       
-        $actual = $bank[0]['cost'];
-        $this->assertEquals($bankCost, $actual);
-    }
-    //Tim id tại vị có id không hợp lệ
-    public function testFindBankByIdNG(){
-        $bankModel = new BankModel();
-        $id  = 1000000;
-        $actual = $bankModel->findBankById($id);
-        $expected = [];
-       
-        $this->assertEquals($expected, $actual);
-       
-    }  
-
+   
    
 }
