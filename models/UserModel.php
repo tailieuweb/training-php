c3402ccf9f3cadc91f911b277049d8f262bce1aa
Minh Tiến <nguyenminhtien@example.com>
2021-12-04 10:18:14 +0700

    commit findUser

--- models/UserModel.php
+++ models/UserModel.php
@@ -5,110 +5,110 @@ require_once 'BaseModel.php';
 class UserModel extends BaseModel
 {
 
-    public function findUserById($id)
-    {
-        $flag = is_integer($id);
-        if ($flag == false) {
-            return false;
-        }
-        $sql = 'SELECT * FROM users WHERE id = ' . $id;
-        $user = $this->select($sql);
-        return $user;
+  public function findUserById($id)
+  {
+    $flag = is_integer($id);
+    if ($flag == false) {
+      return false;
+    }
+    $sql = 'SELECT * FROM users WHERE id = ' . $id;
+    $user = $this->select($sql);
+    return $user;
+  }
+  // Minh Tien
+  public function findUser($keyword)
+  {
+    $flag = is_string($keyword);
+    if ($flag == false) {
+      return false;
+    } else {
+      $sql = 'SELECT * FROM users WHERE name LIKE "%' . $keyword . '%" OR email LIKE "%' . $keyword . '%"';
+      $user = $this->select($sql);
+      return $user;
+    }
+  }
+
+  /**
+   * Authentication user
+   * @param $userName
+   * @param $password
+   * @return array
+   */
+  public function auth($userName, $password)
+  {
+    $md5Password = md5($password);
+    $sql = 'SELECT * FROM users WHERE name = "' . $userName . '" AND password = "' . $md5Password . '"';
+
+    $user = $this->select($sql);
+    return $user;
+  }
+
+  /**
+   * Delete user by id
+   * @param $id
+   * @return mixed
+   */
+  public function deleteUserById($id)
+  {
+    $subString = substr($id, 36, -38);
+    $result = base64_decode($subString);
+    $sql = "DELETE FROM users WHERE MD5(users.id) = '" . md5($result) . "'";
+    return $this->delete($sql);
+  }
+  /**
+   * Update user
+   * @param $input
+   * @return mixed
+   */
+  public function updateUser($input)
+  {
+    $sql = "UPDATE `users` SET name = " . "'" . $input['name'] . "', fullname = " . "'" . $input['fullname'] . "', email = " . "'" . $input['email'] . "', type = " . "'" . $input['type'] . "', password = " . "'" . md5($input['password']) . "' WHERE id = " . "'" . $input['id'] . "'";
+    $user = $this->update($sql);
+    return $user;
+  }
+
+  /**
+   * Insert user
+   * @param $input
+   * @return mixed
+   */
+  public function insertUser($input)
+  {
+    $sql = "INSERT INTO `app_web1`.`users` (`name`,`fullname`,`email`,`type`, `password`) VALUES (" . "'" . $input['name'] . "', '" . $input['fullname'] . "', '" . $input['email'] . "', '" . $input['type'] . "','" . md5($input['password']) . "')";
+    $user = $this->insert($sql);
+    return $user;
+  }
+
+  /**
+   * Search users
+   * @param array $params
+   * @return array
+   */
+  public function getUsers($params = [])
+  {
+    //Keyword
+    if (!empty($params['keyword'])) {
+      $sql = 'SELECT * FROM users WHERE name LIKE "%' . $params['keyword'] . '%"';
+
+      //Keep this line to use Sql Injection
+      //Don't change
+      //Example keyword: abcef%";TRUNCATE banks;##
+      $users = self::$_connection->multi_query($sql);
+    } else {
+      $sql = 'SELECT * FROM users';
+      $users = $this->select($sql);
     }
 
-    public function findUser($keyword)
-    {
-        $flag = is_string($keyword);
-        if ($flag == false) {
-            return false;
-        } else {
-            $sql = 'SELECT * FROM users WHERE name LIKE "%' . $keyword . '%" OR email LIKE "%' . $keyword . '%"';
-            $user = $this->select($sql);
-            return $user;
-        }
-    }
+    return $users;
+  }
 
-    /**
-     * Authentication user
-     * @param $userName
-     * @param $password
-     * @return array
-     */
-    public function auth($userName, $password)
-    {
-        $md5Password = md5($password);
-        $sql = 'SELECT * FROM users WHERE name = "' . $userName . '" AND password = "' . $md5Password . '"';
-
-        $user = $this->select($sql);
-        return $user;
-    }
-
-    /**
-     * Delete user by id
-     * @param $id
-     * @return mixed
-     */
-    public function deleteUserById($id)
-    {
-        $subString = substr($id, 36, -38);
-        $result = base64_decode($subString);
-        $sql = "DELETE FROM users WHERE MD5(users.id) = '" . md5($result) . "'";
-        return $this->delete($sql);
-    }
-    /**
-     * Update user
-     * @param $input
-     * @return mixed
-     */
-    public function updateUser($input)
-    {
-        $sql = "UPDATE `users` SET name = " . "'" . $input['name'] . "', fullname = " . "'" . $input['fullname'] . "', email = " . "'" . $input['email'] . "', type = " . "'" . $input['type'] . "', password = " . "'" . md5($input['password']) . "' WHERE id = " . "'" . $input['id'] . "'";
-        $user = $this->update($sql);
-        return $user;
-    }
-
-    /**
-     * Insert user
-     * @param $input
-     * @return mixed
-     */
-    public function insertUser($input)
-    {
-        $sql = "INSERT INTO `app_web1`.`users` (`name`,`fullname`,`email`,`type`, `password`) VALUES (" . "'" . $input['name'] . "', '" . $input['fullname'] . "', '" . $input['email'] . "', '" . $input['type'] . "','" . md5($input['password']) . "')";
-        $user = $this->insert($sql);
-        return $user;
-    }
-
-    /**
-     * Search users
-     * @param array $params
-     * @return array
-     */
-    public function getUsers($params = [])
-    {
-        //Keyword
-        if (!empty($params['keyword'])) {
-            $sql = 'SELECT * FROM users WHERE name LIKE "%' . $params['keyword'] . '%"';
-
-            //Keep this line to use Sql Injection
-            //Don't change
-            //Example keyword: abcef%";TRUNCATE banks;##
-            $users = self::$_connection->multi_query($sql);
-        } else {
-            $sql = 'SELECT * FROM users';
-            $users = $this->select($sql);
-        }
-
-        return $users;
-    }
-
-    /**
-     * For testing
-     * @param $a
-     * @param $b
-     */
-    public function sumb($a, $b)
-    {
-        return $a + $b;
-    }
+  /**
+   * For testing
+   * @param $a
+   * @param $b
+   */
+  public function sumb($a, $b)
+  {
+    return $a + $b;
+  }
 }
