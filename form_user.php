18583c4011904f4e7ca1d722bd0413b088e93aab
Duc Manh <c9hai120@gmail.com>
2021-12-04 11:31:55 +0700

    update

--- form_user.php
+++ form_user.php
@@ -33,11 +33,7 @@ if (!empty($_POST['submit'])) {
     <?php include 'views/header.php'?>
     <div class="container">
 
-<<<<<<< HEAD
             <?php if ($user || empty($id)) { ?>
-=======
-            <?php if ($user || !isset($_id)) { ?>
->>>>>>> 1-php-202109/3-phpunit
                 <div class="alert alert-warning" role="alert">
                     User form
                 </div>
